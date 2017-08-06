78943d6747a9f55da30db8eca4c324d49ecdb696
Vijay Khemlani <vkhemlan@gmail.com>
2017-08-05 23:09:01 -0400

    * Moved format currency value to puvlic utility script

--- storescraper/currency.py
+++ storescraper/currency.py
@@ -1,4 +1,4 @@
-from decimal import Decimal
+from .utils import format_currency
 
 
 class Currency:
@@ -23,47 +23,6 @@ class Currency:
 
         currency = known_codes[code]
 
-        return cls._format_currency(
+        return format_currency(
             value, currency.prefix, currency.thousands_separator,
             currency.decimal_separator, places=currency.decimal_places)
-
-    @staticmethod
-    def _format_currency(value, curr='', sep='.', dp=',',
-                         pos='', neg='-', trailneg='', places=0):
-        """Convert Decimal to a money formatted string.
-
-        curr: optional currency symbol before the sign (may be blank)
-        sep: optional grouping separator (comma, period, space, or blank)
-        dp: decimal point indicator (comma or period)
-        only specify as blank when places is zero
-        pos: optional sign for positive numbers: '+', space or blank
-        neg: optional sign for negative numbers: '-', '(', space or blank
-        trailneg:optional trailing minus indicator: '-', ')', space or blank
-        places: Number of decimal places to consider
-        """
-
-        quantized_precision = Decimal(10) ** -places  # 2 places --> '0.01'
-        sign, digits, exp = value.quantize(quantized_precision).as_tuple()
-        result = []
-        digits = list(map(str, digits))
-        build, iter_next = result.append, digits.pop
-
-        if sign:
-            build(trailneg)
-        for i in range(places):
-            build(iter_next() if digits else '0')
-        if places:
-            build(dp)
-        if not digits:
-            build('0')
-        i = 0
-        while digits:
-            build(iter_next())
-            i += 1
-            if i == 3 and digits:
-                i = 0
-                build(sep)
-        build(curr)
-        build(neg if sign else pos)
-
-        return ''.join(reversed(result))
--- storescraper/utils.py
+++ storescraper/utils.py
@@ -1,4 +1,5 @@
 import importlib
+from decimal import Decimal
 
 CLP_BLACKLIST = ['CLP$', 'precio', 'internet', 'normal',
                  '$', '.', ',', '&nbsp;', '\r', '\n', '\t']
@@ -20,3 +21,44 @@ def chunks(l, n):
 def get_store_class_by_name(store_class_name):
     store_module = importlib.import_module('storescraper.stores')
     return getattr(store_module, store_class_name)
+
+
+def format_currency(value, curr='', sep='.', dp=',',
+                    pos='', neg='-', trailneg='', places=0):
+    """Convert Decimal to a money formatted string.
+
+    curr: optional currency symbol before the sign (may be blank)
+    sep: optional grouping separator (comma, period, space, or blank)
+    dp: decimal point indicator (comma or period)
+    only specify as blank when places is zero
+    pos: optional sign for positive numbers: '+', space or blank
+    neg: optional sign for negative numbers: '-', '(', space or blank
+    trailneg:optional trailing minus indicator: '-', ')', space or blank
+    places: Number of decimal places to consider
+    """
+
+    quantized_precision = Decimal(10) ** -places  # 2 places --> '0.01'
+    sign, digits, exp = value.quantize(quantized_precision).as_tuple()
+    result = []
+    digits = list(map(str, digits))
+    build, iter_next = result.append, digits.pop
+
+    if sign:
+        build(trailneg)
+    for i in range(places):
+        build(iter_next() if digits else '0')
+    if places:
+        build(dp)
+    if not digits:
+        build('0')
+    i = 0
+    while digits:
+        build(iter_next())
+        i += 1
+        if i == 3 and digits:
+            i = 0
+            build(sep)
+    build(curr)
+    build(neg if sign else pos)
+
+    return ''.join(reversed(result))
