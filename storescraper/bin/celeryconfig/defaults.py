cc65d096880a243b7eb808cd9718e68cdef629ee
Vijay Khemlani <vkhemlan@gmail.com>
2017-09-29 18:30:44 -0300

    * Using UTC for default timezone

--- storescraper/bin/celeryconfig/defaults.py
+++ storescraper/bin/celeryconfig/defaults.py
@@ -1,9 +1,6 @@
 import sys
 sys.path.append('../..')
 
-enable_utc = True
-timezone = 'America/Santiago'
-
 broker_url = 'amqp://storescraper:storescraper@localhost/storescraper'
 result_backend = 'rpc://'
 
