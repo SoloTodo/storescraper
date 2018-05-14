38bab04cd1b09bce9f9d41553c2d2f6b541c40ff
Vijay Khemlani <vkhemlan@gmail.com>
2018-05-14 08:19:33 -0400

    * Updated Walmart Mexico

--- storescraper/stores/walmart_mexico.py
+++ storescraper/stores/walmart_mexico.py
@@ -65,6 +65,9 @@ class WalmartMexico(Store):
     def products_for_url(cls, url, category=None, extra_args=None):
         print(url)
         session = session_with_proxy(extra_args)
+        session.headers['User-Agent'] = \
+            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 ' \
+            '(KHTML, like Gecko) Chrome/62.0.3202.62 Safari/537.36'
 
         response = session.get(url)
         if response.url != url:
