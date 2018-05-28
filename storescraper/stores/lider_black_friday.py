fa6656a7c341756b8e725c366294d506f571f945
Vijay Khemlani <vkhemlan@gmail.com>
2018-05-27 23:24:56 -0400

    * Updated LiderBlackFriday

--- storescraper/stores/lider_black_friday.py
+++ storescraper/stores/lider_black_friday.py
@@ -39,42 +39,28 @@ class LiderBlackFriday(Store):
 
     @classmethod
     def discover_urls_for_category(cls, category, extra_args=None):
-        return ['http://blackfriday.lider.cl/']
+        return ['http://cyber.lider.cl/']
 
     @classmethod
     def products_for_url(cls, url, category=None, extra_args=None):
         categories_dict = {
-            'Computaci\xf3n': 'Notebook',
-            'Smartphones': 'Cell',
-            'Linea Blanca': 'Refrigerator',
-            'Electrodom\xe9sticos': 'Oven',
-            'Televisi\xf3n': 'Television',
-            'Audio': 'StereoSystem',
-            'Impresoras': 'Printer'
+            'Notebook': 'Notebooks',
+            'Cell': 'Smartphones',
+            'Refrigerator': 'Línea Blanca',
+            'Oven': 'Electrodomésticos',
+            'Television': 'Televisión',
+            'StereoSystem': 'Audio',
+            'Printer': 'Impresoras',
+            'VideoGameConsole': 'Videojuegos'
         }
+
+        if category and category not in categories_dict:
+            return []
 
         session = session_with_proxy(extra_args)
 
-        categories = json.loads(
-            session.get(
-                'http://blackfriday.lider.cl/categories.json').text)
-
-        product_to_type = {}
-
-        for value in categories.values():
-            for category_children in value['children']:
-                for local_category, category_values in \
-                        category_children.items():
-                    product_type = categories_dict.get(local_category)
-
-                    if not product_type:
-                        continue
-
-                    for producto in category_values['productos']:
-                        product_to_type[producto] = product_type
-
         product_to_stock = {}
-        stocks = json.loads(session.get('http://blackfriday.lider.cl/'
+        stocks = json.loads(session.get('http://cyber.lider.cl/'
                                         'inventario/ff_inv_landing.json').text)
 
         for stocks in stocks['Inventory']:
@@ -83,24 +69,26 @@ class LiderBlackFriday(Store):
 
         json_products = json.loads(
             requests.get(
-                'http://blackfriday.lider.cl/products.json').text)['products']
+                'http://cyber.lider.cl/products.json').text)['products']
 
         products = []
+        category_keyword = categories_dict[category] if category else None
 
         for json_product in json_products:
-            product_type = product_to_type.get(json_product['ID'])
-            if product_type != category:
+            if category_keyword and category_keyword not in \
+                    json_product['categorias']:
                 continue
 
-            product_url = 'http://blackfriday.lider.cl/#!/index.html/' \
+            product_url = 'http://cyber.lider.cl/#!/product/' \
                           + json_product['ID']
 
             brand = json_product['brand']
             model = json_product['displayName']
-            name = u'{} {}'.format(brand, model)
+            name = '{} {}'.format(brand, model)
             sku = json_product['sku']
 
             if isinstance(json_product['price'], list):
+                print(json_product)
                 continue
 
             normal_price = Decimal(json_product['price']['BasePriceSales'])
@@ -111,15 +99,11 @@ class LiderBlackFriday(Store):
             else:
                 offer_price = normal_price
 
-            description = html_to_markdown(json_product['longDescription'])
+            stock = product_to_stock.get('PROD_' + sku, None)
 
-            soup = BeautifulSoup(json_product['longDescription'],
-                                 'html.parser')
-
-            picture_urls = [tag['src'] for tag in soup.findAll('img')]
-
-            stock = product_to_stock['PROD_' + sku]
-            if stock < 0:
+            if stock is None:
+                stock = -1
+            elif stock < 0:
                 stock = 0
 
             p = Product(
@@ -133,9 +117,7 @@ class LiderBlackFriday(Store):
                 normal_price,
                 offer_price,
                 'CLP',
-                sku=sku,
-                description=description,
-                picture_urls=picture_urls
+                sku=sku
             )
 
             products.append(p)
