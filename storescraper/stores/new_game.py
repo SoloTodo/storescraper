c2f9ba7ec16a9fb3a7d524d0381f80cb5aa1a51e
Vijay Khemlani <vkhemlan@gmail.com>
2018-08-01 16:00:37 -0400

    * Updated NewGamer

--- storescraper/stores/new_game.py
+++ storescraper/stores/new_game.py
@@ -52,7 +52,7 @@ class NewGame(Store):
         response = session.get(url)
 
         if response.status_code == 404:
-            return None
+            return []
 
         soup = BeautifulSoup(response.text, 'html.parser')
 
