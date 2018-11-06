8a975e6e2752a590e15048182489f82b8484d2a9
Vijay Khemlani <vkhemlan@gmail.com>
2018-11-06 15:59:11 -0300

    * Updated ReifStore

--- storescraper/stores/reif_store.py
+++ storescraper/stores/reif_store.py
@@ -1,3 +1,5 @@
+import json
+
 import demjson
 import re
 from bs4 import BeautifulSoup
@@ -22,8 +24,8 @@ class ReifStore(Store):
     def discover_urls_for_category(cls, category, extra_args=None):
         category_paths = [
             ['88-portatil', 'Notebook'],
-            ['14-iphone', 'Cell'],
-            ['12-ipad', 'Tablet'],
+            ['85-iphone', 'Cell'],
+            ['207-ipad', 'Tablet'],
             ['127-audifonos', 'Headphones'],
         ]
 
@@ -34,77 +36,113 @@ class ReifStore(Store):
             if local_category != category:
                 continue
 
-            page = 1
+            category_url = 'https://www.reifstore.cl/{}-'.format(
+                category_path
+            )
 
-            while True:
-                category_url = 'https://www.reifstore.cl/{}?p={}'.format(
-                    category_path, page
-                )
+            response = session.get(category_url)
+            soup = BeautifulSoup(response.text, 'html.parser')
+            containers = soup.findAll('div', 'product-container')
 
-                if page >= 10:
-                    raise Exception('Page overflow: ' + category_url)
+            if not containers:
+                raise Exception('No products found for: ' + category_url)
 
-                response = session.get(category_url)
-
-                if response.url != category_url:
-                    if page == 1:
-                        raise Exception('Empty category: ' + category_url)
-                    break
-
-                soup = BeautifulSoup(response.text, 'html.parser')
-
-                for container in soup.findAll('div', 'product-container'):
-                    product_url = container.find('a')['href']
-                    product_urls.append(product_url)
-
-                page += 1
+            for container in containers:
+                product_url = container.find('a')['href']
+                product_urls.append(product_url)
 
         return product_urls
 
     @classmethod
     def products_for_url(cls, url, category=None, extra_args=None):
+        print(url)
         session = session_with_proxy(extra_args)
-        soup = BeautifulSoup(session.get(url).text, 'html.parser')
+        content = session.get(url).text
+        soup = BeautifulSoup(content, 'html.parser')
 
-        name = soup.find('h1').text.strip()
-        part_number = soup.find('span', {'itemprop': 'sku'})['content']
-        sku = soup.find('input', {'name': 'id_product'})['value']
-
-        availability = soup.find('link', {'itemprop': 'availability'})
-
-        if availability and \
-                availability['href'] == 'http://schema.org/InStock':
-            stock = -1
-        else:
-            stock = 0
-
-        price = Decimal(soup.find('span', {'itemprop': 'price'})['content'])
-
-        picture_urls = []
-
-        for tag in soup.find('ul', {'id': 'thumbs_list_frame'}).findAll('a'):
-            picture_url = demjson.decode(
-                re.search(r'rel="(.+?)"', str(tag)).groups()[0])['largeimage']
-            picture_urls.append(picture_url)
-
+        base_sku = soup.find('input', {'name': 'id_product'})['value']
+        base_price = Decimal(
+            soup.find('span', {'itemprop': 'price'})['content'])
+        base_name = soup.find('h1').text.strip()
         description = html_to_markdown(
             str(soup.find('section', 'page-product-box')))
 
-        p = Product(
-            name,
-            cls.__name__,
-            category,
-            url,
-            url,
-            sku,
-            stock,
-            price,
-            price,
-            'CLP',
-            sku=sku,
-            part_number=part_number,
-            description=description,
-            picture_urls=picture_urls
-        )
+        combinations_text = re.search(r'var combinations = (.+);', content)
 
-        return [p]
+        if combinations_text:
+            combinations = json.loads(combinations_text.groups()[0])
+            products = []
+
+            for combination_id, combination in combinations.items():
+                combination_name = base_name
+
+                for attribute in combination['attributes_values'].values():
+                    combination_name += ' ' + attribute
+
+                stock = combination['quantity']
+                part_number = combination['reference']
+                combination_sku = '{}_{}'.format(base_sku, combination_id)
+                combination_price_delta = Decimal(combination['price'])
+                combination_price = base_price + combination_price_delta
+
+                picture_urls = [
+                    'https://www.reifstore.cl/{}/product.jpg'.format(
+                        combination['id_image'])
+                ]
+
+                products.append(Product(
+                    combination_name,
+                    cls.__name__,
+                    category,
+                    url,
+                    url,
+                    combination_sku,
+                    stock,
+                    combination_price,
+                    combination_price,
+                    'CLP',
+                    sku=base_sku,
+                    part_number=part_number,
+                    description=description,
+                    picture_urls=picture_urls
+                ))
+
+            return products
+        else:
+            part_number = soup.find('span', {'itemprop': 'sku'})['content']
+
+            availability = soup.find('link', {'itemprop': 'availability'})
+
+            if availability and \
+                    availability['href'] == 'http://schema.org/InStock':
+                stock = -1
+            else:
+                stock = 0
+
+            picture_urls = []
+
+            for tag in soup.find(
+                    'ul', {'id': 'thumbs_list_frame'}).findAll('a'):
+                picture_url = demjson.decode(
+                    re.search(r'rel="(.+?)"', str(tag)).groups()[0])[
+                    'largeimage']
+                picture_urls.append(picture_url)
+
+            p = Product(
+                base_name,
+                cls.__name__,
+                category,
+                url,
+                url,
+                base_sku,
+                stock,
+                base_price,
+                base_price,
+                'CLP',
+                sku=base_sku,
+                part_number=part_number,
+                description=description,
+                picture_urls=picture_urls
+            )
+
+            return [p]
