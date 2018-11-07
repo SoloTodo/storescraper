0694cdb248cf199a6f8b767c7d141d5ab7f1ce22
Vijay Khemlani <vkhemlan@gmail.com>
2018-11-07 11:34:07 -0300

    * Updated Hites

--- storescraper/stores/hites.py
+++ storescraper/stores/hites.py
@@ -1,14 +1,11 @@
 import json
-import urllib
 
-import re
 from bs4 import BeautifulSoup
 from decimal import Decimal
 
 from storescraper.product import Product
 from storescraper.store import Store
-from storescraper.utils import remove_words, html_to_markdown, \
-    session_with_proxy
+from storescraper.utils import html_to_markdown, session_with_proxy
 
 
 class Hites(Store):
@@ -40,180 +37,118 @@ class Hites(Store):
             'ExternalStorageDrive',
             'Monitor',
             'Projector',
+            'AirConditioner',
+            'WaterHeater',
+            'UsbFlashDrive',
         ]
 
     @classmethod
     def discover_urls_for_category(cls, category, extra_args=None):
         url_extensions = [
-            ['14002', 'Refrigerator'],
-            ['14067', 'Notebook'],
-            ['14114', 'Television'],
-            ['14068', 'Tablet'],
-            ['14070', 'Printer'],
-            ['14022', 'Oven'],
-            ['14023', 'Oven'],
-            ['14046', 'VacuumCleaner'],
-            ['14014', 'WashingMachine'],
-            ['14017', 'WashingMachine'],
-            ['14015', 'WashingMachine'],
-            ['14108', 'Cell'],
-            ['14074', 'Camera'],
-            ['14082', 'StereoSystem'],  # Microcomponentes
-            ['14083', 'StereoSystem'],  # Minicomponentes
-            ['14084', 'StereoSystem'],  # Audio Portable
-            ['14103', 'StereoSystem'],  # Parlantes portables y Speaker
-            ['14120', 'OpticalDiskPlayer'],
-            ['14079', 'StereoSystem'],  # Hi-Fi y Home Theater
-            ['14087', 'VideoGameConsole'],
-            ['14065', 'AllInOne'],
-            # ['14054', 'SpaceHeater'],
-            # ['14051', 'SpaceHeater'],
-            # ['14052', 'SpaceHeater'],
-            # ['14056', 'SpaceHeater'],  # Termoventiladores
-            # ['14107', 'Keyboard'],
-            ['14094', 'Mouse'],
-            ['14092', 'Headphones'],
-            ['14071', 'ExternalStorageDrive'],
-            ['14069', 'Monitor'],
+            ['electro-hogar/refrigeradores', 'Refrigerator'],
+            ['electro-hogar/lavado-y-secado/lavadoras', 'WashingMachine'],
+            ['electro-hogar/lavado-y-secado/lavadoras-secadoras',
+             'WashingMachine'],
+            ['electro-hogar/lavado-y-secado/secadoras', 'WashingMachine'],
+            ['electro-hogar/lavado-y-secado/centrifugas', 'WashingMachine'],
+            ['electro-hogar/cocina/hornos-empotrados', 'Oven'],
+            ['electro-hogar/cocina/hornos-electricos', 'Oven'],
+            ['electro-hogar/cocina/microondas', 'Oven'],
+            ['electro-hogar/electrodomesticos/hornos-electricos', 'Oven'],
+            ['electro-hogar/electrodomesticos/microondas', 'Oven'],
+            ['electro-hogar/climatizacion/aire-acondicionado',
+             'AirConditioner'],
+            ['electro-hogar/climatizacion/estufa-a-lena', 'SpaceHeater'],
+            ['electro-hogar/climatizacion/calefont-y-termos', 'WaterHeater'],
+            ['tecnologia/tv-video/todos-los-led', 'Television'],
+            ['tecnologia/tv-video/dvd-y-blu-ray', 'OpticalDiskPlayer'],
+            ['tecnologia/computacion/notebook', 'Notebook'],
+            ['tecnologia/computacion/tablets', 'Tablet'],
+            ['tecnologia/computacion/impresoras-y-multifuncionales',
+             'Printer'],
+            ['tecnologia/computacion/pendrive', 'UsbFlashDrive'],
+            ['tecnologia/computacion/monitores-y-proyectores', 'Monitor'],
+            ['tecnologia/computacion/all-in-one', 'AllInOne'],
+            ['tecnologia/computacion/disco-duro', 'ExternalStorageDrive'],
+            ['tecnologia/video-juego/consolas', 'VideoGameConsole'],
+            ['tecnologia/video-juego/consolas', 'VideoGameConsole'],
+            ['tecnologia/audio/parlantes-bluetooth-y-karaokes',
+             'StereoSystem'],
+            ['tecnologia/audio/minicomponentes', 'StereoSystem'],
+            ['tecnologia/audio/hi-fi-y-home-theater', 'StereoSystem'],
+            ['tecnologia/audio/microcomponentes', 'StereoSystem'],
+            ['tecnologia/audio/audifonos', 'Headphones'],
+            ['celulares/accesorios/audifonos', 'Headphones'],
+            ['tecnologia/accesorios-y-otros/mouse-y-teclados', 'Mouse'],
+            ['tecnologia/accesorios-y-otros/tarjetas-de-memoria',
+             'MemoryCard'],
+            ['celulares/smartphone/smartphone', 'Cell'],
+            ['celulares/smartphone/smartphone-liberados', 'Cell'],
+            ['celulares/smartphone/celulares-basicos', 'Cell'],
+            ['electro-hogar/electrodomesticos/aspiradoras-y-enceradoras',
+             'VacuumCleaner'],
         ]
 
         product_urls = []
         session = session_with_proxy(extra_args)
-        session.headers['Content-Type'] = 'application/x-www-form-urlencoded'
 
         for category_id, local_category in url_extensions:
             if local_category != category:
                 continue
 
-            category_url = 'https://www.hites.com/tienda/ProductListingView?' \
-                           'categoryId={}&storeId=10151&pageSize=1000' \
-                           ''.format(category_id)
+            page = 1
 
-            print(category_url)
+            while True:
+                category_url = 'https://www.hites.com/{}?pageSize=48&page={}' \
+                               ''.format(category_id, page)
 
-            soup = BeautifulSoup(session.get(category_url, timeout=10).text,
-                                 'html.parser')
+                print(category_url)
 
-            divs = soup.findAll('div', 'product')
+                response = session.get(category_url, timeout=30)
 
-            if not divs:
-                raise Exception('No products found for {}'.format(
-                    category_url))
+                if response.status_code == 500:
+                    if page == 1:
+                        raise Exception('Empty category: ' + category_url)
+                    break
 
-            for div in divs:
-                product_url = div.find('a')['href']
-                product_url = product_url.replace('http://', 'https://')
+                soup = BeautifulSoup(response.text, 'html.parser')
+                json_data = json.loads(soup.find(
+                    'script', {'id': 'hy-data'}).text)
 
-                if 'ProductDisplay' in product_url:
-                    parsed_product = urllib.parse.urlparse(
-                        product_url)
-                    parameters = urllib.parse.parse_qs(
-                        parsed_product.query)
+                for product_entry in json_data['result']['products']:
+                    slug = product_entry['productString']
+                    product_url = 'https://www.hites.com/' + slug
+                    product_urls.append(product_url)
+                    print(product_url)
 
-                    parameters = {
-                        k: v for k, v in parameters.items()
-                        if k in ['urlRequestType', 'productId', 'storeId']}
-
-                    newqs = urllib.parse.urlencode(parameters, doseq=True)
-
-                    product_url = \
-                        'https://www.hites.com/tienda/ProductDisplay?' + newqs
-
-                product_urls.append(product_url)
+                page += 1
 
         return product_urls
 
     @classmethod
     def products_for_url(cls, url, category=None, extra_args=None):
+        print(url)
+
         session = session_with_proxy(extra_args)
         page_source = session.get(url, timeout=10).text
         soup = BeautifulSoup(page_source, 'html.parser')
+        json_data = json.loads(soup.find('script', {'id': 'hy-data'}).text)[
+            'product']
 
-        if soup.find('div', {'id': 'errorPage'}):
-            return []
+        name = json_data['name']
+        sku = json_data['partNumber']
 
-        sku = soup.find('meta', {'name': 'pageIdentifier'})['content']
+        stock = -1 if json_data['buyable'] else 0
+        normal_price = Decimal(json_data['prices']['offerPrice'])
+        offer_price = Decimal(json_data['prices']['cardPrice'])
 
-        description = ''
+        description = html_to_markdown(json_data['longDescription'])
 
-        for panel in soup.findAll('div', 'descripcion-producto'):
-            description += html_to_markdown(str(panel)) + '\n\n'
+        for attribute in json_data['attributes']:
+            description += '\n{} {}'.format(attribute['name'],
+                                            attribute['value'])
 
-        if soup.find('div', 'noDisponible_button'):
-            stock = 0
-
-            name = soup.find('h1', {'role': 'heading'}).text
-            price_str = soup.find(
-                'input', {'id': 'hitesprice_paymentTerms'})
-
-            if not price_str or not price_str['value']:
-                return []
-
-            normal_price = Decimal(price_str['value'])
-            offer_price = normal_price
-        else:
-            stock = -1
-
-            catentry_id = re.search(r'"catentry_id" : "(\d+)"',
-                                    page_source)
-
-            if not catentry_id:
-                return []
-
-            catentry_id = catentry_id.groups()[0]
-
-            ajax_url = 'https://www.hites.com/tienda/' \
-                       'GetCatalogEntryDetailsByIDView?storeId=10151' \
-                       '&catalogEntryId=' + catentry_id
-
-            ajax_body = session.get(ajax_url).text
-
-            json_body = re.search(r'/\*([\S\s]+)\*/', ajax_body).groups()[0]
-            json_body = json_body.replace('\n', '').replace('\t', '').replace(
-                '\r', '').replace('\x1a', '')
-            json_body = re.sub(r'\"longDescription\": [\S\s]+\"keyword"',
-                               '\"keyword\"', json_body)
-            json_body = json.loads(json_body)['catalogEntry']
-
-            name = json_body['description'][0]['name']
-
-            normal_price = remove_words(json_body['offerPrice'])
-            if normal_price == '':
-                return []
-
-            normal_price = Decimal(normal_price)
-
-            offer_price = remove_words(json_body['hitesPrice'])
-            if offer_price:
-                offer_price = Decimal(offer_price)
-                if offer_price > normal_price:
-                    offer_price = normal_price
-            else:
-                offer_price = normal_price
-
-        # Pictures
-
-        page_id = soup.find('meta', {'name': 'pageId'})['content']
-        gallery_content = soup.find(
-            'div', {'id': 'entitledItem_' + page_id}).text
-        gallery_content = gallery_content.replace('\\', '/')
-        gallery_content = re.sub(r'"DescriptiveAttributes"[\S\s]*?},', '',
-                                 gallery_content)
-        gallery_json = json.loads(gallery_content)
-
-        picture_urls = [
-            'https://www.hites.com' +
-            soup.find('img', {'id': 'productMainImage'})['src']
-        ]
-
-        if gallery_json and 'ItemAngleFullImage' in gallery_json[0]:
-            sorted_pictures = sorted(
-                gallery_json[0]['ItemAngleFullImage'].items(),
-                key=lambda pair: int(pair[0].replace('image_', '')))
-            for picture_pair in sorted_pictures:
-                picture_url = 'https://www.hites.com' + picture_pair[1]
-                if picture_url not in picture_urls:
-                    picture_urls.append(picture_url)
+        picture_urls = json_data['children'][0]['images']
 
         p = Product(
             name,
