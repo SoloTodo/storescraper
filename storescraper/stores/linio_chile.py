ca840e33cc739197e021b9aa78b56afa1adc2a03
Vijay Khemlani <vkhemlan@gmail.com>
2018-11-27 10:37:33 -0300

    * Updated Linio Chile

--- storescraper/stores/linio_chile.py
+++ storescraper/stores/linio_chile.py
@@ -106,7 +106,7 @@ class LinioChile(Store):
         session = session_with_proxy(extra_args)
         response = session.get(url)
 
-        if response.url != url:
+        if response.status_code == 404 or response.url != url:
             return []
 
         soup = BeautifulSoup(response.text, 'html.parser')
