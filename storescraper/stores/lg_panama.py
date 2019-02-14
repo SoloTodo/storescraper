f45128a0e9f42e6743d07d20156c661bc9cd4f1f
Vijay Khemlani <vkhemlan@gmail.com>
2019-02-14 14:11:21 -0300

    * Updated LG Panama

--- storescraper/stores/lg_panama.py
+++ storescraper/stores/lg_panama.py
@@ -8,37 +8,28 @@ class LgPanama(LgChile):
     def _category_paths(cls):
         return [
             ('CT20188038', 'CT20188038', 'Television', True),
-            # ('CT20188038', 'CT20188038', 'Television', False),
-            ('CT20188052', 'CT20188052', 'StereoSystem', True),
-            # ('CT20188052', 'CT20188052', 'StereoSystem', False),
+            # ('CT20188052', 'CT20188052', 'StereoSystem', True),
+            ('CT20188049', 'CT32016442', 'StereoSystem', True),  # OneBody
+            # Minicomponentes
             ('CT20188049', 'CT20188050', 'StereoSystem', True),
-            # ('CT20188049', 'CT20188050', 'StereoSystem', False),
-            ('CT30015340', 'CT30015340', 'StereoSystem', True),
-            # ('CT30015340', 'CT30015340', 'StereoSystem', False),
+            # Barra de sonido
+            # ('CT20188049', 'CT32016444', 'StereoSystem', True),
+            # Parlantes Bluetooth
+            ('CT20188049', 'CT32016443', 'StereoSystem', True),
             ('CT20188055', 'CT20188057', 'OpticalDiskPlayer', True),
-            # ('CT20188055', 'CT20188057', 'OpticalDiskPlayer', False),
             ('CT20188055', 'CT20188056', 'OpticalDiskPlayer', True),
-            # ('CT20188055', 'CT20188056', 'OpticalDiskPlayer', False),
             ('CT20188025', 'CT20188025', 'Projector', True),
             ('CT20188032', 'CT20188032', 'Cell', True),
-            # ('CT20188032', 'CT20188032', 'Cell', False),
             ('CT30006720', 'CT30006720', 'Headphones', True),
-            # ('CT30006720', 'CT30006720', 'Headphones', False),
             ('CT20188021', 'CT20188021', 'Refrigerator', True),
-            # ('CT20188021', 'CT20188021', 'Refrigerator', False),
             ('CT20188014', 'CT20188014', 'WashingMachine', True),
-            # ('CT20188014', 'CT20188014', 'WashingMachine', False),
             ('CT32015142', 'CT32015142', 'WashingMachine', True),  # Styler
             ('CT20188013', 'CT20188013', 'VacuumCleaner', True),  # Aspiradoras
-            # ('CT20188013', 'CT20188013', 'VacuumCleaner', False),
             ('CT30015260', 'CT30015260', 'Oven', True),  # Hornos Y Estufas
-            # ('CT30015260', 'CT30015260', 'Oven', False),
             ('CT20188009', 'CT20188009', 'Oven', True),  # Microondas
-            # ('CT20188009', 'CT20188009', 'Oven', False),
             ('CT30015420', 'CT30015420', 'DishWasher', True),  # Lavaplatos
             # Aire Acondicionado Split Inverter
             ('CT30014140', 'CT30014241', 'AirConditioner', True),
-            # ('CT30014140', 'CT30014241', 'AirConditioner', False),
             # Aire Acondicionado Split Convencional
             # ('CT30014140', 'CT30014242', 'AirConditioner', True),
             # Aire Acondicionado de Ventana
