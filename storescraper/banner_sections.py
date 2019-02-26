cc6cb1dbaf9e796e847b42acc63ffceb51dd6cd8
david <david@solotodo.com>
2019-02-26 11:19:52 -0300

    ADDED: full banner scraper for ripley, working with new model

---
+++ storescraper/banner_sections.py
@@ -0,0 +1,15 @@
+HOME = 'Home'
+REFRIGERATION = 'Refrigeración'
+WASHING_MACHINES = 'Lavado'
+TELEVISIONS = 'Televisión'
+AUDIO = 'Audio'
+CELLS = 'Telefonía'
+
+# Store Specifics #
+ELECTRO_RIPLEY = 'Electro Ripley'
+TECNO_RIPLEY = 'Tecno Ripley'
+
+# Types #
+SUBSECTION_TYPE_HOME = 'Home'
+SUBSECTION_TYPE_CATEGORY_PAGE = 'Category page'
+SUBSECTION_TYPE_MOSAIC = 'Mosaico'
