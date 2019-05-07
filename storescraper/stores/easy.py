f9c0ee107553d5e5d496b24bb603ed60ad91db57
david <david@solotodo.com>
2019-05-07 13:17:41 -0400

    UPDATED: now easy gets position info

--- storescraper/stores/easy.py
+++ storescraper/stores/easy.py
@@ -1,6 +1,6 @@
 import json
-
 import re
+from collections import defaultdict
 from decimal import Decimal
 
 from storescraper.product import Product
@@ -30,55 +30,96 @@ class Easy(Store):
         ]
 
     @classmethod
-    def discover_urls_for_category(cls, category, extra_args=None):
+    def discover_entries_for_category(cls, category, extra_args=None):
         category_paths = [
-            ['frio-directo', 'Refrigerator'],
-            ['no-frost', 'Refrigerator'],
-            ['freezer', 'Refrigerator'],
-            ['frigobar', 'Refrigerator'],
-            ['side-by-side', 'Refrigerator'],
-            ['hornos-electricos', 'Oven'],
-            ['hornos-empotrables', 'Oven'],
-            ['microondas', 'Oven'],
-            ['aspiradoras', 'VacuumCleaner'],
-            ['lavadoras', 'WashingMachine'],
-            ['lavadora-secadora', 'WashingMachine'],
-            ['secadoras', 'WashingMachine'],
-            ['reproductores', 'OpticalDiskPlayer'],
-            ['iluminación-led', 'Lamp'],
-            ['reflectores-exterior', 'LightProjector'],
-            ['calefont-gas-licuado', 'WaterHeater'],
-            ['calefont-gas-natural', 'WaterHeater'],
-            ['termos', 'WaterHeater'],
-            ['calefactores-a-leña', 'SpaceHeater'],
+            ['frio-directo', ['Refrigerator'],
+             'Inicio > Electrohogar > Refrigeración > Frío Directo', 1],
+            ['no-frost', ['Refrigerator'],
+             'Inicio > Electrohogar > Refrigeración > No Frost', 1],
+            ['freezer', ['Refrigerator'],
+             'Inicio > Electrohogar > Refrigeración > Freezer', 1],
+            ['frigobar', ['Refrigerator'],
+             'Inicio > Electrohogar > Refrigeración > Frigobar', 1],
+            ['side-by-side', ['Refrigerator'],
+             'Inicio > Electrohogar > Refrigeración > Side by Side', 1],
+
+            ['hornos-electricos', ['Oven'],
+             'Inicio > Electrohogar > Cocina > Hornos Eléctricos', 1],
+            ['hornos-empotrables', ['Oven'],
+             'Inicio > Electrohogar > Cocina > Hornos Empotrables', 1],
+            ['microondas', ['Oven'],
+             'Inicio > Electrohogar > Cocina > Microondas', 1],
+
+            ['aspiradoras', ['VacuumCleaner'],
+             'Inicio > Electrohogar > Electrodomésticos > Aspiradoras', 1],
+
+            ['lavadoras', ['WashingMachine'],
+             'Inicio > Electrohogar > Lavado y Secado > Lavadoras', 1],
+            ['lavadora-secadora', ['WashingMachine'],
+             'Inicio > Electrohogar > Lavado y Secado > Lavadoras-Secadoras',
+             1],
+            ['secadoras', ['WashingMachine'],
+             'Inicio > Electrohogar > Lavado y Secado > Secadoras', 1],
+            ['lavavajillas', ['DishWasher'],
+             'Inicio > Electrohogar > Lavado y Secado > Lavavajillas', 1],
+
+            # ['reproductores', 'OpticalDiskPlayer'],
+
+            ['iluminación-led', ['Lamp'],
+             'Inicio > Iluminación > Iluminación Led', 1],
+
+            ['calefont-gas-licuado', ['WaterHeater'],
+             'Inicio > Electrohogar > Calefones y Termos > '
+             'Calefones Gas Licuado', 1],
+            ['calefont-gas-natural', ['WaterHeater'],
+             'Inicio > Electrohogar > Calefones y Termos > '
+             'Calefones Gas Natural', 1],
+            ['termos', ['WaterHeater'],
+             'Inicio > Electrohogar > Calefones y Termos > Termos', 1],
+
+            ['estufas-electrica', ['SpaceHeater'],
+             'Inicio > Electrohogar > Calefacción > Estufas Eléctricas', 1],
+            ['estufas-a-gas', ['SpaceHeater'],
+             'Inicio > Electrohogar > Calefacción > Estufas a Gas', 1],
+            ['estufas-a-parafina', ['SpaceHeater'],
+             'Inicio > Electrohogar > Calefacción > Estufas a Parafina', 1],
+            ['calefactores-a-leña', ['SpaceHeater'],
+             'Inicio > Electrohogar > Calefacción > Estufas a Leña', 1],
+            ['calefactores-a-pellet', ['SpaceHeater'],
+             'Inicio > Electrohogar > Calefacción > Estufas a Pellet', 1],
             # ['estufas-infrarrojas', 'SpaceHeater'],
-            ['estufas-a-gas', 'SpaceHeater'],
-            ['estufas-a-parafina', 'SpaceHeater'],
             # ['chimeneas-electricas', 'SpaceHeater'],
             # ['paneles-calefactores', 'SpaceHeater'],
-            # ['calefactores-a-pellet', 'SpaceHeater'],
             # ['termoventiladores', 'SpaceHeater'],
-            ['estufas-electrica', 'SpaceHeater'],
-            ['aire-acondicionado-y-enfriadores-de-aire', 'AirConditioner'],
-            ['parlantes', 'StereoSystem'],
-            # ['audifonos', 'Headphones']
+
+
+            ['aire-acondicionado-y-enfriadores-de-aire', ['AirConditioner'],
+             'Inicio > Electrohogar > Climatización > '
+             'Aire acondicionado y Enfriadores de aire', 1],
+
+            ['parlantes', ['StereoSystem'],
+             'Inicio > Electrohogar > Tecnología > Parlantes', 1],
+            ['audifonos', ['Headphones'],
+             'Inicio > Electrohogar > Tecnología > Audífonos', 1]
         ]
 
         base_prod_url = 'https://www.easy.cl/tienda/producto/{}'
         cat_url = 'https://www.easy.cl/api/cateasy/_search'
         prods_url = 'https://www.easy.cl/api//prodeasy/_search'
-        product_urls = []
+        product_entries = defaultdict(lambda: [])
         session = session_with_proxy(extra_args)
         session.headers['Content-Type'] = 'application/json'
 
-        for category_id, local_category in category_paths:
-            if local_category != category:
+        for e in category_paths:
+            category_path, local_categories, section_name, category_weight = e
+
+            if category not in local_categories:
                 continue
 
             cat_data = {
                 "query": {
                     "term":
-                        {"seo_url.keyword": category_id}
+                        {"seo_url.keyword": category_path}
                 }
             }
 
@@ -87,7 +128,7 @@ class Easy(Store):
             cat_hits = cat_json['hits']['hits']
 
             if not cat_hits:
-                raise Exception('Bad cat id {}'.format(category_id))
+                raise Exception('Bad cat id {}'.format(category_path))
 
             cat_value = cat_hits[0]['_source']['value']
             cat_field = cat_hits[0]['_source']['field'] + ".raw"
@@ -109,13 +150,17 @@ class Easy(Store):
             prods_hits = prods_json['hits']['hits']
 
             if not prods_hits:
-                raise Exception('Empty section {}'.format(category_id))
+                raise Exception('Empty section {}'.format(category_path))
 
-            for prods_hit in prods_hits:
-                product_urls.append(
-                    base_prod_url.format(prods_hit['_source']['url']))
+            for idx, prods_hit in enumerate(prods_hits):
+                product_url = base_prod_url.format(prods_hit['_source']['url'])
+                product_entries[product_url].append({
+                    'category_weight': category_weight,
+                    'section_name': section_name,
+                    'value': idx + 1
+                })
 
-        return product_urls
+        return product_entries
 
     @classmethod
     def products_for_url(cls, url, category=None, extra_args=None):
