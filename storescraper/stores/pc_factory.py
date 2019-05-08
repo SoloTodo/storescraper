ce2280a05c100cef7e825cd0a59101954f334554
david <david@solotodo.com>
2019-05-08 17:41:07 -0400

    ADDED: positions to pc factory

--- storescraper/stores/pc_factory.py
+++ storescraper/stores/pc_factory.py
@@ -1,7 +1,7 @@
 import json
-import urllib
-
 import re
+
+from collections import defaultdict
 from datetime import datetime
 from decimal import Decimal
 
@@ -49,71 +49,155 @@ class PcFactory(Store):
         ]
 
     @classmethod
-    def discover_urls_for_category(cls, category, extra_args=None):
-        url_extensions = [
-            ['735', 'Notebook'],  # Notebooks
-            ['454', 'VideoCard'],  # Tarjetas de video AMD
-            ['378', 'VideoCard'],  # Tarjetas de video NVIDIA
-            ['488', 'Tablet'],  # Tablets
-            ['432', 'Cell'],  # Celulares
-            ['6', 'Camera'],  # Camaras compactas
-            ['620', 'Camera'],  # Camaras semiprofesional
-            ['422', 'ExternalStorageDrive'],  # Discos Externos
-            ['904', 'ExternalStorageDrive'],  # SSD Externos
-            ['218', 'UsbFlashDrive'],
-            ['48', 'MemoryCard'],
-            ['46', 'Projector'],
-            ['438', 'VideoGameConsole'],
-            ['889', 'StereoSystem'],  # Equipos de Música
-            ['890', 'StereoSystem'],  # Audio All in One
-            ['798', 'StereoSystem'],  # Microcomponentes
-            ['700', 'StereoSystem'],  # Minicomponentes
-            ['831', 'StereoSystem'],  # Parlantes PC
-            ['34', 'StereoSystem'],  # Parlantes Portátiles
-            ['797', 'StereoSystem'],  # Barras de Sonido
-            ['475', 'AllInOne'],
-            ['22', 'Mouse'],
-            ['789', 'Television'],
-            ['286', 'OpticalDrive'],
-            ['272', 'Processor'],  # Procesadores
-            ['995', 'Monitor'],  # Monitores
-            ['292', 'Motherboard'],  # Placas madre
-            ['112', 'Ram'],  # Memoria PC
-            ['100', 'Ram'],  # Memoria Notebook
-            ['340', 'StorageDrive'],  # HDD PC
-            ['585', 'SolidStateDrive'],  # SSD
-            ['421', 'StorageDrive'],  # HDD Notebook
-            ['932', 'StorageDrive'],  # Discos de Video Vigilancia
-            ['54', 'PowerSupply'],  # Fuentes de poder
-            ['16', 'ComputerCase'],  # Gabinetes con PSU
-            ['328', 'ComputerCase'],  # Gabinetes sin PSU
-            ['42', 'CpuCooler'],  # Refrigeracion
-            ['262', 'Printer'],  # Impresoras Laser
-            ['36', 'Keyboard'],  # Teclados
-            ['418', 'KeyboardMouseCombo'],  # Combo teclado mouse
-            ['850', 'Headphones'],  # Audifonos
-            ['895', 'Headphones'],  # Audifonos
-            ['860', 'Headphones'],  # Audífonos DJ
-            ['861', 'Headphones'],  # Audífonos Monitoreo
-            ['685', 'Wearable']
+    def discover_entries_for_category(cls, category, extra_args=None):
+        category_paths = [
+            ['735', ['Notebook'],
+             'Inicio > Computadores y Tablets > Notebooks', 1],
+
+            ['454', ['VideoCard'],
+             'Inicio > Componentes/Partes y Piezas > Tarjetas Gráficas > '
+             'Tarjetas Gráficas AMD', 1],
+            ['378', ['VideoCard'],
+             'Inicio > Componentes/Partes y Piezas > Tarjetas Gráficas > '
+             'Tarjetas Gráficas NVIDIA', 1],
+
+            ['994', ['Tablet'],
+             'Inicio > Computadores y Tablets > Tablets', 1],
+
+            ['432', ['Cell'],
+             'Inicio > Celulares > Smartphones', 1],
+
+            ['6', ['Camera'],
+             'Inicio > Fotografía, Video y Drones > Cámaras Compactas', 1],
+            ['620', ['Camera'],
+             'Inicio > Fotografía, Video y Drones > '
+             'Cámaras Semiprofesionales', 1],
+
+            ['422', ['ExternalStorageDrive'],
+             'Inicio > Discos Duros y Almacenamiento > Discos Externos', 1],
+            ['904', ['ExternalStorageDrive'],
+             'Inicio > Discos Duros y Almacenamiento > SSD Externos ', 1],
+            ['218', ['UsbFlashDrive'],
+             'Inicio > Discos Duros y Almacenamiento > Pendrives', 1],
+            ['48', ['MemoryCard'],
+             'Inicio > Discos Duros y Almacenamiento > Memoris Flash', 1],
+            ['46', ['Projector'],
+
+             'Inicio > Monitores y Proyectores > Proyectores', 1],
+            ['438', ['VideoGameConsole'],
+
+             'Inicio > Juegos y Consola > Consolas', 1],
+            ['889', ['StereoSystem'],
+
+             'Inicio > Audio > Equipos de Música', 1],
+            ['890', ['StereoSystem'],
+             'Inicio > Audio > Equipos de Música > Audio All in One', 1],
+            ['798', ['StereoSystem'],
+             'Inicio > Audio > Equipos de Música > Microcomponente', 1],
+            ['700', ['StereoSystem'],
+             'Inicio > Audio > Equipos de Música > Minicomponentes', 1],
+            ['831', ['StereoSystem'],
+             'Inicio > Audio > Parlantes > Parlantes PC', 1],
+            ['34', ['StereoSystem'],
+             'Inicio > Audio > Parlantes > Parlantes Portátiles', 1],
+            ['797', ['StereoSystem'],
+             'Inicio > TV y Smart TV > Video/Audio TV > Barras de Sonido', 1],
+
+            ['475', ['AllInOne'],
+             'Inicio > Computadores y Tablets > Escritorio > All-in-One', 1],
+
+            ['22', ['Mouse'],
+             'Inicio > Computadores y Tablets > Mouses y Teclados > '
+             'Mouses', 1],
+
+            ['789', ['Television'],
+             'Inicio > TV y Smart TV > TV y Smart TV', 1],
+
+            ['286', ['OpticalDrive'],
+             'Inicio > Componentes/Partes y Piezas > Ópticos', 1],
+
+            ['272', ['Processor'],
+             'Inicio > Componentes/Partes y Piezas > Procesadores', 1],
+
+            ['995', ['Monitor'],
+             'Inicio > Monitores y Proyectores > Monitores', 1],
+
+            ['292', ['Motherboard'],
+             'Inicio > Componentes/Partes y Piezas > Placas Madres', 1],
+
+            ['112', ['Ram'],
+             'Inicio > Componentes/Partes y Piezas > Memorias > '
+             'Memorias PC', 1],
+            ['100', ['Ram'],
+             'Inicio > Componentes/Partes y Piezas > Memorias > '
+             'Memorias Notebook', 1],
+
+            ['340', ['StorageDrive'],
+             'Inicio > Discos Duros y Almacenamiento > Discos Duros PC', 1],
+            ['585', ['SolidStateDrive'],
+             'Inicio > Discos Duros y Almacenamiento > Discos SSD', 1],
+            ['421', ['StorageDrive'],
+             'Inicio > Discos Duros y Almacenamiento > Discos Notebooks', 1],
+            ['932', ['StorageDrive'],
+             'Inicio > Discos Duros y Almacenamiento > '
+             'Discos de Video Vigilancia', 1],
+
+            ['54', ['PowerSupply'],
+             'Inicio > Componentes/Partes y Piezas > '
+             'Fuentes de Poder (PSU)', 1],
+
+            ['16', ['ComputerCase'],
+             'Inicio > Componentes/Partes y Piezas > Gabinetes > '
+             'Gabinetes con Fuente', 1],
+            ['328', ['ComputerCase'],
+             'Inicio > Componentes/Partes y Piezas > Gabinetes > '
+             'Gabinetes sin Fuente', 1],
+
+            ['42', ['CpuCooler'],
+             'Inicio > Componentes/Partes y Piezas > Refrigeración', 1],
+
+            ['262', ['Printer'],
+             'Inicio > Impresoras y Suministros > '
+             'Impresoras Hogar y Oficina', 1],
+
+            ['36', ['Keyboard'],
+             'Inicio > Computadores y Tablets > Mouse y Teclados > '
+             'Teclados', 1],
+            ['418', ['KeyboardMouseCombo'],
+             'Inicio > Computadores y Tablets > Mouses y Teclados > '
+             'Combos Teclado/Mouse', 1],
+
+            ['850', ['Headphones'],
+             'Inicio > Audio > Audifonos', 1],
+            ['860', ['Headphones'],
+             'Inicio > Audio > Audifonos > Audifonos DJ', 1],
+            ['861', ['Headphones'],
+             'Inicio > Audio > Audifonos > Audifonos Monitoreo', 1],
+
+            ['685', ['Wearable'],
+             'Inicio > Celulares > Wearables > Smartwatches', 1]
         ]
 
         session = session_with_proxy(extra_args)
         session.get('https://www.pcfactory.cl')
 
-        product_urls = []
-        for categoria, extension_category in url_extensions:
-            if extension_category != category:
+        product_entries = defaultdict(lambda: [])
+
+        for e in category_paths:
+            category_path, local_categories, section_name, category_weight = e
+
+            if category not in local_categories:
                 continue
 
             page = 1
+            current_position = 1
 
             while True:
                 if page > 30:
                     raise Exception('Page overflow')
 
                 category_url = 'https://www.pcfactory.cl/foo?categoria={}' \
-                               '&pagina={}'.format(categoria, page)
+                               '&pagina={}'.format(category_path, page)
                 print(category_url)
                 soup = BeautifulSoup(session.get(category_url).text,
                                      'html.parser')
@@ -134,11 +218,17 @@ class PcFactory(Store):
                     sku = product_cell.find('span', 'txt-id').text.strip()
                     product_url = 'https://www.pcfactory.cl/producto/{}' \
                                   ''.format(sku)
-                    product_urls.append(product_url)
+                    product_entries[product_url].append({
+                        'category_weight': category_weight,
+                        'section_name': section_name,
+                        'value': current_position
+                    })
+
+                    current_position += 1
 
                 page += 1
 
-        return product_urls
+        return product_entries
 
     @classmethod
     def products_for_url(cls, url, category=None, extra_args=None):
