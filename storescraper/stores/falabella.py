1252fff62b60131f74a95598b8802662192db8b9
Vijay Khemlani <vkhemlan@gmail.com>
2019-06-11 16:40:12 -0400

    * Updated Falabella concurrency params

--- storescraper/stores/falabella.py
+++ storescraper/stores/falabella.py
@@ -22,6 +22,9 @@ from storescraper.utils import HeadlessChrome
 
 
 class Falabella(Store):
+    preferred_discover_urls_concurrency = 1
+    preferred_products_for_url_concurrency = 1
+
     @classmethod
     def categories(cls):
         return [
