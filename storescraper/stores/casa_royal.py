078c9348adfd11d8b72736e68b23008c5177b018
david <david@solotodo.com>
2019-07-22 12:17:18 -0400

    UPDATED: Casa Royal scraper

--- storescraper/stores/casa_royal.py
+++ storescraper/stores/casa_royal.py
@@ -12,15 +12,13 @@ class CasaRoyal(Store):
     def categories(cls):
         return [
             'Cell',
+            'Wearable',
             'Mouse',
             'Headphones',
+            'StereoSystem',
             'Keyboard',
-            'KeyboardMouseCombo',
             'Printer',
-            'PowerSupply',
             'SolidStateDrive',
-            'StorageDrive',
-            'ExternalStorageDrive',
             'UsbFlashDrive',
             'MemoryCard',
             'VideoGameConsole',
@@ -29,35 +27,19 @@ class CasaRoyal(Store):
     @classmethod
     def discover_urls_for_category(cls, category, extra_args=None):
         url_extensions = [
-            ['comunicaciones/celulares/', 'Cell'],
-            ['computacion/accesorios-computacion/mouse-gamer/', 'Mouse'],
-            ['computacion/accesorios-computacion/mouse-inalambricos/',
-             'Mouse'],
-            ['computacion/accesorios-computacion/mouse-inalambricos/',
-             'Mouse'],
-            ['categoria-producto/computacion/accesorios-computacion/'
-             'mouse-usb/', 'Mouse'],
-            ['categoria-producto/computacion/accesorios-computacion/'
-             'audifonos-gamer/', 'Headphones'],
-            # ['computacion/accesorios-computacion/audifono-gamer/',
-            #  'Headphones'],
-            ['computacion/accesorios-computacion/teclados-inalambricos/',
-             'Keyboard'],
-            ['computacion/accesorios-computacion/teclados-numericos/',
-             'Keyboard'],
-            ['computacion/accesorios-computacion/teclados-usb/', 'Keyboard'],
-            ['computacion/accesorios-computacion/teclado-gamer/', 'Keyboard'],
-            # ['computacion/accesorios-computacion/teclado-gaming/',
-            # 'Keyboard'],
-            # ['computacion/equipos-impresion/impresoras/', 'Printer'],
-            # ['computacion/equipos-impresion/impresora-multifuncional/',
-            #  'Printer'],
-            # ['computacion/componentes-pc/fuente-poder-atx/', 'PowerSupply'],
-            ['computacion/componentes-pc/discos-ssd/', 'SolidStateDrive'],
-            ['computacion/componentes-pc/discos-ssd/', 'SolidStateDrive'],
-            ['computacion/almacenamiento/', 'MemoryCard'],
-            ['computacion/consolas-y-juegos/consolas-de-videojuegos/',
-             'VideoGameConsole'],
+            ['telefonia/celulares/smartphones', 'Cell'],
+            ['telefonia/celulares/basicos-senior', 'Cell'],
+            ['telefonia/wearables', 'Wearable'],
+            ['computacion/perifericos-y-adaptadores/mouse', 'Mouse'],
+            ['audio/audifonos', 'Headphones'],
+            ['audio/audio-hogar', 'StereoSystem'],
+            ['audio/audio-profesional/bafles', 'StereoSystem'],
+            ['computacion/perifericos-y-adaptadores/teclados', 'Keyboard'],
+            ['computacion/impresoras-y-tintas/impresoras', 'Printer'],
+            ['computacion/almacenamiento/discos-duros', 'SolidStateDrive'],
+            ['computacion/almacenamiento/pendrives', 'UsbFlashDrive'],
+            ['computacion/almacenamiento/tarjetas-de-memoria', 'MemoryCard'],
+            ['computacion/gamers/consolas', 'VideoGameConsole']
         ]
 
         product_urls = []
@@ -67,26 +49,29 @@ class CasaRoyal(Store):
             if local_category != category:
                 continue
 
+            done = False
             page = 1
 
-            while True:
-                if page >= 10:
+            while not done:
+                if page >= 15:
                     raise Exception('Page overflow: ' + category_path)
 
-                url_webpage = 'https://www.casaroyal.cl/categoria-producto/' \
-                              '{}?pag={}'.format(category_path, page)
-                soup = BeautifulSoup(session.get(url_webpage).text,
-                                     'html.parser')
+                category_url = 'https://www.casaroyal.cl/{}?p={}'\
+                    .format(category_path, page)
+                soup = BeautifulSoup(
+                    session.get(category_url).text, 'html.parser')
 
-                link_containers = soup.findAll('article', 'producto')
+                link_containers = soup.findAll('li', 'item')
 
                 if not link_containers:
                     if page == 1:
                         raise Exception('Empty category: ' + category_path)
-                    break
 
                 for link_container in link_containers:
                     product_url = link_container.find('a')['href']
+                    if product_url in product_urls:
+                        done = True
+                        break
                     product_urls.append(product_url)
 
                 page += 1
@@ -95,46 +80,39 @@ class CasaRoyal(Store):
 
     @classmethod
     def products_for_url(cls, url, category=None, extra_args=None):
+        print(url)
         session = session_with_proxy(extra_args)
         soup = BeautifulSoup(session.get(url).text, 'html.parser')
-        sku_tags = soup.findAll('span', 'tags')
 
-        if len(sku_tags) < 2:
-            return []
+        name = soup.find('h1', {'itemprop': 'name'}).text.strip()
+        sku = soup.find('div', 'sku').find('span', 'value').text.strip()
 
-        name = soup.find('h1', 'title-producto').text.strip()
+        stock = -1
 
-        model_text = sku_tags[0].text.strip()
-        if model_text:
-            model = model_text.split(':')[1].strip()
-            name += ' ({})'.format(model)
-        sku = sku_tags[1].text.split(':')[1].strip()
+        price = soup.find('span', 'price').text.strip()
+        price = Decimal(price.replace('$', '').replace('.', ''))
 
-        if soup.find('span', 'label-agotado'):
-            stock = 0
+        description_panels = soup.find('div', 'tabs-panels')\
+            .findAll('div', 'panel')
+
+        description = html_to_markdown(
+            str(description_panels[0]) + '\n' + str(description_panels[1]))
+
+        if 'reacond' in name.lower() or 'reacond' in description.lower():
+            condition = 'https://schema.org/RefurbishedCondition'
         else:
-            stock = -1
+            condition = 'https://schema.org/NewCondition'
 
-        price_text = soup.find('span', 'lightBlue-b')
-        if not price_text:
-            price_text = soup.find('span', 'lightRed-b')
-
-        price = Decimal(remove_words(price_text.text))
-        description = html_to_markdown(str(soup.find('div', 'bgLightGrey')))
         picture_urls = []
 
-        for container in soup.find('div', 'galleria').findAll('a'):
+        for container in soup.find(
+                'div', {'id': 'amasty_gallery'}).findAll('a'):
             try:
                 picture_url = container['data-zoom-image']
                 if picture_url.strip():
                     picture_urls.append(picture_url.strip())
             except KeyError:
                 pass
-
-        if 'reacond' in name.lower() or 'reacond' in description.lower():
-            condition = 'https://schema.org/RefurbishedCondition'
-        else:
-            condition = 'https://schema.org/NewCondition'
 
         p = Product(
             name,
