8b78b2c564d896662565572da60a314322724211
david <david@solotodo.com>
2019-08-23 12:25:37 -0400

    FIXED raenco and redo hp online

--- storescraper/stores/hp_online.py
+++ storescraper/stores/hp_online.py
@@ -22,7 +22,7 @@ class HpOnline(Store):
         category_paths = [
             ['notebooks', 'Notebook'],
             ['impresoras', 'Printer'],
-            ['desktops/desktops-all-in-one', 'AllInOne'],
+            ['desktops', 'AllInOne'],
         ]
 
         session = session_with_proxy(extra_args)
@@ -35,12 +35,13 @@ class HpOnline(Store):
             page = 1
 
             while True:
-                category_url = 'https://www.hponline.cl/c/{}?page={}' \
-                               '&lazy=true'.format(category_path, page)
+                category_url = 'https://store.hp.com/cl-es/default/{}.html?' \
+                               'p={}'.format(category_path, page)
 
                 soup = BeautifulSoup(session.get(category_url).text,
                                      'html.parser')
-                product_cells = soup.findAll('div', 'catalogue-product')
+
+                product_cells = soup.findAll('li', 'product-item')
 
                 if not product_cells:
                     if page == 1:
@@ -48,8 +49,7 @@ class HpOnline(Store):
                     break
 
                 for cell in product_cells:
-                    product_url = 'https://www.hponline.cl' + \
-                                  cell.find('a')['href']
+                    product_url = cell.find('a')['href']
                     product_urls.append(product_url)
 
                 page += 1
@@ -60,42 +60,36 @@ class HpOnline(Store):
     def products_for_url(cls, url, category=None, extra_args=None):
         print(url)
         session = session_with_proxy(extra_args)
-
         response = session.get(url)
 
-        if response.url != url or response.status_code == 404:
-            return []
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-        page_source = response.text
+        name = soup.find('span', {'itemprop': 'name'}).text.strip()
+        sku = soup.find('div', {'itemprop': 'sku'}).text.strip()
+        stock = -1
 
-        pricing_str = re.search(r'hpContainerPushData = ([\S\s]+?);',
-                                page_source).groups()[0]
-        pricing_data = json.loads(pricing_str)['ecommerce'][
-            'detail']['products'][0]
+        price = soup.find('span', {'data-price-type': 'finalPrice'})\
+            .find('span', 'price').text.strip()
+        price = Decimal(price.replace('$', '').replace('.', ''))
 
-        name = pricing_data['name']
-        part_number = pricing_data['id']
-        price = Decimal(pricing_data['price'])
+        description = html_to_markdown(str(soup.find(
+            'div', 'product info detailed').find('div', 'overview')))
 
-        soup = BeautifulSoup(page_source, 'html.parser')
-        sku = soup.find('meta', {'itemprop': 'sku'})['content']
+        script_containers = soup.findAll(
+            'script', {'type': 'text/x-magento-init'})
+        images_json = None
 
-        description = html_to_markdown(
-            str(soup.find('div', 'product-description-container')))
+        for script in script_containers:
+            if 'data-gallery-role' in script.text:
+                images_json = json.loads(script.text)
+                break
 
-        picture_urls = ['https:' + tag.find('img')['data-lazy'] for tag in
-                        soup.findAll('div', {'id': 'image-product'})]
+        picture_urls = []
 
-        availability_container = soup.find('meta',
-                                           {'itemprop': 'availability'})
-
-        if not availability_container:
-            stock = 0
-        elif soup.find('meta', {'itemprop': 'availability'})['href'] == \
-                'http://schema.org/InStock':
-            stock = -1
-        else:
-            stock = 0
+        if images_json:
+            images_data = images_json['[data-gallery-role=gallery-placeholder]']['mage/gallery/gallery']['data']
+            for image_data in images_data:
+                picture_urls.append(image_data['img'])
 
         p = Product(
             name,
@@ -109,7 +103,7 @@ class HpOnline(Store):
             price,
             'CLP',
             sku=sku,
-            part_number=part_number,
+            part_number=sku,
             description=description,
             picture_urls=picture_urls
         )
