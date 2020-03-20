d4fde778eca4cf01686113d6a2332610fe927390
David <david@fml.cl>
2020-03-20 11:44:52 -0300

    Fixed hites banners... again

--- storescraper/stores/hites.py
+++ storescraper/stores/hites.py
@@ -471,8 +471,8 @@ class Hites(Store):
                     #     .find_element_by_class_name('owl-stage-outer')
 
                     controls = driver.find_element_by_class_name(
-                        'carousel__controls__center')\
-                        .find_elements_by_class_name('slider-controls__dots')
+                        'slick-dots')\
+                        .find_elements_by_tag_name('li')
 
                     # controls = driver.find_elements_by_class_name('owl-dot')
 
@@ -485,7 +485,7 @@ class Hites(Store):
                     soup = BeautifulSoup(driver.page_source, 'html.parser')
 
                     images = soup.find('div', 'slick-track')\
-                        .findAll('li', 'slick-slide')
+                        .findAll('div', 'slick-slide')
 
                     # images = soup.find('div', 'owl-stage') \
                     #     .findAll('div', 'owl-item')
