5cd1691ee4d3211714b1c3acb86d366ce6f01125
Vijay Khemlani <vkhemlan@gmail.com>
2020-09-04 07:53:24 -0400

    * Updated FalabellaFast, GrupoDecme, PcExpress

--- storescraper/stores/falabella_fast.py
+++ storescraper/stores/falabella_fast.py
@@ -1,4 +1,5 @@
 import json
+import logging
 from decimal import Decimal
 
 from storescraper.stores import Falabella
@@ -112,7 +113,7 @@ class FalabellaFast(Store):
 
             if 'results' not in res:
                 if page == 1:
-                    raise Exception('Empty category: {}'.format(category_id))
+                    logging.warning('Empty category: {}'.format(category_id))
                 break
 
             for result in res['results']:
