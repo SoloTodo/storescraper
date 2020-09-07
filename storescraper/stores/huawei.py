ff17732b9515e697c2c89be8adf3f43f5c6269fe
sebastian donoso <sdonoso@solotodo.com>
2020-09-07 18:01:22 -0300

    products for url huawei

--- storescraper/stores/huawei.py
+++ storescraper/stores/huawei.py
@@ -1,7 +1,12 @@
+import json
+import re
+from decimal import Decimal
+
 from bs4 import BeautifulSoup
 
+from storescraper.product import Product
 from storescraper.store import Store
-from storescraper.utils import session_with_proxy
+from storescraper.utils import session_with_proxy, remove_words, html_to_markdown
 
 
 class Huawei(Store):
@@ -49,9 +54,66 @@ class Huawei(Store):
                     break
                 for container in product_container:
                     product_url = container.find('a')['href']
+                    # TODO add https: to begin of href
                     product_urls.append(product_url)
                 offset += 1
 
         return product_urls
 
+    @classmethod
+    def products_for_url(cls, url, category=None, extra_args=None):
+        session = session_with_proxy(extra_args)
+        response = session.get(url)
+        if response.status_code == 500:
+            return []
+        soup = BeautifulSoup(response.text, 'html5lib')
+        script_container = soup.findAll('script')
+        json_info, inventory = cls.get_products_info(script_container)
+        products = []
+        products_json = json_info['sbomList']
+        normal_price = Decimal(json_info['totalUnitPrice'])
+        offer_price = normal_price
+        for product in products_json:
+            name = product['name'].replace('&#x2b;', ', ')
+            sku = product['sbomCode']
+            stock = int(inventory[sku])
+            picture_urls = cls.create_pictures_urls(product)
+            p = Product(
+                name,
+                cls.__name__,
+                category,
+                url,
+                url,
+                sku,
+                stock,
+                normal_price,
+                offer_price,
+                'CLP',
+                sku=sku,
+                picture_urls=picture_urls
 
+            )
+            products.append(p)
+
+        return products
+
+    @classmethod
+    def get_products_info(cls, script_container):
+        for s in script_container:
+            product_info = re.search(r'var productInfo = transObjectAttribute\(.*?\)', s.text)
+            inventory_info = re.findall(r'sbomInvInfo\["(.*?)"] = (.*?);', s.text)
+            if product_info and inventory_info:
+                break
+        inventory = {i[0]: i[1] for i in inventory_info}
+
+        json_info = json.loads(product_info.group(0)[40:-2])
+        return json_info, inventory
+
+    @classmethod
+    def create_pictures_urls(cls, info):
+        picture_urls = []
+
+        base_url = 'https://img01.huaweifile.com/sg/ms/cl/pms/product/{}/{}'
+        for picture in info['groupPhotoList']:
+            picture_urls.append(base_url.format(info['gbomCode'], picture['photoName']))
+        return picture_urls
