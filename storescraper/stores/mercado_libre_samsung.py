600d64682b3774c6ca6fbcc6b58c3f55c4d8b5b7
Vijay Khemlani <vkhemlan@gmail.com>
2020-09-08 18:21:41 -0300

    * Updated MercadoLibreSamsung

--- storescraper/stores/mercado_libre_samsung.py
+++ storescraper/stores/mercado_libre_samsung.py
@@ -1,7 +1,7 @@
-from .mercadolibre_chile import MercadoLibreChile
+from .mercado_libre_chile import MercadoLibreChile
 
 
-class MercadolibreSamsung(MercadoLibreChile):
+class MercadoLibreSamsung(MercadoLibreChile):
     store_extension = '_Tienda_samsung'
 
     @classmethod
@@ -24,3 +24,15 @@ class MercadolibreSamsung(MercadoLibreChile):
             ('computacion', 'Tablet'),
             ('instrumentos', 'StereoSystem'),
         ]}
+
+    @classmethod
+    def products_for_url(cls, url, category=None, extra_args=None):
+        # Remove the seller because we consider MecadoLibreSamsung to be a
+        # standalone retailer, in particular because the Samsung WTB system
+        # only displays entries without a seller (not from marketplaces)
+        # and we want to consider MercadoLibreSamsung for that.
+        products = super(cls).products_for_url(
+            url, category=category, extra_args=extra_args)
+        for product in products:
+            product.seller = None
+        return products
