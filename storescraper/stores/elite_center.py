8c303cdaa8a575f61d35e1b4a2bcb53c1320805d
sebastian donoso <sdonoso@solotodo.com>
2020-09-16 14:35:54 -0300

    finished scraper

--- storescraper/stores/elite_center.py
+++ storescraper/stores/elite_center.py
@@ -1,9 +1,15 @@
+import logging
+from decimal import Decimal
+
+from bs4 import BeautifulSoup
+
+from storescraper.product import Product
 from storescraper.store import Store
 from storescraper.categories import HEADPHONES, SOLID_STATE_DRIVE, \
     MOUSE, KEYBOARD, CPU_COOLER, COMPUTER_CASE, \
     POWER_SUPPLY, RAM, MONITOR, MOTHERBOARD, \
     PROCESSOR, VIDEO_CARD, STEREO_SYSTEM, STORAGE_DRIVE
-from storescraper.utils import session_with_proxy
+from storescraper.utils import session_with_proxy, remove_words
 
 
 class EliteCenter(Store):
@@ -30,50 +36,77 @@ class EliteCenter(Store):
     @classmethod
     def discover_urls_for_category(cls, category, extra_args=None):
         url_extensions = [
-            # Stereo System
             ['accesorios-gamer', STEREO_SYSTEM],
-            # Storage Drive
             ['almacenamiento', STORAGE_DRIVE],
-            # Headphones
             ['audifonos-2', HEADPHONES],
-            # Storage Drive
             ['disco-duro-pcs', STORAGE_DRIVE],
-            # Solid State Drive
             ['disco-estado-solido', SOLID_STATE_DRIVE],
-            # CPU Cooler
             ['disipadores', CPU_COOLER],
-            # Power Supply
             ['fuente-de-poder', POWER_SUPPLY],
-            # Computer Case
             ['gabinetes', COMPUTER_CASE],
-            # Ram
             ['memorias-ram', RAM],
-            # Monitor
             ['monitores', MONITOR],
-            # Mouse
             ['mouse-2', MOUSE],
-            # Mother Board
             ['placas-madres', MOTHERBOARD],
-            # Processor
             ['procesadores', PROCESSOR],
-            # Video Card
             ['tarjeta-de-video', VIDEO_CARD],
-            # Video Card
             ['tarjetas-de-video', VIDEO_CARD],
-            # Keyboard
             ['teclados-2', KEYBOARD],
 
         ]
 
         session = session_with_proxy(extra_args)
-        products_urls = []
+        product_urls = []
         for url_extension, local_category in url_extensions:
             if local_category != category:
                 continue
-            offset = 1
+            page = 1
             while True:
-                if offset > 10:
-                    raise Exception('page overflow: ' + url_extensions)
+                if page > 10:
+                    raise Exception('page overflow: ' + url_extension)
 
                 url_webpage = 'https://elitecenter.cl/product-category/{}/' \
-                              '?orderby=popularity&paged={}'.format(url_extension, offset)
+                              '?orderby=popularity&paged={}'.format(
+                    url_extension, page)
+                data = session.get(url_webpage).text
+                soup = BeautifulSoup(data, 'html.parser')
+                product_containers = soup.findAll('div', 'product-small')
+                if not product_containers:
+                    if page == 1:
+                        logging.warning('Empty category: ' + url_extension)
+                    break
+                for container in product_containers:
+                    product_url = container.find('a')['href']
+                    product_urls.append(product_url)
+                page += 1
+        return product_urls
+
+    @classmethod
+    def products_for_url(cls, url, category=None, extra_args=None):
+        session = session_with_proxy(extra_args)
+        response = session.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        name = soup.find('h1', 'product-title').text
+        sku = soup.find('span', 'sku').text
+        stock = int(soup.find('p', 'stock').text.split()[0])
+        normal_price = Decimal(
+            remove_words(soup.find('div', 'product-main').find('bdi').text))
+        offer_price = normal_price
+        picture_urls = [tag['src'].split('?')[0] for tag in
+                        soup.find('div', 'product-gallery').findAll('img')]
+        p = Product(
+            name,
+            cls.__name__,
+            category,
+            url,
+            url,
+            sku,
+            stock,
+            normal_price,
+            offer_price,
+            'CLP',
+            sku=sku,
+            picture_urls=picture_urls
+
+        )
+        return [p]
