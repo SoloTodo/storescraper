a06c4c1a85679531501d7a7ea5ff1d4975cdfb42
sebastian donoso <sdonoso@solotodo.com>
2021-02-03 13:07:27 -0300

    new storescraper

--- storescraper/stores/sevenwin.py
+++ storescraper/stores/sevenwin.py
@@ -1,5 +1,12 @@
+import logging
+from decimal import Decimal
+
+from bs4 import BeautifulSoup
+
 from storescraper.categories import GAMING_CHAIR, HEADPHONES, MOUSE
+from storescraper.product import Product
 from storescraper.store import Store
+from storescraper.utils import session_with_proxy, remove_words
 
 
 class Sevenwin(Store):
@@ -18,3 +25,61 @@ class Sevenwin(Store):
             ['seven-win-audifonos-gamer', HEADPHONES],
             ['seven-win-mouse-gamer', MOUSE]
         ]
+        session = session_with_proxy(extra_args)
+        product_urls = []
+        for url_extension, local_category in url_extensions:
+            if local_category != category:
+                continue
+            page = 1
+            while True:
+                if page > 10:
+                    raise Exception('page overflow: ' + url_extension)
+                url_webpage = 'https://www.sevenwin.cl/{}?page={}'.format(
+                    url_extension, page)
+                data = session.get(url_webpage).text
+                soup = BeautifulSoup(data, 'html.parser')
+                product_containers = soup.find('div', 'container '
+                                                      'category-page') \
+                    .findAll('div', 'col-lg-4')
+                if not product_containers:
+                    if page == 1:
+                        logging.warning('Empty category: ' + url_extension)
+                    break
+                for container in product_containers:
+                    product_url = container.find('a')['href']
+                    product_urls.append(
+                        'https://www.sevenwin.cl' + product_url)
+                page += 1
+        return product_urls
+
+    @classmethod
+    def products_for_url(cls, url, category=None, extra_args=None):
+        print(url)
+        session = session_with_proxy(extra_args)
+        response = session.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        import ipdb
+        name = soup.find('h1', 'page-header').text
+        sku = soup.find('form', 'form-horizontal')['action'].split('/')[-1]
+        stock = -1
+        price = Decimal(remove_words(soup.find('span', 'product-form-price').text))
+        picture_urls = [tag['src'] for tag in soup.find('div',
+                                                        'col-md-3 '
+                                                        'product-page-thumbs '
+                                                        'space mt-3')
+                        .findAll('img')]
+        p = Product(
+            name,
+            cls.__name__,
+            category,
+            url,
+            url,
+            sku,
+            stock,
+            price,
+            price,
+            'CLP',
+            sku=sku,
+            picture_urls=picture_urls
+        )
+        return [p]
