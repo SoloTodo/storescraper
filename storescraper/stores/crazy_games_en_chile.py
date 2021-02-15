dda37e102bbe4df47eacb7d2db71718b1d7a6de6
Vijay Khemlani <vkhemlan@gmail.com>
2021-02-15 16:31:51 -0300

    * Fixed CrazyGamesEnChile

--- storescraper/stores/crazy_games_en_chile.py
+++ storescraper/stores/crazy_games_en_chile.py
@@ -35,7 +35,7 @@ class CrazyGamesenChile(Store):
             ['audifonos', HEADPHONES],
             ['audifonos-ps4-xbox-one-y-switch', HEADPHONES],
             ['audifonos-home-oficce', HEADPHONES],
-            ['accesorios-para-pc-pc-gamer', HEADPHONES],
+            # ['accesorios-para-pc-pc-gamer', HEADPHONES],
             ['gabinetes-gamer', COMPUTER_CASE],
             ['gabinetes-pc', COMPUTER_CASE],
             ['memorias-pendrives-disco-duro', SOLID_STATE_DRIVE],
@@ -55,12 +55,15 @@ class CrazyGamesenChile(Store):
         for url_extension, local_category in url_extensions:
             if local_category != category:
                 continue
+
+            local_product_urls = []
             page = 1
             while True:
                 if page > 10:
                     raise Exception('page overflow: ' + url_extension)
                 url_webpage = 'https://www.crazygamesenchile.com/{}?page={}' \
                     .format(url_extension, page)
+                print(url_webpage)
                 data = session.get(url_webpage).text
                 soup = BeautifulSoup(data, 'html.parser')
                 product_containers = soup.findAll('li', {
@@ -69,13 +72,14 @@ class CrazyGamesenChile(Store):
                     if page == 1:
                         logging.warning('Empty category: ' + url_extension)
                     break
-                if len(product_containers) == len(product_urls):
-                    return product_urls
-                product_containers = product_containers[len(product_urls):len(
+                if len(product_containers) == len(local_product_urls):
+                    break
+                product_containers = product_containers[len(local_product_urls):len(
                     product_containers) + 1]
                 for container in product_containers:
                     product_url = container.find('a')['href']
                     product_urls.append(product_url)
+                    local_product_urls.append(product_url)
                 page += 1
         return product_urls
 
