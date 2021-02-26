38b67bab0e8fb5ac546966772ba957c9baf0d881
Vijay Khemlani <vkhemlan@gmail.com>
2021-02-26 13:26:32 -0300

    * Finished Youtech

--- storescraper/stores/you_tech.py
+++ storescraper/stores/you_tech.py
@@ -1,13 +1,15 @@
 import logging
+from decimal import Decimal
 
 from bs4 import BeautifulSoup
 
 from storescraper.categories import NOTEBOOK, HEADPHONES, KEYBOARD_MOUSE_COMBO, \
     MONITOR, MOUSE, KEYBOARD, POWER_SUPPLY, PROCESSOR, MOTHERBOARD, CPU_COOLER, \
     SOLID_STATE_DRIVE, EXTERNAL_STORAGE_DRIVE, STORAGE_DRIVE, RAM, MEMORY_CARD, \
-    USB_FLASH_DRIVE, CELL, WEARABLE, PRINTER
+    USB_FLASH_DRIVE, CELL, WEARABLE, PRINTER, UPS
+from storescraper.product import Product
 from storescraper.store import Store
-from storescraper.utils import session_with_proxy
+from storescraper.utils import session_with_proxy, remove_words
 
 
 class YouTech(Store):
@@ -32,7 +34,8 @@ class YouTech(Store):
             USB_FLASH_DRIVE,
             CELL,
             WEARABLE,
-            PRINTER
+            PRINTER,
+            UPS,
         ]
 
     @classmethod
@@ -59,9 +62,13 @@ class YouTech(Store):
             ['67833_67927', CELL],
             ['67833_67951', WEARABLE],
             ['67833_67966', WEARABLE],
-            ['67851', PRINTER]
+            ['67851', PRINTER],
+            ['67822_67857', UPS],
         ]
         session = session_with_proxy(extra_args)
+        session.headers['User-Agent'] = 'curl/7.52.1'
+        session.headers['Accept'] = '*/*'
+
         product_urls = []
         for url_extension, local_category in url_extensions:
             if local_category != category:
@@ -74,12 +81,10 @@ class YouTech(Store):
                               '=product/category&path={}&page={}'.\
                     format(url_extension, page)
                 print(url_webpage)
-                data = session.get(url_webpage).text
+                data = session.get(url_webpage, verify=False).text
                 soup = BeautifulSoup(data, 'html.parser')
-                import ipdb
-                ipdb.set_trace()
-                product_containers = soup.findAll()
-                if product_containers:
+                product_containers = soup.findAll('div', 'product-thumb')
+                if not product_containers:
                     if page == 1:
                         logging.warning('Empty category: ' + url_extension)
                     break
@@ -88,3 +93,41 @@ class YouTech(Store):
                     product_urls.append(product_url)
                 page += 1
             return product_urls
+
+    @classmethod
+    def products_for_url(cls, url, category=None, extra_args=None):
+        print(url)
+        session = session_with_proxy(extra_args)
+        response = session.get(url, verify=False)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        name = soup.find('h1', 'product-title').text.strip()
+        key = soup.find('input', {'name': 'product_id'})['value']
+        sku = soup.find('div', text='Código del producto: ').next.next.strip()
+        stock_text = soup.find(
+            'div', text='Disponibilidad: ').next.next.strip()
+        if stock_text == 'En Stock':
+            stock = -1
+        else:
+            stock = 0
+
+        price_container = soup.find('h2', 'special-price')
+        if not price_container:
+            price_container = soup.find('h2', 'product-price')
+        price = Decimal(remove_words(price_container.text))
+        picture_urls = [soup.find('img', {'id': 'zoom'})['data-zoom-image']]
+
+        p = Product(
+            name,
+            cls.__name__,
+            category,
+            url,
+            url,
+            key,
+            stock,
+            price,
+            price,
+            'CLP',
+            sku=sku,
+            picture_urls=picture_urls
+        )
+        return [p]
