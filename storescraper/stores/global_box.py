c419d5908c96566659523c4ee7faf726fafa4c73
sebastian donoso <sdonoso@solotodo.com>
2021-03-01 16:39:24 -0300

    add products for url

--- storescraper/stores/global_box.py
+++ storescraper/stores/global_box.py
@@ -1,4 +1,5 @@
 import logging
+from decimal import Decimal
 
 from bs4 import BeautifulSoup
 
@@ -6,8 +7,9 @@ from storescraper.categories import MOTHERBOARD, POWER_SUPPLY, PROCESSOR, \
     VIDEO_CARD, NOTEBOOK, TABLET, ALL_IN_ONE, RAM, USB_FLASH_DRIVE, \
     EXTERNAL_STORAGE_DRIVE, STORAGE_DRIVE, SOLID_STATE_DRIVE, \
     KEYBOARD_MOUSE_COMBO, MONITOR, PRINTER, CELL, STEREO_SYSTEM, HEADPHONES
+from storescraper.product import Product
 from storescraper.store import Store
-from storescraper.utils import session_with_proxy
+from storescraper.utils import session_with_proxy, remove_words
 
 
 class Globalbox(Store):
@@ -99,3 +101,41 @@ class Globalbox(Store):
 
                 page += 1
         return product_urls
+
+    @classmethod
+    def products_for_url(cls, url, category=None, extra_args=None):
+        print(url)
+        session = session_with_proxy(extra_args)
+        session.headers['user-agent'] = \
+            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 ' \
+            '(KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
+        response = session.get(url, verify=False)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        name = soup.find('h1', {'itemprop': 'name'}).text
+        sku = soup.find('th', text='SKU').next.next.next.text
+        part_number = soup.find('th', text='Part Number').next.next.next.text
+        if soup.find('link', {'itemprop': 'availability'})[
+             'href'] == 'http://schema.org/InStock':
+            stock = -1
+        else:
+            stock = 0
+        price = Decimal(
+            remove_words(soup.find('span', 'regular-price').text.strip()))
+        picture_urls = [tag['src'] for tag in
+                        soup.find('figure').findAll('img')]
+        p = Product(
+            name,
+            cls.__name__,
+            category,
+            url,
+            url,
+            sku,
+            stock,
+            price,
+            price,
+            'CLP',
+            sku=sku,
+            part_number=part_number,
+            picture_urls=picture_urls
+        )
+        return [p]
