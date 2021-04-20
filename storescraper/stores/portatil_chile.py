6a667be57c7e0fe46a8c7c02949d33ad6a602b52
sebastian donoso <sdonoso@solotodo.com>
2021-04-20 17:35:07 -0400

    update portatil chile

--- storescraper/stores/portatil_chile.py
+++ storescraper/stores/portatil_chile.py
@@ -1,8 +1,10 @@
 import json
+import logging
 
 from bs4 import BeautifulSoup
 from decimal import Decimal
 
+from storescraper.categories import NOTEBOOK
 from storescraper.product import Product
 from storescraper.store import Store
 from storescraper.utils import session_with_proxy, remove_words, \
@@ -13,86 +15,64 @@ class PortatilChile(Store):
     @classmethod
     def categories(cls):
         return [
-            'Notebook',
+            NOTEBOOK
         ]
 
     @classmethod
     def discover_urls_for_category(cls, category, extra_args=None):
-        url_base = 'http://portatilchile.com/modules/blocklayered/' \
-                   'blocklayered-ajax.php?id_category_layered={}&p={}'
-
-        category_paths = [
-            ['4', 'Notebook']
+        url_extensions = [
+            NOTEBOOK
         ]
-
-        product_urls = []
         session = session_with_proxy(extra_args)
-
-        for category_path, local_category in category_paths:
+        product_urls = []
+        for local_category in url_extensions:
             if local_category != category:
                 continue
-
             page = 1
-
             while True:
-                print(category_path, page)
-                category_url = url_base.format(category_path, page)
-                print(category_url)
-
-                json_data = json.loads(session.get(category_url,
-                                                   verify=False).text)
-                soup = BeautifulSoup(json_data['productList'], 'html.parser')
-
-                containers = soup.findAll('div', 'item')
-
-                if not containers:
+                if page > 10:
+                    raise Exception('page overflow')
+                url_webpage = 'https://portatilchile.com/4-notebooks?page={}' \
+                              ''.format(page)
+                print(url_webpage)
+                data = session.get(url_webpage).text
+                soup = BeautifulSoup(data, 'html.parser')
+                product_containers = soup.findAll('article',
+                                                  'product-miniature')
+                if not product_containers:
                     if page == 1:
-                        raise Exception('Empty category: ' + category_path)
+                        logging.warning('Empty category')
                     break
-
-                for product in containers:
-                    product_url = product.find('a')['href']
+                for container in product_containers:
+                    product_url = container.find('a')['href']
                     product_urls.append(product_url)
-
                 page += 1
-
         return product_urls
 
     @classmethod
     def products_for_url(cls, url, category=None, extra_args=None):
         print(url)
         session = session_with_proxy(extra_args)
-        response = session.get(url, verify=False)
-
-        if response.status_code == 404:
-            return []
-
-        soup = BeautifulSoup(session.get(url, verify=False).text,
-                             'html.parser')
-
-        name = soup.find('h1', {'itemprop': 'name'}).text.strip()
+        response = session.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        name = soup.find('h1', 'h1 page-title').text
         sku = soup.find('input', {'name': 'id_product'})['value'].strip()
         part_number = soup.find('span', {'itemprop': 'sku'}).text.strip()
-
-        unavailable_container = soup.find(
-            'span', {'id': 'availability_value'}).string
-
-        if unavailable_container:
-            stock = 0
+        stock_container = soup.find('div', 'product-quantities')
+        if stock_container:
+            stock = int(stock_container.find('span')['data-stock'])
         else:
-            stock = -1
-
-        price_container = soup.find('span', {'id': 'our_price_display'})
-
-        price = price_container.string.split('$')[1]
-        price = Decimal(remove_words(price))
-
-        condition = soup.find('link', {'itemprop': 'itemCondition'})['href']
-
-        description = html_to_markdown(
-            str(soup.find('div', 'page-product-box')))
-
-        picture_urls = [tag['href'] for tag in soup.findAll('a', 'fancybox')]
+            stock = 0
+        price = Decimal(remove_words(soup.find('span', 'current-price').text))
+        if soup.find('div', {'id': 'product-images-thumbs'}):
+            picture_urls = [tag['data-image-large-src'] for tag in
+                            soup.find('div',
+                                      {'id': 'product-images-thumbs'}).findAll(
+                                'img')]
+        else:
+            picture_urls = [
+                soup.find('div', 'images-container').find('img')[
+                    'data-image-large-src']]
 
         p = Product(
             name,
@@ -107,9 +87,7 @@ class PortatilChile(Store):
             'CLP',
             sku=sku,
             part_number=part_number,
-            description=description,
             picture_urls=picture_urls,
-            condition=condition
         )
 
         return [p]
