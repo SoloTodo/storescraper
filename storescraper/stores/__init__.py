17f4fd0f9b5d40cf841bd54d109e0f5780323bc9
sebastian donoso <sdonoso@solotodo.com>
2021-08-09 12:34:42 -0400

    new storescraper

--- storescraper/stores/__init__.py
+++ storescraper/stores/__init__.py
@@ -272,6 +272,7 @@ from .r_s_tech import RSTech
 from .sams import Sams
 from .samsung_chile import SamsungChile
 from .samsung_shop import SamsungShop
+from .samurai_store import SamuraiStore
 from .sanborns import Sanborns
 from .saraiva import Saraiva
 from .sc_global import ScGlobal
---
+++ storescraper/stores/samurai_store.py
@@ -0,0 +1,87 @@
+import logging
+from decimal import Decimal
+
+from bs4 import BeautifulSoup
+
+from storescraper.categories import RAM, VIDEO_CARD, SOLID_STATE_DRIVE
+from storescraper.product import Product
+from storescraper.store import Store
+from storescraper.utils import session_with_proxy, remove_words
+
+
+class SamuraiStore(Store):
+    @classmethod
+    def categories(cls):
+        return [
+            RAM,
+            VIDEO_CARD,
+            SOLID_STATE_DRIVE
+        ]
+
+    @classmethod
+    def discover_urls_for_category(cls, category, extra_args=None):
+        url_extensions = [
+            ['ram', RAM],
+            ['tarjetas-graficas', VIDEO_CARD],
+            ['unidades-de-estado-solido', SOLID_STATE_DRIVE]
+        ]
+        session = session_with_proxy(extra_args)
+        product_urls = []
+        for url_extension, local_category in url_extensions:
+            if local_category != category:
+                continue
+            page = 1
+            while True:
+                if page > 10:
+                    raise Exception('page overflow: ' + url_extension)
+                url_webpage = 'https://www.samuraistorejp.cl/' \
+                              'product-category/{}/page/{}/'.format(
+                                url_extension, page)
+                print(url_webpage)
+                response = session.get(url_webpage)
+                soup = BeautifulSoup(response.text, 'html.parser')
+                product_containers = soup.findAll('div', 'product-small')
+                if not product_containers:
+                    if page == 1:
+                        logging.warning('Empty category: ' + url_extension)
+                    break
+                for container in product_containers:
+                    product_url = container.find('a')['href']
+                    product_urls.append(product_url)
+                page += 1
+        return product_urls
+
+    @classmethod
+    def products_for_url(cls, url, category=None, extra_args=None):
+        print(url)
+        session = session_with_proxy(extra_args)
+        response = session.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        name = soup.find('h1', 'product-title').text.strip()
+        sku = soup.find('link', {'rel': 'shortlink'})['href'].split('p=')[-1]
+        if soup.find('p', 'stock out-of-stock'):
+            stock = 0
+        else:
+            stock = int(soup.find('p', 'stock in-stock').text.split()[0])
+        price_container = soup.find('div', 'product-stacked-info').find(
+            'table').findAll('bdi')
+        normal_price = Decimal(remove_words(price_container[0].text))
+        offer_price = Decimal(remove_words(price_container[1].text))
+        picture_urls = [tag.find('a')['href'] for tag in soup.find('div',
+                        'product-gallery').findAll('div',
+                        'woocommerce-product-gallery__image')]
+        p = Product(
+            name,
+            cls.__name__,
+            category,
+            url,
+            url,
+            sku,
+            stock,
+            normal_price,
+            offer_price,
+            'CLP',
+            sku=sku,
+            picture_urls=picture_urls
+        )
+        return [p]
