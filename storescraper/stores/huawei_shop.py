d440653dc11f0f774885d908c2dac096e619e728
sebastian donoso <sdonoso@solotodo.com>
2021-08-23 11:45:56 -0400

    fix huaweishop storescraper

--- storescraper/stores/huawei_shop.py
+++ storescraper/stores/huawei_shop.py
@@ -78,20 +78,30 @@ class HuaweiShop(Store):
                     'DisplayDetailInfo?productId={}&siteCode=CL'.format(
                         product_id)
         product_json = json.loads(session.get(query_url).text)
+
         products_price = json.loads(session.get('https://itrinity-sg.c'
                                                 '.huawei.com/eCommerce'
                                                 '/queryMinPriceAndInv'
-                                                '?productIds={}&'
-                                                'siteCode=CL'.format(
-                                                    product_id)).text
-                                    )['data']['minPriceAndInvList'][0][
+                                                '?productIds={}&siteCode'
+                                                '=CL'.format(product_id)).
+                                    text)['data']['minPriceAndInvList'][0][
             'minPriceByColors']
+        products_id = [product_id['sbomCode'] for product_id in
+                       product_json['data']['sbomList']]
+        products_id = "%2C".join(products_id)
+        json_stock = json.loads(session.get('https://itrinity-sg.c.huawei.'
+                                            'com/eCommerce/querySkuInventory?'
+                                            'skuCodes={}&siteCode=CL'.format(
+                                                products_id)).text)
         products = []
 
         for product in product_json['data']['sbomList']:
             name = product['name']
             sku = product['sbomCode']
-            stock = -1
+            for product_stock in json_stock['data']['inventoryReqVOs']:
+                if product_stock['skuCode'] == sku:
+                    stock = product_stock['inventoryQty']
+                    break
             price_value = product['gbomAttrList'][0]['attrValue']
             price = Decimal([price['unitPrice'] for price in products_price if
                              price_value in price.values()][0])
