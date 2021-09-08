f2b2a0e76faca5e64ed65bf5767f4360eb4633cc
Vijay Khemlani <vkhemlan@gmail.com>
2021-09-08 11:24:18 -0300

    * Updated DePrati

--- storescraper/stores/de_prati.py
+++ storescraper/stores/de_prati.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 from storescraper.categories import TELEVISION
 from storescraper.product import Product
 from storescraper.store import Store
-from storescraper.utils import session_with_proxy
+from storescraper.utils import session_with_proxy, html_to_markdown
 
 
 class DePrati(Store):
@@ -19,15 +19,19 @@ class DePrati(Store):
 
     @classmethod
     def discover_urls_for_category(cls, category, extra_args=None):
-        url_extensions = [
-            TELEVISION
-        ]
+        if category != TELEVISION:
+            return []
+
         session = session_with_proxy(extra_args)
         product_urls = []
-        for local_category in url_extensions:
-            if local_category != category:
-                continue
-            url_webpage = 'https://www.deprati.com.ec/search?q=LG'
+
+        page = 0
+        while True:
+            if page >= 10:
+                raise Exception('Page overflow')
+
+            url_webpage = 'https://www.deprati.com.ec/search?q=LG&page={}'\
+                .format(page)
             print(url_webpage)
             response = session.get(url_webpage)
             soup = BeautifulSoup(response.text, 'html.parser')
@@ -35,12 +39,17 @@ class DePrati(Store):
                 json.loads(
                     soup.find('div', {'id': 'vueApp'})['searchpagedata'])[
                     'results']
+
             if not product_containers:
-                logging.warning('Empty category')
+                if page == 1:
+                    logging.warning('Empty category')
                 break
+
             for container in product_containers:
                 product_url = container['url']
                 product_urls.append('https://www.deprati.com.ec' + product_url)
+
+            page += 1
         return product_urls
 
     @classmethod
@@ -53,11 +62,12 @@ class DePrati(Store):
             soup.find('input', {'name': 'producthidden'})['value'])
         name = product_json['name']
         sku = product_json['code']
-        stock = -1
+        description = html_to_markdown(product_json['description'])
+        stock = product_json['stock']['stockLevel']
         price = Decimal(
             product_json['price']['formattedValue'].replace('$', '').replace(
                 '.', '').replace(',', '.'))
-        picture_urls = [product_json['images'][0]['url']]
+        picture_urls = [x['url'] for x in product_json['images']]
         p = Product(
             name,
             cls.__name__,
@@ -70,6 +80,7 @@ class DePrati(Store):
             price,
             'USD',
             sku=sku,
-            picture_urls=picture_urls
+            picture_urls=picture_urls,
+            description=description,
         )
         return [p]
