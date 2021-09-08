86e002ef61fc4f785f4069993cd6e4d9b8a82e1d
sebastian donoso <sdonoso@solotodo.com>
2021-09-08 12:23:34 -0300

    new storescraper eljuri storescraper

---
+++ storescraper/stores/el_juri_store.py
@@ -0,0 +1,85 @@
+import logging
+from decimal import Decimal
+
+from bs4 import BeautifulSoup
+
+from storescraper.categories import TELEVISION
+from storescraper.product import Product
+from storescraper.store import Store
+from storescraper.utils import session_with_proxy
+
+
+class ElJuriStore(Store):
+    @classmethod
+    def categories(cls):
+        return [
+            TELEVISION
+        ]
+
+    @classmethod
+    def discover_urls_for_category(cls, category, extra_args=None):
+        url_extensions = [
+            TELEVISION
+        ]
+
+        session = session_with_proxy(extra_args)
+        product_urls = []
+        for local_category in url_extensions:
+            if local_category != category:
+                continue
+            page = 1
+            while True:
+                if page > 10:
+                    raise Exception('page overflow')
+
+                url_webpage = 'https://eljuri.store/index.php?fc=module&' \
+                              'module=leoproductsearch&controller=product' \
+                              'search&search_query=LG&page={}'.format(page)
+                print(url_webpage)
+                response = session.get(url_webpage)
+                soup = BeautifulSoup(response.text, 'html.parser')
+                product_containers = soup.findAll('div', 'ajax_block_product')
+
+                if not product_containers:
+                    if page == 1:
+                        logging.warning('Empty category')
+                    break
+                for container in product_containers:
+                    product_url = container.find('a')['href']
+                    product_urls.append(product_url)
+                page += 1
+        return product_urls
+
+    @classmethod
+    def products_for_url(cls, url, category=None, extra_args=None):
+        print(url)
+        session = session_with_proxy(extra_args)
+        session.headers['user-agent'] = \
+            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 ' \
+            '(KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
+        response = session.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        # import ipdb
+        # ipdb.set_trace()
+        name = soup.find('h1', {'itemprop': 'name'}).text
+        sku = soup.find('input', {'name': 'id_product'})['value']
+        stock = -1
+        price = Decimal(soup.find('span', {'itemprop': 'price'})['content'])
+        picture_urls = [tag['src'] for tag in
+                        soup.find('div', 'product-images').findAll('img')]
+        p = Product(
+            name,
+            cls.__name__,
+            category,
+            url,
+            url,
+            sku,
+            stock,
+            price,
+            price,
+            'USD',
+            sku=sku,
+            picture_urls=picture_urls,
+
+        )
+        return [p]
