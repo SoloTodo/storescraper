64fe5d13d3c077ecadeff46be7036730d9229916
Vijay Khemlani <vkhemlan@gmail.com>
2021-09-08 15:02:40 -0300

    * Updated ElJuriStore

--- storescraper/stores/el_juri_store.py
+++ storescraper/stores/el_juri_store.py
@@ -18,36 +18,35 @@ class ElJuriStore(Store):
 
     @classmethod
     def discover_urls_for_category(cls, category, extra_args=None):
-        url_extensions = [
-            TELEVISION
-        ]
+        if category != TELEVISION:
+            return []
 
         session = session_with_proxy(extra_args)
+        session.headers['user-agent'] = \
+            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 ' \
+            '(KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
         product_urls = []
-        for local_category in url_extensions:
-            if local_category != category:
-                continue
-            page = 1
-            while True:
-                if page > 10:
-                    raise Exception('page overflow')
+        page = 1
+        while True:
+            if page > 10:
+                raise Exception('page overflow')
 
-                url_webpage = 'https://eljuri.store/index.php?fc=module&' \
-                              'module=leoproductsearch&controller=product' \
-                              'search&search_query=LG&page={}'.format(page)
-                print(url_webpage)
-                response = session.get(url_webpage)
-                soup = BeautifulSoup(response.text, 'html.parser')
-                product_containers = soup.findAll('div', 'ajax_block_product')
+            url_webpage = 'https://eljuri.store/index.php?fc=module&' \
+                          'module=leoproductsearch&controller=product' \
+                          'search&search_query=LG&page={}'.format(page)
+            print(url_webpage)
+            response = session.get(url_webpage)
+            soup = BeautifulSoup(response.text, 'html.parser')
+            product_containers = soup.findAll('div', 'ajax_block_product')
 
-                if not product_containers:
-                    if page == 1:
-                        logging.warning('Empty category')
-                    break
-                for container in product_containers:
-                    product_url = container.find('a')['href']
-                    product_urls.append(product_url)
-                page += 1
+            if not product_containers:
+                if page == 1:
+                    logging.warning('Empty category')
+                break
+            for container in product_containers:
+                product_url = container.find('a')['href']
+                product_urls.append(product_url)
+            page += 1
         return product_urls
 
     @classmethod
@@ -59,11 +58,10 @@ class ElJuriStore(Store):
             '(KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
         response = session.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
-        # import ipdb
-        # ipdb.set_trace()
         name = soup.find('h1', {'itemprop': 'name'}).text
         sku = soup.find('input', {'name': 'id_product'})['value']
-        stock = -1
+        stock_tag_container = soup.find('div', 'product-quantities')
+        stock = int(stock_tag_container.find('span')['data-stock'])
         price = Decimal(soup.find('span', {'itemprop': 'price'})['content'])
         picture_urls = [tag['src'] for tag in
                         soup.find('div', 'product-images').findAll('img')]
