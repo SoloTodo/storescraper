71d531a3591b54da3c2431bbd8ebd23a47a3fa19
sebastian donoso <sdonoso@solotodo.com>
2021-09-16 13:54:15 -0300

    mercado libre chile

--- storescraper/stores/mercado_libre_chile.py
+++ storescraper/stores/mercado_libre_chile.py
@@ -7,8 +7,11 @@ import demjson
 import re
 from decimal import Decimal
 
+import ipdb
 from bs4 import BeautifulSoup
 
+from storescraper.categories import NOTEBOOK, MONITOR, SOLID_STATE_DRIVE, \
+    WEARABLE, PRINTER, MOUSE, ALL_IN_ONE, KEYBOARD_MOUSE_COMBO, HEADPHONES
 from storescraper.product import Product
 from storescraper.store import Store
 from storescraper.utils import session_with_proxy
@@ -17,59 +20,39 @@ from storescraper.utils import session_with_proxy
 class MercadoLibreChile(Store):
     @classmethod
     def categories(cls):
-        categories = set()
-        stores_paths = cls._category_paths().values()
-        for store_paths in stores_paths:
-            for store_path in store_paths:
-                categories.add(store_path[1])
-        return list(categories)
+        return [
+            NOTEBOOK,
+            MONITOR
+        ]
 
     @classmethod
     def discover_urls_for_category(cls, category, extra_args=None):
         session = session_with_proxy(extra_args)
         product_urls = []
 
-        for store_extension, store_paths in cls._category_paths().items():
-            for category_path, local_category in store_paths:
-                if local_category != category:
-                    continue
+        for store_extension, local_category in cls._category_paths().items():
+            if local_category != category:
+                continue
+            print(store_extension)
+            url_webpage = 'https://listado.mercadolibre.cl/{}'.format(
+                store_extension)
+            print(url_webpage)
+            response = session.get(url_webpage)
+            soup = BeautifulSoup(response.text, 'html.parser')
+            product_containers = soup.findAll('li', 'ui-search-layout__item')
+            if not product_containers:
+                logging.warning('Empty category: ' + store_extension)
+                break
 
-                category_url = 'https://listado.mercadolibre.cl/{}/' \
-                               '{}'.format(category_path, store_extension)
-                response = session.get(category_url)
-
-                if response.url != category_url:
-                    logging.warning('Invalid and/or empty category: ' +
-                                    response.url)
-                    continue
-
-                soup = BeautifulSoup(response.text, 'html.parser')
-                title_tag = soup.find('title')
-
-                if not title_tag or 'oficial' not in title_tag.text.lower():
-                    logging.warning('Non official store: ' + response.url)
-                    continue
-
-                if soup.find('div', 'zrp-offical-message'):
-                    raise Exception('Invalid category: ' + category_url)
-
-                containers = soup.findAll('li', 'results-item')
-
-                if not containers:
-                    containers = soup.findAll('li', 'ui-search-layout__item')
-
-                if not containers:
-                    logging.warning('Empty category: ' + category_url)
-
-                for container in containers:
-                    product_url = container.find('a')['href'].split(
-                        '?')[0].split('#')[0]
-                    product_urls.append(product_url)
+            for container in product_containers:
+                product_url = container.find('a')['href']
+                product_urls.append(product_url)
 
         return product_urls
 
     @classmethod
     def products_for_url(cls, url, category=None, extra_args=None):
+        category = NOTEBOOK
         print(url)
         session = session_with_proxy(extra_args)
         page_source = session.get(url).text
@@ -77,16 +60,26 @@ class MercadoLibreChile(Store):
 
         new_mode_data = re.search(
             r'window.__PRELOADED_STATE__ =([\S\s]+?);\n', page_source)
-
         if new_mode_data:
             data = json.loads(new_mode_data.groups()[0])
+            # import ipdb
+            # ipdb.set_trace()
 
             for entry in data['initialState']['components'].get('head', []):
-                if entry['id'] == 'item_status_message' and 'PAUSADA' in entry['body']['text'].upper():
+                if entry['id'] == 'item_status_message' and 'PAUSADA' in \
+                        entry['body']['text'].upper():
                     return []
+            key_to_categories = cls.category_paths()
+            for possibly_category in data['initialState']['schema'][1][
+                'itemListElement']:
+                category_name = possibly_category['item']['name']
+                # print(category_name)
+                if category_name in key_to_categories:
+                    category = key_to_categories[category_name]
+                    break
 
             if 'component_id' in data['initialState']['components'][
-                    'variations']:
+                'variations']:
                 return cls.retrieve_type2_products(session, url, soup,
                                                    category, data)
             else:
@@ -116,7 +109,6 @@ class MercadoLibreChile(Store):
             endpoint = 'https://www.mercadolibre.cl/p/api/products/' + \
                        variation
             variation_data = json.loads(session.get(endpoint).text)
-
             if variation_data['schema'][0]['offers']['availability'] == \
                     'https://schema.org/OutOfStock':
                 # No price information in this case, so skip it
@@ -187,7 +179,8 @@ class MercadoLibreChile(Store):
                 name = '{} ({})'.format(base_name, color_name)
                 color_id = variation['attribute_id']
 
-                variation_url = '{}?attributes={}:{}'.format(url, picker_id, color_id)
+                variation_url = '{}?attributes={}:{}'.format(url, picker_id,
+                                                             color_id)
                 res = session.get(variation_url)
                 key_match = re.search(r'variation=(\d+)', res.url)
 
@@ -309,148 +302,23 @@ class MercadoLibreChile(Store):
     @classmethod
     def _category_paths(cls):
         return {
-            '_Tienda_acer': [
-                ('computacion/notebooks', 'Notebook'),
-                ('almacenamiento', 'ExternalStorageDrive'),
-                ('tablets-accesorios', 'Tablet'),
-                ('monitores-accesorios', 'Monitor'),
-            ],
-            '_Tienda_cintegral': [
-                ('notebooks-accesorios', 'Notebook'),
-                ('pc-escritorio', 'AllInOne'),
-                ('almacenamiento', 'SolidStateDrive'),
-            ],
-            '_Tienda_corsair': [
-                ('notebooks-accesorios', 'CpuCooler'),
-                ('computacion/perifericos-accesorios/teclados', 'Keyboard'),
-                ('computacion/perifericos-accesorios/mouses', 'Mouse'),
-                ('componentes-pc', 'PowerSupply'),
-            ],
-            '_Tienda_cougar': [
-                ('computacion/perifericos-accesorios/teclados', 'Keyboard'),
-                ('computacion/perifericos-accesorios/mouses', 'Mouse'),
-                ('webcams-audio-pc', 'Headphones'),
-                ('electronica', 'Headphones'),
-            ],
-            '_Tienda_hp': [
-                ('computacion/notebooks', 'Notebook'),
-                ('pc-escritorio', 'AllInOne'),
-                ('computacion/impresoras/impresoras', 'Printer'),
-                ('computacion/perifericos-accesorios/teclados', 'Keyboard'),
-                ('almacenamiento', 'AllInOne'),
-                ('monitores-accesorios', 'Monitor'),
-            ],
-            '_Tienda_huawei': [
-                ('celulares-telefonia/celulares', 'Cell'),
-                ('smartwatches-accesorios', 'Wearable'),
-                ('notebooks-accesorios', 'Notebook'),
-                ('tablets-accesorios', 'Tablet'),
-                ('audio-audifonos', 'Headphones'),
-                ('electronica/audio-hogar/parlantes-subwoofers',
-                 'Headphones'),
-            ],
-            '_Tienda_hyperx': [
-                ('computacion/perifericos-accesorios/teclados', 'Keyboard'),
-                ('computacion/perifericos-accesorios/mouses', 'Mouse'),
-                ('webcams-audio-pc', 'Headphones'),
-            ],
-            '_Tienda_lenovo': [
-                ('pc-escritorio-all-in-one', 'AllInOne'),
-            ],
-            '_Tienda_logitech': [
-                ('computacion/perifericos-accesorios/teclados', 'Keyboard'),
-                ('computacion/perifericos-accesorios/mouses', 'Mouse'),
-                ('mouses-teclados-controles-kits-mouse-teclado',
-                 'KeyboardMouseCombo'),
-                ('webcams-audio-pc', 'StereoSystem'),
-            ],
-            '_Tienda_logitech-g': [
-                ('computacion/perifericos-accesorios/teclados', 'Keyboard'),
-                ('computacion/perifericos-accesorios/mouses', 'Mouse'),
-                ('webcams-audio-pc-audifonos', 'Headphones'),
-                ('electronica', 'Headphones'),
-                ('webcams-audio-pc-parlantes', 'StereoSystem'),
-            ],
-            '_Tienda_marvo': [
-                ('computacion/perifericos-accesorios/teclados', 'Keyboard'),
-                ('computacion/perifericos-accesorios/mouses', 'Mouse'),
-                ('webcams-audio-pc-audifonos', 'Headphones'),
-                ('webcams-audio-pc-parlantes', 'StereoSystem'),
-            ],
-            '_Tienda_ozone': [
-                ('computacion/perifericos-accesorios/teclados', 'Keyboard'),
-                ('computacion/perifericos-accesorios/mouses', 'Mouse'),
-                ('webcams-audio-pc', 'Headphones'),
-                ('videojuegos', 'Headphones'),
-            ],
-            '_Tienda_pc-factory_OfficialStoreId_810': [
-                ('celulares-telefonia', 'Cell'),
-                ('computacion', 'Notebook'),
-            ],
-            '_Tienda_primus': [
-                ('computacion/perifericos-accesorios/teclados', 'Keyboard'),
-                ('computacion/perifericos-accesorios/mouses', 'Mouse'),
-            ],
-            '_Tienda_razer': [
-                ('computacion/perifericos-accesorios/teclados', 'Keyboard'),
-                ('computacion/perifericos-accesorios/mouses', 'Mouse'),
-                ('webcams-audio-pc', 'Headphones'),
-            ],
-            '_Tienda_redragon': [
-                ('mouses-teclados-controles', 'Keyboard'),
-            ],
-            '_Tienda_seagate': [
-                ('almacenamiento-discos-accesorios-duros-ssds/externo',
-                 'ExternalStorageDrive'),
-                ('almacenamiento-discos-accesorios-duros-ssds/interno',
-                 'StorageDrive'),
-            ],
-            '_Tienda_trust': [
-                ('computacion/perifericos-accesorios/teclados', 'Keyboard'),
-                ('computacion/perifericos-accesorios/mouses', 'Mouse'),
-            ],
-            '_Tienda_western-digital': [
-                ('almacenamiento-discos-duros-removibles-accesorios',
-                 'SolidStateDrive'),
-                ('almacenamiento-discos-accesorios-duros-ssds/externo',
-                 'ExternalStorageDrive'),
-                ('almacenamiento-discos-accesorios-duros-ssds/interno',
-                 'StorageDrive'),
-            ],
-            '_Tienda_xpg': [
-                ('computacion/perifericos-accesorios/teclados', 'Keyboard'),
-                ('computacion/perifericos-accesorios/mouses', 'Mouse'),
-                ('webcams-audio-pc', 'Headphones'),
-            ],
-            '_Tienda_xtech': [
-                ('computacion/perifericos-accesorios/teclados', 'Keyboard'),
-                ('computacion/perifericos-accesorios/mouses', 'Mouse'),
-                ('componentes-pc', 'PowerSupply'),
-                ('audio-audifonos', 'Headphones'),
-            ],
-            '_Tienda_blu': [
-                ('celulares-telefonia/celulares', 'Cell'),
-            ],
-            '_Tienda_kingston': [
-                ('celulares-telefonia', 'MemoryCard'),
-                ('almacenamiento-discos-accesorios', 'SolidStateDrive'),
-                ('almacenamiento-pen-drives', 'UsbFlashDrive'),
-            ],
-            '_Tienda_motorola': [
-                ('audio', 'Headphones'),
-                ('celulares-telefonia/celulares', 'Cell'),
-                ('celulares-telefonia/accesorios-celulares', 'StereoSystem'),
-            ],
-            '_Tienda_nokia': [
-                ('celulares-telefonia/celulares', 'Cell'),
-            ],
-            '_Tienda_daewoo': [
-                ('refrigeracion', 'Refrigerator'),
-                ('lavado-y-secado-de-ropa', 'WashingMachine'),
-            ],
-            '_Tienda_xiaomi': [
-                ('celulares-telefonia/celulares', 'Cell'),
-                ('smartwatches-accesorios-smartwatch', 'Wearable'),
-                ('audio', 'Headphones'),
-            ]
+            '_Tienda_acer':NOTEBOOK,
+            '_Tienda_hp': NOTEBOOK,
+
+        }
+
+    @classmethod
+    def category_paths(cls):
+        return {
+            'MONITORES': MONITOR,
+            'Discos Duros y SSDs': SOLID_STATE_DRIVE,
+            'Notebooks': NOTEBOOK,
+            'Smartbands': WEARABLE,
+            'Impresoras': PRINTER,
+            'Mouses': MOUSE,
+            'All In One': ALL_IN_ONE,
+            'Kits de Mouse y Teclado': KEYBOARD_MOUSE_COMBO,
+            'Audífonos':HEADPHONES,
+
+
         }
