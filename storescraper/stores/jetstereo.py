23ec464ab711c7ca8cba4822572f3cc442348960
sebastian donoso <sdonoso@solotodo.com>
2021-10-07 16:34:15 -0300

    fix jetstereo scraper

--- storescraper/stores/jetstereo.py
+++ storescraper/stores/jetstereo.py
@@ -9,7 +9,8 @@ from storescraper.product import Product
 from storescraper.store import Store
 from storescraper.utils import session_with_proxy
 from storescraper.categories import TELEVISION, STEREO_SYSTEM, CELL, \
-    REFRIGERATOR, OVEN, WASHING_MACHINE, STOVE, CELL_ACCESORY
+    REFRIGERATOR, OVEN, WASHING_MACHINE, STOVE, CELL_ACCESORY, HEADPHONES, \
+    MONITOR, AIR_CONDITIONER, WEARABLE, VACUUM_CLEANER
 
 
 class Jetstereo(Store):
@@ -18,56 +19,66 @@ class Jetstereo(Store):
     @classmethod
     def categories(cls):
         return [
-            TELEVISION,
             STEREO_SYSTEM,
+            TELEVISION,
             CELL,
             REFRIGERATOR,
+            STOVE,
             OVEN,
             WASHING_MACHINE,
-            STOVE,
             CELL_ACCESORY,
+            HEADPHONES,
+            MONITOR,
+            AIR_CONDITIONER,
+            WEARABLE,
+            VACUUM_CLEANER,
         ]
 
     @classmethod
     def discover_urls_for_category(cls, category, extra_args=None):
-        url_extensions = [
-            ['48', STEREO_SYSTEM, ],
-            ['41', TELEVISION],
-            ['26', CELL],
-            ['74', REFRIGERATOR],
-            ['75', STOVE],
-            ['76', OVEN],
-            ['77', WASHING_MACHINE],
-            ['63', CELL_ACCESORY]
-        ]
+        category_path = {
+            STEREO_SYSTEM: ['48', '212'],
+            TELEVISION: ['41'],
+            CELL: ['26'],
+            REFRIGERATOR: ['74', '159', '195', '160', '161'],
+            STOVE: ['75', '163', '164'],
+            OVEN: ['76'],
+            WASHING_MACHINE: ['77', '157', '78', '249', '158', '156'],
+            CELL_ACCESORY: ['63'],
+            HEADPHONES: ['132'],
+            MONITOR: ['91'],
+            AIR_CONDITIONER: ['79'],
+            WEARABLE: ['89'],
+            VACUUM_CLEANER: ['152'],
+        }
         session = session_with_proxy(extra_args)
         product_urls = []
-        for url_extension, local_category in url_extensions:
-            if local_category != category:
-                continue
-            page = 1
-            while True:
-                if page > 20:
-                    raise Exception('Page overflow: ' + url_extension)
-                url = 'https://api.jetstereo.com/manufacturers/2/' \
-                      'categories/{}/products?per-page=20&page={}'.format(
-                        url_extension, page)
-                print(url)
-                response = session.get(url)
-                product_containers = json.loads(response.text)['content']
+        page = 1
+        while True:
+            if page > 20:
+                raise Exception('Page overflow')
+            url = 'https://api.jetstereo.com/manufacturers/2/products?' \
+                  'per-page=20&page={}'.format(page)
+            print(url)
+            response = session.get(url)
+            product_containers = json.loads(response.text)
+            if page > product_containers['_meta']['pageCount']:
+                break
+            product_containers = product_containers['content']
 
-                if not product_containers:
-                    if page == 1:
-                        logging.warning('Empty category: ' + url)
-                    break
+            if not product_containers:
+                if page == 1:
+                    logging.warning('Empty category: ' + url)
+                break
 
-                for container in product_containers:
-                    product_url = 'https://www.jetstereo.com/product/' + \
-                                  container['url'].split('.com/')[-1]
-                    if product_url in product_urls:
-                        return product_urls
-                    product_urls.append(product_url)
-                page += 1
+            for container in product_containers:
+                if container['mainCategory']['id'] not in \
+                        category_path[category]:
+                    continue
+                product_url = 'https://www.jetstereo.com/product/' + \
+                              container['url'].split('.com/')[-1]
+                product_urls.append(product_url)
+            page += 1
         return product_urls
 
     @classmethod
