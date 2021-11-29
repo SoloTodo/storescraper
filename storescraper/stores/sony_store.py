6a26424d20517f501c16608f411249f6cd09c227
Sebastián Donoso <sebastian.a.donoso.b@gmail.com>
2021-11-29 17:00:13 -0300

    fix sony

--- storescraper/stores/sony_store.py
+++ storescraper/stores/sony_store.py
@@ -1,5 +1,7 @@
 import json
 import logging
+import re
+
 from bs4 import BeautifulSoup
 from decimal import Decimal
 
@@ -45,14 +47,17 @@ class SonyStore(Store):
 
             response = session.get(url_webpage)
             soup = BeautifulSoup(response.text, 'html.parser')
-            product_containers = soup.findAll('div', 'vtex-search-result'
-                                                     '-3-x-galleryItem')
-
+            r = re.compile(r'Product:sp-(\d+$)')
+            product_containers = json.loads(
+                soup.find('template', {'data-varname': '__STATE__'}).find(
+                    'script').text)
+            product_container_keys = product_containers.keys()
+            products_to_find = list(filter(r.match, product_container_keys))
             if not product_containers:
                 logging.warning('Empty category: ' + url_extension)
 
-            for container in product_containers:
-                product_url = container.find('a')['href']
+            for product_key in products_to_find:
+                product_url = product_containers[product_key]['link']
                 product_urls.append('https://store.sony.cl' + product_url)
 
         return product_urls
@@ -63,77 +68,39 @@ class SonyStore(Store):
         session = session_with_proxy(extra_args)
         response = session.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
-        json_product = json.loads(
-            soup.find('script', {'type': 'application/ld+json'}).text)
-        if len(json_product['offers']['offers']) > 1:
-            products = []
-            api_url = 'https://store.sony.cl/api/catalog_system/pub/products' \
-                      '/search?fq=productId:{}'.format(json_product['mpn'])
-            api_response = session.get(api_url)
-            variations = json.loads(api_response.text)[0]['items']
-            for product in variations:
-                variation_name = json_product['name'] + ' - ' + \
-                                 product['Color'][0]
-                part_number = product['name'].replace('|', '').strip()
-                sku = product['itemId']
-                if product['sellers'][0]['commertialOffer'][
-                            'AvailableQuantity'] > 10:
-                    stock = 10
-                else:
-                    stock = product['sellers'][0]['commertialOffer'][
-                        'AvailableQuantity']
-                price = Decimal(
-                    product['sellers'][0]['commertialOffer']['Price'])
-                picture_urls = [picture['imageUrl'].split('?v')[0] for picture
-                                in product['images']]
-                p = Product(
-                    variation_name,
-                    cls.__name__,
-                    category,
-                    url,
-                    url,
-                    sku,
-                    stock,
-                    price,
-                    price,
-                    'CLP',
-                    sku=sku,
-                    part_number=part_number,
-                    picture_urls=picture_urls
-                )
-                products.append(p)
-            return products
+        json_container = list(json.loads(
+                soup.find('template', {'data-varname': '__STATE__'}).find(
+                    'script').text).values())[0]
+        api_url = 'https://store.sony.cl/api/catalog_system/pub/products' \
+                  '/search?fq=productId:{}'.format(json_container['productId'])
+        api_response = session.get(api_url)
+        json_product = json.loads(api_response.text)[0]['items'][0]
+        name = json_product['name']
+        part_number = json_product['name'].replace('|', '').strip()
+        sku = json_product['itemId']
+        if json_product['sellers'][0]['commertialOffer']['AvailableQuantity'] \
+                > 10:
+            stock = 10
         else:
-
-            name = json_product['name'].replace('\xa0', ' ')
-            part_number = soup.find('p', 'pasonyb2cqa-shelf'
-                                         '-custom-0-x-labelSkuName').text.\
-                replace('|', '').strip()
-            sku = json_product['sku']
-            if soup.find('div', 'pasonyb2cqa-product-custom-0-x-btnSinStock'):
-                stock = 0
-            else:
-                stock = int(soup.find('div', 'pasonyb2cqa-product-custom-0-x-'
-                                             'labelStock').text.split()[2])
-            price = Decimal(json_product['offers']['offers'][0]['price'])
-            picture_urls = [tag['src'].split('-')[0] for tag in
-                            soup.findAll('img',
-                                         'vtex-store-components-3-x-thumbImg')]
-
-            p = Product(
-                name,
-                cls.__name__,
-                category,
-                url,
-                url,
-                sku,
-                stock,
-                price,
-                price,
-                'CLP',
-                sku=sku,
-                part_number=part_number,
-                picture_urls=picture_urls
-            )
-
-            return [p]
+            stock = json_product['sellers'][0]['commertialOffer'][
+                'AvailableQuantity']
+        price = Decimal(
+            json_product['sellers'][0]['commertialOffer']['Price'])
+        picture_urls = [picture['imageUrl'].split('?v')[0] for picture
+                        in json_product['images']]
+        p = Product(
+            name,
+            cls.__name__,
+            category,
+            url,
+            url,
+            sku,
+            stock,
+            price,
+            price,
+            'CLP',
+            sku=sku,
+            part_number=part_number,
+            picture_urls=picture_urls
+        )
+        return [p]
