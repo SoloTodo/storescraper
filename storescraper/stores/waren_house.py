638083913ea7d9e1a8a8efd9c7637284d3b463d0
sebastian donoso <sdonoso@solotodo.com>
2021-12-06 17:29:16 -0300

    fix warenhouse and changes on mercadolibre chile

--- storescraper/stores/waren_house.py
+++ storescraper/stores/waren_house.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import re
 from decimal import Decimal
 
 from bs4 import BeautifulSoup
@@ -22,9 +23,9 @@ class WarenHouse(Store):
     @classmethod
     def discover_urls_for_category(cls, category, extra_args=None):
         url_extensions = [
-            ['audifonos-gamer', HEADPHONES],
-            ['mouse-gamer', MOUSE],
-            ['teclados-gamer', KEYBOARD]
+            ['accesorios-pc-gaming-audifonos', HEADPHONES],
+            ['computacion/perifericos-accesorios/mouses', MOUSE],
+            ['computacion/perifericos-accesorios/teclados', KEYBOARD]
         ]
         session = session_with_proxy(extra_args)
         product_urls = []
@@ -33,93 +34,96 @@ class WarenHouse(Store):
                 continue
             page = 1
             while True:
-                if page > 10:
+                if page > 100:
                     raise Exception('page overflow: ' + url_extension)
-                url_webpage = 'https://warenhouse.cl/product-category/' \
-                              '{}/page/{}/'.format(url_extension, page)
+                url_webpage = 'https://www.warenhouse.cl/listado/{}/' \
+                              '_Desde_{}'.format(url_extension, page)
                 print(url_webpage)
                 data = session.get(url_webpage).text
                 soup = BeautifulSoup(data, 'html.parser')
-                product_containers = soup.findAll('article', 'w-grid-item')
+                product_containers = soup.findAll('li',
+                                                  'ui-search-layout__item')
                 if not product_containers:
                     if page == 1:
                         logging.warning('Empty category: ' + url_extension)
                     break
                 for container in product_containers:
                     product_url = container.find('a')['href']
+                    if product_url in product_urls:
+                        return product_urls
                     product_urls.append(product_url)
-                page += 1
+                page += 48
         return product_urls
 
     @classmethod
     def products_for_url(cls, url, category=None, extra_args=None):
         print(url)
         session = session_with_proxy(extra_args)
-        response = session.get(url)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        name = soup.find('h1', 'w-post-elm').text
-        if soup.find('form', 'variations_form'):
-            variations = json.loads(soup.find('form', 'variations_form')[
-                                        'data-product_variations'])
-            products = []
-            for product in variations:
-                if len(product['attributes']) > 1:
-                    return []
-                variation_name = name + ' - ' + product['attributes'][
-                    'attribute_pa_color']
-                sku = str(product['variation_id'])
-                if product['is_in_stock']:
-                    stock = product['max_qty']
-                else:
-                    stock = 0
-                price = Decimal(product['display_price'])
-                picture_urls = [product['image']['src']]
-                p = Product(
-                    variation_name,
-                    cls.__name__,
-                    category,
-                    url,
-                    url,
-                    sku,
-                    stock,
-                    price,
-                    price,
-                    'CLP',
-                    sku=sku,
-                    picture_urls=picture_urls
-                )
-                products.append(p)
+        page_source = session.get(url).text
+        soup = BeautifulSoup(page_source, 'html.parser')
 
+        new_mode_data = re.search(
+            r'window.__PRELOADED_STATE__ =([\S\s]+?);\n', page_source)
+        data = json.loads(new_mode_data.groups()[0])
+        for entry in data['initialState']['components'].get('head', []):
+            if entry['id'] == 'item_status_message' and 'PAUSADA' in \
+                    entry['body']['text'].upper():
+                return []
+
+        sku = data['initialState']['id']
+        base_name = data['initialState']['components'][
+            'short_description'][0]['title']
+        price = Decimal(data['initialState']['schema'][0][
+                            'offers']['price'])
+        picture_urls = [x['data-zoom'] for x in
+                        soup.findAll('img', 'ui-pdp-image')[1::2]
+                        if 'data-zoom' in x.attrs]
+
+        products = []
+        pickers = data['initialState']['components']['variations']['pickers']
+        if len(pickers) > 1:
+            picker = pickers[1]
         else:
-            sku = soup.find('link', {'rel': 'shortlink'})['href'].split('p=')[
-                -1]
-            if soup.find('p', 'stock in-stock'):
-                stock = int(soup.find('p', 'stock in-stock').text.split()[0])
-            else:
-                stock = 0
-            price_container = soup.find('section', 'l-section wpb_row '
-                                                   'product height_auto')
-            if price_container.find('ins'):
-                price = Decimal(remove_words(price_container.find('ins').
-                                find('span', 'woocommerce-Price-amount').text))
-            else:
-                price = Decimal(remove_words(price_container.
-                                find('span', 'woocommerce-Price-amount').text))
+            picker = pickers[0]
 
-            picture_urls = [tag['src'] for tag in soup.find('div',
-                            'woocommerce-product-gallery').findAll('img')]
+        picker_id = picker['id']
+        for variation in picker['products']:
+            color_name = variation['label']['text']
+            name = '{} ({})'.format(base_name, color_name)
+            color_id = variation['attribute_id']
+
+            if '?' in url:
+                separator = '&'
+            else:
+                separator = '?'
+
+            variation_url = '{}{}attributes={}:{}'.format(url, separator,
+                                                          picker_id,
+                                                          color_id)
+            res = session.get(variation_url)
+            key_match = re.search(r'variation=(\d+)', res.url)
+
+            if key_match:
+                key = key_match.groups()[0]
+                variation_url = '{}?variation={}'.format(url, key)
+            else:
+                key = variation['id']
+
             p = Product(
                 name,
                 cls.__name__,
                 category,
+                variation_url,
                 url,
-                url,
-                sku,
-                stock,
+                key,
+                -1,
                 price,
                 price,
                 'CLP',
                 sku=sku,
-                picture_urls=picture_urls
+                picture_urls=picture_urls,
+
             )
-            return [p]
+
+            products.append(p)
+        return products
