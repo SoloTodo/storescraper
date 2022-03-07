5db70438b25b04c1621c21ea64fe4a4f1d783717
Vijay Khemlani <vj@solotodo.com>
2022-03-07 14:21:55 -0300

    * Updated Movistar

--- storescraper/stores/movistar.py
+++ storescraper/stores/movistar.py
@@ -11,7 +11,7 @@ from storescraper.utils import session_with_proxy, remove_words
 
 class Movistar(Store):
     preferred_discover_urls_concurrency = 1
-    preferred_products_for_url_concurrency = 3
+    preferred_products_for_url_concurrency = 8
     prepago_url = 'http://ww2.movistar.cl/prepago/'
     planes_url = 'https://ww2.movistar.cl/movil/planes-portabilidad/'
     cell_catalog_suffix = ''
