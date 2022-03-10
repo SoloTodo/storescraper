a9f23282b4a1b17af2309a4d46a86946c51a2516
Vijay Khemlani <vj@solotodo.com>
2022-03-10 10:12:11 -0300

    * Added categories documentation

--- storescraper/categories.py
+++ storescraper/categories.py
@@ -1,54 +1,106 @@
-CELL_ACCESORY = 'CellAccesory'
+#########################
+# Activas
+#########################
+
+# Aire acondicionado (no purificadores ni ventiladores)
 AIR_CONDITIONER = 'AirConditioner'
+# All in one (equipos tipo iMac, no desktops sin monitor o pc armados)
 ALL_IN_ONE = 'AllInOne'
-LAMP = 'Lamp'
-VACUUM_CLEANER = 'VacuumCleaner'
+# Audifonos de todos los tipos
 HEADPHONES = 'Headphones'
-B_2_B = 'B2B'
-WATER_HEATER = 'WaterHeater'
+# Celulares
+CELL = 'Cell'
+# Combos teclado mouse, opcionalmente con audifonos u otros accesorios
+KEYBOARD_MOUSE_COMBO = 'KeyboardMouseCombo'
+# Consolas (solo oficiales)
+VIDEO_GAME_CONSOLE = 'VideoGameConsole'
+# Cooler de procesador (no ventiladores sueltos)
+CPU_COOLER = 'CpuCooler'
+# Discos duros de desktop o notebook (no SSDs ni discos de servidor)
+STORAGE_DRIVE = 'StorageDrive'
+# Discos duros externos (usualmente USB)
+EXTERNAL_STORAGE_DRIVE = 'ExternalStorageDrive'
+# Equipos de audio en general (soundtower, soundbar, etc)
+STEREO_SYSTEM = 'StereoSystem'
+# Estufas de todos tipo (electricas, parafina, etc)
+SPACE_HEATER = 'SpaceHeater'
+# Pendrives (USB / USB-C, etc)
+USB_FLASH_DRIVE = 'UsbFlashDrive'
+# Fuentes de poder de PC desktop (no servidores)
+POWER_SUPPLY = 'PowerSupply'
+# Gabinetes de PC Desktop
+COMPUTER_CASE = 'ComputerCase'
+# Hornos y microondas
+OVEN = 'Oven'
+# Impresoras (solo de casa / oficina, no térmicas)
+PRINTER = 'Printer'
+# Lavadoras y secadoras de todos tipo
+WASHING_MACHINE = 'WashingMachine'
+# Monitores de PC desktop (no all in one ni pantallas de anuncios)
+MONITOR = 'Monitor'
+# Mouse
+MOUSE = 'Mouse'
+# Notebooks
+NOTEBOOK = 'Notebook'
+# Placas madres de pc desktop (no servidores)
+MOTHERBOARD = 'Motherboard'
+# Planes de telefonía
+CELL_PLAN = 'CellPlan'
+# Procesadores de PC Desktop (no de notebook ni de servidor)
+PROCESSOR = 'Processor'
+# RAM de PC desktop o notebook (no servidores)
+RAM = 'Ram'
+# Refrigeradores en general (incluye freezers)
+REFRIGERATOR = 'Refrigerator'
+# Tablets Android / iOS (no incluye e-readers como los kindle)
+TABLET = 'Tablet'
+# Tarjetas de memoria SD / microSD / CompactFlash
+MEMORY_CARD = 'MemoryCard'
+# Tarjetas de video para PC desktop
+VIDEO_CARD = 'VideoCard'
+# Teclados
+KEYBOARD = 'Keyboard'
+# Televisores (no monitores)
+TELEVISION = 'Television'
+# Unidades de estado solido (SSD) de 2.5" o M.2 para desktop o notebook
+SOLID_STATE_DRIVE = 'SolidStateDrive'
+# UPS (solo con bateria, no reguladores de voltaje)
+UPS = 'Ups'
+# Wearables (bandas deportivas o smartwatch)
+WEARABLE = 'Wearable'
+# Sillas gamer (no de oficina)
+GAMING_CHAIR = 'GamingChair'
+# Microfonos
+MICROPHONE = 'Microphone'
+# Escritorios gamer (no de oficina)
+GAMING_DESK = 'GamingDesk'
+# Ventiladores para gabinetes de PC desktop
+CASE_FAN = 'CaseFan'
+
+#########################
+# Legacy (no usar)
+#########################
+
+OPTICAL_DRIVE = 'OpticalDrive'
+LIGHT_TUBE = 'LightTube'
+LIGHT_PROJECTOR = 'LightProjector'
+VIDEO_GAME = 'VideoGame'
+D_S_L_R = 'DSLR'
+LAMP = 'Lamp'
 VIDEO_CAMERA = 'VideoCamera'
 CAMERA = 'Camera'
-CELL = 'Cell'
-STOVE = 'Stove'
-KEYBOARD_MOUSE_COMBO = 'KeyboardMouseCombo'
-VIDEO_GAME_CONSOLE = 'VideoGameConsole'
-CPU_COOLER = 'CpuCooler'
 VGA_COOLER = 'VgaCooler'
-STORAGE_DRIVE = 'StorageDrive'
-EXTERNAL_STORAGE_DRIVE = 'ExternalStorageDrive'
-D_S_L_R = 'DSLR'
-STEREO_SYSTEM = 'StereoSystem'
-SPACE_HEATER = 'SpaceHeater'
-USB_FLASH_DRIVE = 'UsbFlashDrive'
-POWER_SUPPLY = 'PowerSupply'
-COMPUTER_CASE = 'ComputerCase'
-OVEN = 'Oven'
-PRINTER = 'Printer'
-VIDEO_GAME = 'VideoGame'
-WASHING_MACHINE = 'WashingMachine'
-DISH_WASHER = 'DishWasher'
-MONITOR = 'Monitor'
-MOUSE = 'Mouse'
-NOTEBOOK = 'Notebook'
-MOTHERBOARD = 'Motherboard'
-CELL_PLAN = 'CellPlan'
-PROCESSOR = 'Processor'
-PROJECTOR = 'Projector'
-LIGHT_PROJECTOR = 'LightProjector'
-RAM = 'Ram'
-REFRIGERATOR = 'Refrigerator'
+# Aspiradoras
+VACUUM_CLEANER = 'VacuumCleaner'
+WATER_HEATER = 'WaterHeater'
+
+#########################
+# Reservados (no usar normalmente)
+#########################
+
 OPTICAL_DISK_PLAYER = 'OpticalDiskPlayer'
-TABLET = 'Tablet'
-MEMORY_CARD = 'MemoryCard'
-VIDEO_CARD = 'VideoCard'
-KEYBOARD = 'Keyboard'
-TELEVISION = 'Television'
-LIGHT_TUBE = 'LightTube'
-SOLID_STATE_DRIVE = 'SolidStateDrive'
-OPTICAL_DRIVE = 'OpticalDrive'
-UPS = 'Ups'
-WEARABLE = 'Wearable'
-GAMING_CHAIR = 'GamingChair'
-MICROPHONE = 'Microphone'
-GAMING_DESK = 'GamingDesk'
-CASE_FAN = 'CaseFan'
+PROJECTOR = 'Projector'
+DISH_WASHER = 'DishWasher'
+CELL_ACCESORY = 'CellAccesory'
+B_2_B = 'B2B'
+STOVE = 'Stove'
