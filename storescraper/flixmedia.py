98aeb955797dc99c5f272a97342f2ed1b4a0a9eb
Max Edwards <max@solotodo.com>
2022-03-30 11:07:20 -0300

    add try except

--- storescraper/flixmedia.py
+++ storescraper/flixmedia.py
@@ -1,37 +1,42 @@
 import json
 import re
+import traceback
 
 import requests
 from bs4 import BeautifulSoup
 
 
 def flixmedia_video_urls(mpn):
-    session = requests.Session()
+    try:
+        session = requests.Session()
 
-    url = 'https://media.flixcar.com/delivery/js/inpage/4800/cl/mpn/{}/' \
-          ''.format(mpn)
+        url = 'https://media.flixcar.com/delivery/js/inpage/4800/cl/mpn/{}/' \
+            ''.format(mpn)
 
-    response = session.get(url).text
-    match = re.search('product: \'(.+)\'', response)
+        response = session.get(url).text
+        match = re.search('product: \'(.+)\'', response)
 
-    if not match:
+        if not match:
+            return None
+
+        product_id = int(match.groups()[0])
+
+        if not product_id:
+            return None
+
+        url = 'https://media.flixcar.com/delivery/inpage/show/4800/cl/{}/html' \
+            ''.format(product_id)
+        soup = BeautifulSoup(session.get(url).text, 'html.parser')
+
+        video_containers = soup.findAll('input', 'flix-jw')
+        video_urls = []
+
+        for container in video_containers:
+            data = json.loads(container['value'])
+            for entry in data['playlist']:
+                video_urls.append('https:' + entry['file'])
+
+        return video_urls
+    except:
+        traceback.print_exc()
         return None
-
-    product_id = int(match.groups()[0])
-
-    if not product_id:
-        return None
-
-    url = 'https://media.flixcar.com/delivery/inpage/show/4800/cl/{}/html' \
-          ''.format(product_id)
-    soup = BeautifulSoup(session.get(url).text, 'html.parser')
-
-    video_containers = soup.findAll('input', 'flix-jw')
-    video_urls = []
-
-    for container in video_containers:
-        data = json.loads(container['value'])
-        for entry in data['playlist']:
-            video_urls.append('https:' + entry['file'])
-
-    return video_urls
