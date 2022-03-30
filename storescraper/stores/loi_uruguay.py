4dd3a915c1f1a4f67467fa3d4734d014d4aba3c2
Max Edwards <max@solotodo.com>
2022-03-30 11:07:53 -0300

    change api key and requests params

--- storescraper/stores/loi_uruguay.py
+++ storescraper/stores/loi_uruguay.py
@@ -20,11 +20,13 @@ class LoiUruguay(LoiChile):
 
         session = session_with_proxy(extra_args)
         res = session.post(
-            'https://90i0mrelm2-dsn.algolia.net/1/indexes/*/queries?x-'
-            'algolia-application-id=90I0MRELM2&x-algolia-api-key=f1b9f85e'
-            'ae8df13ab13361ccfe4793d7',
-            '{"requests":[{"indexName":"uy_products_date_desc","params":'
-            '"query=lg"}]}')
+            'https://90i0mrelm2-dsn.algolia.net/1/indexes/*/queries?x-algolia-'
+            'agent=Algolia%20for%20JavaScript%20(4.13.0)%3B%20Browser%20(lite)'
+            '%3B%20react%20(17.0.2)%3B%20react-instantsearch%20(6.22.0)%3B%20J'
+            'S%20Helper%20(3.7.3)&x-algolia-api-key=004b911528dce8b9f9543d1461'
+            'c60347&x-algolia-application-id=90I0MRELM2',
+            '{"requests":[{"indexName":"uy_products_price_asc","params":"filte'
+            'rs=product_enabled%3A1&query=lg"}]}')
 
         product_urls = []
 
