7f0b29c11d982b3f678dd51c57eaa6198276cf0e
Vijay Khemlani <vj@solotodo.com>
2022-04-07 14:43:47 -0400

    * Updated BookComputer

--- storescraper/stores/bbcc.py
+++ storescraper/stores/bbcc.py
@@ -100,6 +100,10 @@ class BookComputer(Store):
         print(url)
         session = session_with_proxy(extra_args)
         response = session.get(url)
+
+        if response.status_code == 404:
+            return []
+
         soup = BeautifulSoup(response.text, 'html.parser')
         if soup.find('select', 'form-control'):
             products = []
