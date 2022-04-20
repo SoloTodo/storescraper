0cce595cf402717ddf58c5a5771e5b5c3e1ad1f2
Max Edwards <max@solotodo.com>
2022-04-20 11:38:50 -0400

    fix regular expression

--- storescraper/stores/entel.py
+++ storescraper/stores/entel.py
@@ -107,7 +107,7 @@ class Entel(Store):
         endpoint = 'https://www.entel.cl/planes/includes/includes-planes/' \
                    'new-card/public/js/data-planes.js?v=66.10'
         res = session.get(endpoint)
-        raw_plans = re.search(r'Planes_TP_25dcto = ([\s\S]*?)\],',
+        raw_plans = re.search(r'Planes_TP_25dcto = ([\s\S]*?)\];',
                               res.text).groups()[0]
         plans = demjson.decode(raw_plans + ']')
 
