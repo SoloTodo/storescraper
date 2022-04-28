01fbce74a4b7ecbc258326c1cc444184b4e093cf
Max Edwards <max@solotodo.com>
2022-04-28 13:06:16 -0400

    check if product info is present

--- storescraper/stores/power_play.py
+++ storescraper/stores/power_play.py
@@ -106,7 +106,10 @@ class PowerPlay(Store):
         key = soup.find('link', {'rel': 'shortlink'})['href'].split('?p=')[-1]
 
         json_data = json.loads(soup.findAll(
-            'script', {'type': 'application/ld+json'})[1].text)['@graph'][1]
+            'script', {'type': 'application/ld+json'})[1].text)
+        if '@graph' not in json_data:
+            return []
+        json_data = json_data['@graph'][1]
 
         name = json_data['name']
         sku = json_data['sku']
