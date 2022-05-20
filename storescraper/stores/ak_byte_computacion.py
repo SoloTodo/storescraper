53c0c56853fcd7cf07df0418892aad3d22e48d75
Max Edwards <max@solotodo.com>
2022-05-20 11:18:19 -0400

    pycodestyle xd

--- storescraper/stores/ak_byte_computacion.py
+++ storescraper/stores/ak_byte_computacion.py
@@ -2,7 +2,9 @@ from decimal import Decimal
 import json
 import logging
 from bs4 import BeautifulSoup
-from storescraper.categories import CASE_FAN, COMPUTER_CASE, HEADPHONES, KEYBOARD, MONITOR, MOTHERBOARD, MOUSE, POWER_SUPPLY, PROCESSOR, RAM, STORAGE_DRIVE, VIDEO_CARD
+from storescraper.categories import CASE_FAN, COMPUTER_CASE, HEADPHONES, \
+    KEYBOARD, MONITOR, MOTHERBOARD, MOUSE, POWER_SUPPLY, PROCESSOR, RAM, \
+    STORAGE_DRIVE, VIDEO_CARD
 from storescraper.product import Product
 from storescraper.store import Store
 from storescraper.utils import session_with_proxy
@@ -52,8 +54,8 @@ class AkByteComputacion(Store):
             while True:
                 if page > 10:
                     raise Exception('Page overflow: ' + url_extension)
-                url_webpage = 'https://www.akbytecomputacion.cl/categoria-producto/{}/' \
-                              'page/{}/'.format(url_extension, page)
+                url_webpage = 'https://www.akbytecomputacion.cl/categoria-pr' \
+                              'oducto/{}/page/{}/'.format(url_extension, page)
                 data = session.get(url_webpage).text
                 soup = BeautifulSoup(data, 'html.parser')
                 product_containers = soup.findAll('div', 'product')
@@ -67,7 +69,7 @@ class AkByteComputacion(Store):
                     product_urls.append(product_url)
                 page += 1
         return product_urls
-    
+
     @classmethod
     def products_for_url(cls, url, category=None, extra_args=None):
         print(url)
@@ -78,7 +80,7 @@ class AkByteComputacion(Store):
 
         key = soup.find('link', {'rel': 'shortlink'})[
             'href'].split('=')[-1]
-        
+
         json_data = json.loads(soup.find(
             'script', {'type': 'application/ld+json'}).text)
 
@@ -96,12 +98,14 @@ class AkByteComputacion(Store):
         if soup.find('p', 'out-of-stock'):
             stock = 0
         elif soup.find('p', 'in-stock'):
-            stock = int(soup.find('p', 'in-stock').text.split(' disponible')[0])
+            stock = int(
+                soup.find('p', 'in-stock').text.split(' disponible')[0])
         else:
             stock = -1
 
         picture_urls = []
-        picture_container = soup.find('figure', 'woocommerce-product-gallery__wrapper')
+        picture_container = soup.find(
+            'figure', 'woocommerce-product-gallery__wrapper')
         for i in picture_container.findAll('img'):
             picture_urls.append(i['src'])
 
@@ -120,6 +124,3 @@ class AkByteComputacion(Store):
             description=description
         )
         return [p]
-
-
-        
