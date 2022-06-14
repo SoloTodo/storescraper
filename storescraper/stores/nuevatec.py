9dae7aeeb4dcee160031ab268df0f17b46eb3c96
Max Edwards <max@solotodo.com>
2022-06-14 11:18:46 -0400

    new page

--- storescraper/stores/nuevatec.py
+++ storescraper/stores/nuevatec.py
@@ -1,10 +1,11 @@
 from decimal import Decimal
+import json
 import demjson
 import logging
 from bs4 import BeautifulSoup
 from storescraper.categories import CASE_FAN, COMPUTER_CASE, \
     EXTERNAL_STORAGE_DRIVE, HEADPHONES, KEYBOARD, MICROPHONE, MONITOR, \
-    MOTHERBOARD, MOUSE, POWER_SUPPLY, PRINTER, PROCESSOR, RAM, \
+    MOTHERBOARD, MOUSE, NOTEBOOK, POWER_SUPPLY, PRINTER, PROCESSOR, RAM, \
     SOLID_STATE_DRIVE, STEREO_SYSTEM, STORAGE_DRIVE, USB_FLASH_DRIVE, \
     VIDEO_CARD
 from storescraper.product import Product
@@ -33,31 +34,30 @@ class Nuevatec(Store):
             PRINTER,
             MICROPHONE,
             HEADPHONES,
-            STEREO_SYSTEM
+            NOTEBOOK
         ]
 
     @classmethod
     def discover_urls_for_category(cls, category, extra_args=None):
         url_extensions = [
             ['monitores', MONITOR],
-            ['componentes-pc/placas-madres', MOTHERBOARD],
-            ['componentes-pc/procesadores', PROCESSOR],
-            ['tarjetas-de-video', VIDEO_CARD],
-            ['componentes-pc/memorias-ram', RAM],
-            ['componentes-pc/gabinetes', COMPUTER_CASE],
-            ['refrigeracion', CASE_FAN],
-            ['componentes-pc/fuentes-de-poder', POWER_SUPPLY],
-            ['almacenamiento/discos-duros', STORAGE_DRIVE],
+            ['procesadores', PROCESSOR],
+            ['placas-madres', MOTHERBOARD],
+            ['memorias-ram', RAM],
+            ['discos-duros', STORAGE_DRIVE],
             ['unidades-de-estado-solido-ssd', SOLID_STATE_DRIVE],
-            ['discos-duro-externo', EXTERNAL_STORAGE_DRIVE],
+            ['tarjetas-de-video', VIDEO_CARD],
+            ['fuentes-de-poder', POWER_SUPPLY],
+            ['refrigeracion', CASE_FAN],
+            ['gabinetes', COMPUTER_CASE],
+            ['notebook', NOTEBOOK],
+            ['impresoras', PRINTER],
             ['mouses', MOUSE],
             ['teclados', KEYBOARD],
-            ['pendrive', USB_FLASH_DRIVE],
-            ['accesorios-1/impresoras', PRINTER],
+            ['almacenamiento-externo', EXTERNAL_STORAGE_DRIVE],
+            ['pendrives', USB_FLASH_DRIVE],
+            ['audifonos', HEADPHONES],
             ['microfonos', MICROPHONE],
-            ['accesorios-1/audifonos', HEADPHONES],
-            ['accesorios-1/parlantes', STEREO_SYSTEM],
-            ['cables-y-accesorios/memoria-ram-notebook', RAM],
         ]
         session = session_with_proxy(extra_args)
         product_urls = []
@@ -68,19 +68,19 @@ class Nuevatec(Store):
             while True:
                 if page > 10:
                     raise Exception('Page overflow: ' + url_extension)
-                url_webpage = 'https://www.nuevatec.cl/{}?' \
-                              'page={}'.format(url_extension, page)
+                url_webpage = 'https://www.nuevatec.cl/{}/' \
+                              'page/{}/'.format(url_extension, page)
+                print(url_webpage)
                 data = session.get(url_webpage).text
                 soup = BeautifulSoup(data, 'html.parser')
-                product_containers = soup.findAll('div', 'product-block')
+                product_containers = soup.findAll('div', 'product-grid-item')
                 if not product_containers:
                     if page == 1:
                         logging.warning('Empty category: ' + url_extension)
                     break
                 for container in product_containers:
                     product_url = container.find('a')['href']
-                    product_urls.append(
-                        'https://www.nuevatec.cl' + product_url)
+                    product_urls.append(product_url)
                 page += 1
         return product_urls
 
@@ -90,29 +90,41 @@ class Nuevatec(Store):
         session = session_with_proxy(extra_args)
         response = session.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
-        key = soup.find('form', 'product-form')['action'].split('/')[-1]
-        json_data = demjson.decode(soup.find(
-            'script', {'type': 'application/ld+json'}).text, strict=False)
 
-        name = json_data['name']
-        sku = json_data.get('sku', None)
-        if sku is None:
-            sku = key
-        description = json_data['description']
-        price = Decimal(json_data['offers']['price'])
+        key = soup.find('link', {'rel': 'shortlink'})['href'].split('p=')[1]
 
-        stock_span = soup.find('span', 'product-form-stock')
-        if stock_span:
-            stock = int(stock_span.text)
+        json_data = json.loads(soup.find(
+            'script', {'type': 'application/ld+json'}).text)
+        for entry in json_data['@graph']:
+            if entry['@type'] == 'Product':
+                product_data = entry
+                break
         else:
-            stock = 0
+            raise Exception('No JSON product data found')
+
+        name = product_data['name']
+        sku = str(product_data['sku'])
+        description = product_data['description']
+        offer_price = Decimal(product_data['offers'][0]['price'])
+        normal_price = (offer_price * Decimal(1.06)).quantize(0)
+
+        qty_input = soup.find('input', 'input-text qty text')
+        if qty_input:
+            if qty_input['max']:
+                stock = int(qty_input['max'])
+            else:
+                stock = -1
+        else:
+            if soup.find('button', 'single_add_to_cart_button'):
+                stock = 1
+            else:
+                stock = 0
 
         picture_urls = []
         image_container = soup.find('div', 'product-images')
+        for image in image_container.findAll('div', 'product-image-wrap'):
+            picture_urls.append(image.find('img')['src'])
 
-        # TODO: exchange sku with keys in new page version
-        for image in image_container.findAll('img'):
-            picture_urls.append(image['src'])
         p = Product(
             name,
             cls.__name__,
@@ -121,8 +133,8 @@ class Nuevatec(Store):
             url,
             sku,
             stock,
-            price,
-            price,
+            normal_price,
+            offer_price,
             'CLP',
             sku=key,
             picture_urls=picture_urls,
