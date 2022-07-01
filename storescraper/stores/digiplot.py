b2b1d9f810a3a31c5631db2a9f0f09b052c8d361
Max Edwards <max@solotodo.com>
2022-07-01 10:35:21 -0400

    fix stock

--- storescraper/stores/digiplot.py
+++ storescraper/stores/digiplot.py
@@ -127,7 +127,15 @@ class Digiplot(Store):
 
         name = data['descripcion'].strip()
         sku = data['idproducto'].strip()
-        stock = round(float(data['stock']))
+
+        stock = 0
+        stock_containers = soup.findAll(
+            'div', 'product-single__availability-item')
+        for container in stock_containers:
+            stock_text = container.text.split(':')[1].split('unid')[0].strip()
+            if stock_text != '':
+                stock += int(stock_text)
+
         offer_price = Decimal(data['precioweb1'])
         normal_price = Decimal(data['precioweb2'])
         description = None
