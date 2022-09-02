33476691e4c80de5032d8d630f07ff3ec46b833b
Vijay Khemlani <vj@solotodo.com>
2022-09-02 10:40:09 -0400

    * Updated TecTec

--- storescraper/stores/tec_tec.py
+++ storescraper/stores/tec_tec.py
@@ -56,6 +56,9 @@ class TecTec(Store):
         ]
 
         session = session_with_proxy(extra_args)
+        session.headers['user-agent'] = \
+            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 ' \
+            '(KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
         product_urls = []
         for url_extension, local_category in url_extensions:
             if local_category != category:
@@ -67,7 +70,7 @@ class TecTec(Store):
 
                 url_webpage = 'https://tectec.cl/categoria/{}/page' \
                               '/{}/'.format(url_extension, page)
-                # print(url_webpage)
+                print(url_webpage)
                 response = session.get(url_webpage)
                 soup = BeautifulSoup(response.text, 'html.parser')
                 product_containers = soup.findAll('div', 'product-wrapper')
@@ -87,6 +90,9 @@ class TecTec(Store):
     def products_for_url(cls, url, category=None, extra_args=None):
         print(url)
         session = session_with_proxy(extra_args)
+        session.headers['user-agent'] = \
+            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 ' \
+            '(KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
         response = session.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
         name = soup.find('h1', 'product_title').text.strip()
