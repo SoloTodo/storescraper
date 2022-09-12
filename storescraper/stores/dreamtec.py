2bcdfa10ae5189f0e29cf33eb6078206749134b1
Max Edwards <max@solotodo.com>
2022-09-12 12:48:11 -0300

    new store

--- storescraper/stores/dreamtec.py
+++ storescraper/stores/dreamtec.py
@@ -7,7 +7,7 @@ from storescraper.categories import ALL_IN_ONE, GAMING_CHAIR, HEADPHONES, \
     VIDEO_GAME_CONSOLE, WEARABLE
 from storescraper.product import Product
 from storescraper.store import Store
-from storescraper.utils import session_with_proxy
+from storescraper.utils import remove_words, session_with_proxy
 
 
 class Dreamtec(Store):
@@ -32,25 +32,23 @@ class Dreamtec(Store):
     @classmethod
     def discover_urls_for_category(cls, category, extra_args=None):
         url_extensions = [
+            ['gamer-zone/audifonos-gamer-zone/', HEADPHONES],
+            ['gamer-zone/consolas', VIDEO_GAME_CONSOLE],
             ['gamer-zone/monitores-gamer-zone', MONITOR],
-            ['gamer-zone/notebook-gamers', NOTEBOOK],
-            ['gamer-zone/consola-gamer-zone', VIDEO_GAME_CONSOLE],
-            ['gamer-zone/audifonos-gamer-gamer-zone', HEADPHONES],
-            ['gamer-zone/silla-gamer-gamer-zone', GAMING_CHAIR],
-            ['gamer-zone/accesorios-gamer-gamer-zone', KEYBOARD],
-            ['home-office/monitores-home-office', MONITOR],
-            ['ome-office/notebooks-home-office', NOTEBOOK],
-            ['home-office/macbook', NOTEBOOK],
-            ['home-office/all-in-one-home-office', ALL_IN_ONE],
-            ['home-office/impresoras', PRINTER],
+            ['gamer-zone/notebooks-gamer-zone', NOTEBOOK],
+            ['gamer-zone/sillas-gamer', GAMING_CHAIR],
+            ['home-office/all-in-one', ALL_IN_ONE],
             ['home-office/escaner', PRINTER],
-            ['home-office/accesorios-home-officie', KEYBOARD],
-            ['smart-home/smart-tv-smart-home', TELEVISION],
-            ['smart-home/soundbar-smat-home', STEREO_SYSTEM],
-            ['smart-home/tablets-smart-home', TABLET],
-            ['smart-home/ipad-smart-home', TABLET],
-            ['smart-home/smartwatch-smart-home', WEARABLE],
+            ['home-office/impresoras', PRINTER],
+            ['home-office/macbook', NOTEBOOK],
+            ['home-office/monitores', MONITOR],
+            ['home-office/notebooks', NOTEBOOK],
             ['smart-home/audifonos', HEADPHONES],
+            ['smart-home/ipad', TABLET],
+            ['smart-home/smart-tv', TELEVISION],
+            ['smart-home/smart-watch', WEARABLE],
+            ['smart-home/soundbar', STEREO_SYSTEM],
+            ['smart-home/tablets', TABLET],
         ]
 
         session = session_with_proxy(extra_args)
@@ -58,23 +56,18 @@ class Dreamtec(Store):
         for url_extension, local_category in url_extensions:
             if local_category != category:
                 continue
-            page = 1
-            while True:
-                if page > 10:
-                    raise Exception('Page overflow: ' + url_extension)
-                url_webpage = 'https://www.dreamtec.cl/product-category/{}/' \
-                              'page/{}/'.format(url_extension, page)
-                data = session.get(url_webpage).text
-                soup = BeautifulSoup(data, 'html.parser')
-                product_containers = soup.findAll('section', 'product')
-                if not product_containers:
-                    if page == 1:
-                        logging.warning('Empty category: ' + url_extension)
-                    break
-                for container in product_containers:
-                    product_url = container.find('a')['href']
-                    product_urls.append(product_url)
-                page += 1
+
+            url_webpage = 'https://www.dreamtec.cl/{}/' \
+                ''.format(url_extension)
+            data = session.get(url_webpage).text
+            soup = BeautifulSoup(data, 'html.parser')
+            product_containers = soup.findAll('div', 'foto-prod')
+            if not product_containers:
+                logging.warning('Empty category: ' + url_extension)
+
+            for container in product_containers:
+                product_url = container.find('a')['href']
+                product_urls.append(product_url)
         return product_urls
 
     @classmethod
@@ -84,43 +77,20 @@ class Dreamtec(Store):
         response = session.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
 
-        key = soup.find('link', {'rel': 'shortlink'})['href'].split('?p=')[-1]
+        name = soup.find('h1').text.strip()
+        description = soup.find('div', {'id': 'nav-home'}).text.strip()
+        sku = str(soup.find('span', 'sku-detalle').find('strong').text)
+        normal_price = Decimal(remove_words(soup.find('strong', 'fs-3').text))
+        offer_price = Decimal(remove_words(soup.find('h3', 'fs-1').text))
 
-        json_data = json.loads(soup.findAll(
-            'script', {'type': 'application/ld+json'})[-1].text)
-
-        if '@graph' not in json_data:
-            return []
-
-        for entry in json_data['@graph']:
-            if entry['@type'] == 'Product':
-                product_data = entry
-                break
-        else:
-            raise Exception('No JSON product data found')
-
-        name = product_data['name']
-        description = product_data['description']
-        sku = str(product_data['sku'])
-        price = Decimal(product_data['offers'][0]['price'])
-
-        qty_input = soup.find('input', 'input-text qty text')
-        if qty_input:
-            if qty_input['max']:
-                stock = int(qty_input['max'])
-            else:
-                stock = -1
-        else:
-            if soup.find('button', 'single_add_to_cart_button'):
-                stock = 1
-            else:
-                stock = 0
+        stock_select = soup.find('select', {'id': 'select-cantidad'})
+        options = stock_select.findAll('option')
+        stock = int(options[-1]['value'])
 
         picture_urls = []
-        picture_container = soup.find(
-            'figure', 'woocommerce-product-gallery__wrapper')
-        for a in picture_container.findAll('a'):
-            picture_urls.append(a['href'])
+        for i in soup.findAll('img', 'img-producto-miniatura'):
+            if i['src'] not in picture_urls:
+                picture_urls.append(i['src'])
 
         p = Product(
             name,
@@ -128,10 +98,10 @@ class Dreamtec(Store):
             category,
             url,
             url,
-            key,
+            sku,
             stock,
-            price,
-            price,
+            normal_price,
+            offer_price,
             'CLP',
             sku=sku,
             picture_urls=picture_urls,
