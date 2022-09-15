5de98e30bac5c3c4aff1254e0b73a8f3ecfa0304
Max Edwards <max@solotodo.com>
2022-09-15 12:41:01 -0300

    refactor categories ids

--- storescraper/stores/travel_tienda.py
+++ storescraper/stores/travel_tienda.py
@@ -43,8 +43,8 @@ class TravelTienda(Store):
             ('816923966', WEARABLE),  # Tech > Telefonía > Smartwatch
             ('2722336774', TELEVISION),  # Tech > TV > Televisores
             ('2234300147', STEREO_SYSTEM),  # Tech > TV > Sistemas de Sonido TV
-            ('2114119513', TABLET),  # Tech > Comp. > Tablets & Note. > Tablets
-            ('2611045626', NOTEBOOK),  # Tech > Comp. > Tablets & Note. > Note.
+            ('2934181475', TABLET),  # Tech > Comp. > Tablets
+            ('3415774358', NOTEBOOK),  # Tech > Comp. > Notebook
             ('375810843', MONITOR),  # Tech > Computación > Monitores
             ('3213133197', PRINTER),  # Tech > Computación > Domótica & Acc.
             ('714969430', GAMING_CHAIR),  # Tech > Gamer > Sillas y Escritorios
