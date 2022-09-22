4ff48d767ac308d3c3721dd35f3f2191275532ef
Max Edwards <max@solotodo.com>
2022-09-22 14:35:19 -0300

    new page

--- storescraper/stores/acer_store.py
+++ storescraper/stores/acer_store.py
@@ -1,13 +1,12 @@
 import json
-import re
 
 from bs4 import BeautifulSoup
 from decimal import Decimal
+from storescraper.categories import MONITOR, NOTEBOOK
 
 from storescraper.product import Product
 from storescraper.store import Store
-from storescraper.utils import session_with_proxy, remove_words, \
-    html_to_markdown
+from storescraper.utils import check_ean13, session_with_proxy
 
 
 class AcerStore(Store):
@@ -17,24 +16,16 @@ class AcerStore(Store):
     @classmethod
     def categories(cls):
         return [
-            'Notebook',
-            'Monitor',
-            'Tablet',
-            'AllInOne',
+            NOTEBOOK,
+            MONITOR,
         ]
 
     @classmethod
     def discover_urls_for_category(cls, category, extra_args=None):
         category_paths = [
-            ['25/notebooks-gamer', 'Notebook'],
-            ['28/notebook', 'Notebook'],
-            ['89/chromebooks-chile', 'Notebook'],
-            ['35/notebooks-ultralivianos', 'Notebook'],
-            ['31/notebooks-convertible', 'Notebook'],
-            ['34/notebooks-tablets-2-en-1', 'Notebook'],
-            ['38/all-in-one', 'AllInOne'],
-            ['36/monitores', 'Monitor'],
-            ['47/tablets', 'Tablet'],
+            ['notebook-gamer', NOTEBOOK],
+            ['notebook', NOTEBOOK],
+            ['monitores/monitores-pc', MONITOR],
         ]
 
         product_urls = []
@@ -45,30 +36,32 @@ class AcerStore(Store):
                 continue
 
             page = 1
-
-            while True:
+            done = False
+            while not done:
                 if page >= 10:
                     raise Exception('Page overflow')
 
-                category_url = 'https://www.acerstore.cl/site/c/{}/' \
-                               'productos?&page={}&orden=2'.format(
-                                   category_id, page)
+                category_url = 'https://www.acerstore.cl/{}/' \
+                               '?page={}'.format(category_id, page)
                 print(category_url)
-                json_data = json.loads(session.get(category_url).text)
 
-                if 'items' not in json_data['categorias'][0]:
-                    break
+                data = session.get(category_url).text
+                soup = BeautifulSoup(data, 'html.parser')
+                page_state_tag = soup.find('template',
+                                           {'data-varname': '__STATE__'})
+                page_state = json.loads(page_state_tag.text)
 
-                if 'categorias' not in json_data:
-                    if page == 1:
-                        raise Exception('Empty category: {}'.format(
-                            category_id))
+                done = True
+                for key, product in page_state.items():
+                    if 'productId' not in product:
+                        continue
+                    done = False
+                    product_url = 'https://www.acerstore.cl/{}/p'.format(
+                        product['linkText'])
+                    product_urls.append(product_url)
 
-                    break
-
-                for container in json_data['categorias'][0]['items']:
-                    soup = BeautifulSoup(container, 'html.parser')
-                    product_urls.append(soup.find('a')['href'])
+                if done and page == 1:
+                    raise Exception('Empty site')
 
                 page += 1
 
@@ -78,40 +71,47 @@ class AcerStore(Store):
     def products_for_url(cls, url, category=None, extra_args=None):
         print(url)
         session = session_with_proxy(extra_args)
-        page_source = session.get(url)
-        if len(page_source.history) and page_source.history[
-                0].status_code == 301:
-            return []
-        soup = BeautifulSoup(page_source.text, 'html.parser')
+        response = session.get(url)
+        soup = BeautifulSoup(response.text, 'html5lib')
 
-        sku = re.search(r'/p/(\d+)/', url).groups()[0]
-        model = soup.find('h1', 'producto-nombre').text.strip()
-        part_number = soup.find('div', 'producto-subtitulo').text.strip()
-        name = ' '.join('{} ({})'.format(model, part_number).split())
+        product_data = json.loads(
+            soup.find('template', {'data-varname': '__STATE__'}).text)
 
-        stock_container = soup.find('div', 'producto-stock')
+        base_json_key = list(product_data.keys())[0]
+        product_specs = product_data[base_json_key]
 
-        if stock_container:
-            stock = int(re.search(
-                r'STOCK: (\d+)',
-                stock_container.text).groups()[0])
-        else:
-            stock = 0
+        key = product_specs['productId']
+        name = product_specs['productName']
+        sku = product_specs['productReference']
+        description = product_specs.get('description', None)
 
-        price = soup.find('div', 'producto-precio').text.split('(')[0]
-        price = Decimal(remove_words(price))
+        pricing_key = '${}.items.0.sellers.0.commertialOffer'.format(
+            base_json_key)
+        pricing_data = product_data[pricing_key]
 
-        if 'PREVENTA' in soup.find('div', 'producto-precio').text:
-            stock = 0
+        price = Decimal(str(pricing_data['Price']))
+        stock = pricing_data['AvailableQuantity']
 
-        description = html_to_markdown(
-            str(soup.find('table', 'producto-ficha-tabla')))
+        picture_list_key = '{}.items.0'.format(base_json_key)
+        picture_list_node = product_data[picture_list_key]
+        picture_ids = [x['id'] for x in picture_list_node['images']]
 
         picture_urls = []
-        for tag in soup.findAll('div', 'producto-galeria-imagenes-item'):
-            picture_tag = tag.find('a')
-            if picture_tag:
-                picture_urls.append(picture_tag['href'])
+        for picture_id in picture_ids:
+            picture_node = product_data[picture_id]
+            picture_urls.append(picture_node['imageUrl'].split('?')[0])
+
+        item_key = '{}.items.0'.format(base_json_key)
+        part_number = product_data[item_key]['name']
+
+        ean = None
+        for property in product_specs['properties']:
+            if product_data[property['id']]['name'] == 'EAN':
+                ean = product_data[property['id']]['values']['json'][0]
+                if check_ean13(ean):
+                    break
+                else:
+                    ean = None
 
         p = Product(
             name,
@@ -119,13 +119,14 @@ class AcerStore(Store):
             category,
             url,
             url,
-            sku,
+            key,
             stock,
             price,
             price,
             'CLP',
             sku=sku,
             part_number=part_number,
+            ean=ean,
             description=description,
             picture_urls=picture_urls
         )
