019c40edee037f6c291313088ec2ffeb06ee08f8
Max Edwards <max@solotodo.com>
2022-09-26 14:39:03 -0300

    use lider class

--- storescraper/stores/lider.py
+++ storescraper/stores/lider.py
@@ -23,6 +23,130 @@ from storescraper import banner_sections as bs
 class Lider(Store):
     DEFAULT_USER_AGENT = 'PostmanRuntime/7.28.4'
 
+    tenant = 'catalogo'
+    category_paths = [
+        # TECNO
+        ['Tecno/TV/Televisores', [TELEVISION],
+         'Tecno > TV > Televisores', 1],
+        ['Tecno/TV/Home_Theater', [STEREO_SYSTEM],
+         'Tecno > TV > Home Theater', 1],
+        ['Tecno/TV/Proyectores', [PROJECTOR],
+         'Tecno > TV > Proyectores', 1],
+        ['Tecno/Audio', [STEREO_SYSTEM],
+         'Tecno > Audio', 1],
+        ['Tecno/Audio/Equipos_de_Música_y_Karaoke', [STEREO_SYSTEM],
+         'Tecno > Audio > Equipos de Música y Karaoke', 1],
+        ['Tecno/Audio/Soundbars_y_Home_Theater', [STEREO_SYSTEM],
+         'Tecno > Audio > Equipos de Música y Karaoke', 1],
+        ['Tecno/Audio/Audio_Portable', [STEREO_SYSTEM],
+         'Tecno > Audio > Audio Portable', 1],
+        ['Tecno/Audio/Micro_y_Mini_Componentes', [STEREO_SYSTEM],
+         'Tecno > Audio > Micro y Mini Componentes', 1],
+        ['Tecno/Audio/Audífonos', [HEADPHONES],
+         'Tecno > Audio > Audífonos', 1],
+        ['Tecno/Audio/Tornamesas_y_Vinilos', [STEREO_SYSTEM],
+         'Tecno > Audio > Tornamesas y Vinilos', 1],
+        ['Tecno/Audio/Audio_HI-FI', [STEREO_SYSTEM],
+         'Tecno > Audio > Audio HI-FI', 1],
+        ['Tecno/Smart_Home/Proyectores', [PROJECTOR],
+         'Tecno > Smart Home > Proyectores', 1],
+        ['Tecno/Videojuegos/Consolas', [VIDEO_GAME_CONSOLE],
+         'Tecno > Videojuegos > Consolas', 1],
+        ['Tecno/Videojuegos/Nintendo', [VIDEO_GAME_CONSOLE],
+         'Tecno > Videojuegos > Nintendo', 1],
+        ['Tecno/Videojuegos/PlayStation', [VIDEO_GAME_CONSOLE],
+         'Tecno > Videojuegos > PlayStation', 1],
+        ['Tecno/Videojuegos/XBOX', [VIDEO_GAME_CONSOLE],
+         'Tecno > Videojuegos > XBOX', 1],
+        # CELULARES
+        ['Celulares/Celulares_y_Teléfonos', [CELL],
+         'Celulares > Celulares y Teléfonos', 1],
+        ['Celulares/Smartwatches_y_Wearables', [WEARABLE],
+         'Celulares > Smartwatches y Wearables', 1],
+        # COMPUTACION
+        ['Computación/Computadores/Notebooks', [NOTEBOOK],
+         'Computación > Computadores > Notebooks', 1],
+        ['Computación/Computadores/Tablets', [TABLET],
+         'Computación > Computadores > Tablets', 1],
+        ['Computación/Computadores/Computadores_All_in_One', [ALL_IN_ONE],
+         'Computación > Computadores > Computadores All in One', 1],
+        ['Computación/Computadores/Monitores_y_Proyectores', [MONITOR],
+         'Computación > Computadores > Monitores y Proyectores', 1.0],
+        ['Computación/Computadores/Accesorios_Computación', [MOUSE],
+         'Computación > Computadores > Accesorios Computación', 1.0],
+        ['Computación/Mundo_Gamer/Computación_Gamer', [NOTEBOOK],
+         'Computación > Mundo Gamer > Computación Gamer', 1.0],
+        ['Computación/Mundo_Gamer/Mouse_y_Teclados', [KEYBOARD],
+         'Computación > Mundo Gamer > Mouse y Teclados', 1.0],
+        ['Computación/Mundo_Gamer/Audífonos', [HEADPHONES],
+         'Computación > Mundo Gamer > Audífonos', 1.0],
+        ['Computación/Mundo_Gamer/Consolas', [VIDEO_GAME_CONSOLE],
+         'Computación > Mundo Gamer > Consolas', 1.0],
+        ['Computación/Mundo_Gamer/Sillas_Gamer', [GAMING_CHAIR],
+         'Computación > Mundo Gamer > Sillas Gamer', 1],
+        ['Computación/Impresión/Impresoras_y_Multifuncionales',
+         [PRINTER],
+         'Computación > Impresión > Impresoras y Multifuncionales', 1.0],
+        ['Computación/Impresión/Impresoras_Láser', [PRINTER],
+         'Computación > Impresión > Impresoras Láser', 1.0],
+        ['Computación/Almacenamiento/Discos_Duros',
+         [EXTERNAL_STORAGE_DRIVE],
+         'Computación > Almacenamiento > Discos Duros', 1.0],
+        ['Computación/Almacenamiento/Discos_Duros_SSD',
+         [SOLID_STATE_DRIVE],
+         'Computación > Almacenamiento > Discos Duros SSD', 1.0],
+        ['Computación/Almacenamiento/Tarjetas_de_Memoria',
+         [MEMORY_CARD],
+         'Computación > Almacenamiento > Tarjetas de Memoria', 1.0],
+        ['Computación/Almacenamiento/Pendrives', [USB_FLASH_DRIVE],
+         'Computación > Almacenamiento > Pendrives', 1.0],
+        # ELECTROHOGAR
+        ['Electrohogar/Refrigeración', [REFRIGERATOR],
+         'Electrohogar > Refrigeración', 1.0],
+        ['Electrohogar/Refrigeración/No_Frost',
+         [REFRIGERATOR],
+         'Electrohogar > Refrigeración > No Frost', 1.0],
+        ['Electrohogar/Refrigeración/Frío_Directo',
+         [REFRIGERATOR],
+         'Electrohogar > Refrigeración > Frio Directo',
+         1.0],
+        ['Electrohogar/Refrigeración/Side_By_Side', [REFRIGERATOR],
+         'Electrohogar > Refrigeración > Side By Side',
+         1.0],
+        ['Electrohogar/Refrigeración/Freezer', [REFRIGERATOR],
+         'Electrohogar > Refrigeración > Freezer', 1.0],
+        ['Electrohogar/Refrigeración/Frigobar', [REFRIGERATOR],
+         'Electrohogar > Refrigeración > Frigobar', 1.0],
+        ['Electrohogar/Lavado_y_Planchado', [WASHING_MACHINE],
+         'Electrohogar > Lavado y Planchado', 1.0],
+        ['Electrohogar/Lavado_y_Planchado/Lavadoras', [WASHING_MACHINE],
+         'Electrohogar > Lavado y Planchado > Lavadoras', 1.0],
+        ['Electrohogar/Lavado_y_Planchado/Lavadoras_Secadoras',
+         [WASHING_MACHINE],
+         'Electrohogar > Lavado y Planchado > Lavadoras Secadoras', 1.0],
+        ['Electrohogar/Lavado_y_Planchado/Secadoras', [WASHING_MACHINE],
+         'Electrohogar > Lavado y Planchado > Secadoras', 1.0],
+        ['Electrohogar/Lavado_y_Planchado/Lavavajillas', [DISH_WASHER],
+         'Electrohogar > Lavado y Planchado > Lavavajillas', 1.0],
+        ['Electrohogar/Aspiradoras_y_Limpieza', [VACUUM_CLEANER],
+         'Electrohogar > Aspiradoras y Limpieza', 1.0],
+        ['Electrohogar/Electrodomésticos_Cocina/Hornos_Eléctricos', [OVEN],
+         'Electrohogar > Electrodomésticos Cocina > Hornos Eléctricos',
+         1.0],
+        ['Electrohogar/Electrodomésticos_Cocina/Microondas', [OVEN],
+         'Electrohogar > Electrodomésticos Cocina > Microondas', 1.0],
+        ['Electrohogar/Cocinas/Hornos_Empotrables', [OVEN],
+         'Electrohogar > Cocinas > Hornos Empotrables', 1.0],
+        ['Electrohogar/Climatización/Calefacción', [SPACE_HEATER],
+         'Electrohogar > Climatización > Calefacción', 1.0],
+        ['Electrohogar/Climatización/Termos_y_Calefonts', [WATER_HEATER],
+         'Electrohogar > Climatización > Termos y Calefonts', 1.0],
+        ['Electrohogar/Climatización/Ventilación/Aire_Acondicionado',
+         [AIR_CONDITIONER],
+         'Electrohogar > Climatización > Ventilación > Aire Acondicionado',
+         1.0],
+    ]
+
     @classmethod
     def categories(cls):
         return [
@@ -57,135 +181,14 @@ class Lider(Store):
 
     @classmethod
     def discover_entries_for_category(cls, category, extra_args=None):
-        category_paths = [
-            # TECNO
-            ['Tecno/TV/Televisores', [TELEVISION],
-             'Tecno > TV > Televisores', 1],
-            ['Tecno/TV/Home_Theater', [STEREO_SYSTEM],
-             'Tecno > TV > Home Theater', 1],
-            ['Tecno/TV/Proyectores', [PROJECTOR],
-             'Tecno > TV > Proyectores', 1],
-            ['Tecno/Audio', [STEREO_SYSTEM],
-             'Tecno > Audio', 1],
-            ['Tecno/Audio/Equipos_de_Música_y_Karaoke', [STEREO_SYSTEM],
-             'Tecno > Audio > Equipos de Música y Karaoke', 1],
-            ['Tecno/Audio/Soundbars_y_Home_Theater', [STEREO_SYSTEM],
-             'Tecno > Audio > Equipos de Música y Karaoke', 1],
-            ['Tecno/Audio/Audio_Portable', [STEREO_SYSTEM],
-             'Tecno > Audio > Audio Portable', 1],
-            ['Tecno/Audio/Micro_y_Mini_Componentes', [STEREO_SYSTEM],
-             'Tecno > Audio > Micro y Mini Componentes', 1],
-            ['Tecno/Audio/Audífonos', [HEADPHONES],
-             'Tecno > Audio > Audífonos', 1],
-            ['Tecno/Audio/Tornamesas_y_Vinilos', [STEREO_SYSTEM],
-             'Tecno > Audio > Tornamesas y Vinilos', 1],
-            ['Tecno/Audio/Audio_HI-FI', [STEREO_SYSTEM],
-             'Tecno > Audio > Audio HI-FI', 1],
-            ['Tecno/Smart_Home/Proyectores', [PROJECTOR],
-             'Tecno > Smart Home > Proyectores', 1],
-            ['Tecno/Videojuegos/Consolas', [VIDEO_GAME_CONSOLE],
-             'Tecno > Videojuegos > Consolas', 1],
-            ['Tecno/Videojuegos/Nintendo', [VIDEO_GAME_CONSOLE],
-             'Tecno > Videojuegos > Nintendo', 1],
-            ['Tecno/Videojuegos/PlayStation', [VIDEO_GAME_CONSOLE],
-             'Tecno > Videojuegos > PlayStation', 1],
-            ['Tecno/Videojuegos/XBOX', [VIDEO_GAME_CONSOLE],
-             'Tecno > Videojuegos > XBOX', 1],
-            # CELULARES
-            ['Celulares/Celulares_y_Teléfonos', [CELL],
-             'Celulares > Celulares y Teléfonos', 1],
-            ['Celulares/Smartwatches_y_Wearables', [WEARABLE],
-             'Celulares > Smartwatches y Wearables', 1],
-            # COMPUTACION
-            ['Computación/Computadores/Notebooks', [NOTEBOOK],
-             'Computación > Computadores > Notebooks', 1],
-            ['Computación/Computadores/Tablets', [TABLET],
-             'Computación > Computadores > Tablets', 1],
-            ['Computación/Computadores/Computadores_All_in_One', [ALL_IN_ONE],
-             'Computación > Computadores > Computadores All in One', 1],
-            ['Computación/Computadores/Monitores_y_Proyectores', [MONITOR],
-             'Computación > Computadores > Monitores y Proyectores', 1.0],
-            ['Computación/Computadores/Accesorios_Computación', [MOUSE],
-             'Computación > Computadores > Accesorios Computación', 1.0],
-            ['Computación/Mundo_Gamer/Computación_Gamer', [NOTEBOOK],
-             'Computación > Mundo Gamer > Computación Gamer', 1.0],
-            ['Computación/Mundo_Gamer/Mouse_y_Teclados', [KEYBOARD],
-             'Computación > Mundo Gamer > Mouse y Teclados', 1.0],
-            ['Computación/Mundo_Gamer/Audífonos', [HEADPHONES],
-             'Computación > Mundo Gamer > Audífonos', 1.0],
-            ['Computación/Mundo_Gamer/Consolas', [VIDEO_GAME_CONSOLE],
-             'Computación > Mundo Gamer > Consolas', 1.0],
-            ['Computación/Mundo_Gamer/Sillas_Gamer', [GAMING_CHAIR],
-             'Computación > Mundo Gamer > Sillas Gamer', 1],
-            ['Computación/Impresión/Impresoras_y_Multifuncionales',
-             [PRINTER],
-             'Computación > Impresión > Impresoras y Multifuncionales', 1.0],
-            ['Computación/Impresión/Impresoras_Láser', [PRINTER],
-             'Computación > Impresión > Impresoras Láser', 1.0],
-            ['Computación/Almacenamiento/Discos_Duros',
-             [EXTERNAL_STORAGE_DRIVE],
-             'Computación > Almacenamiento > Discos Duros', 1.0],
-            ['Computación/Almacenamiento/Discos_Duros_SSD',
-             [SOLID_STATE_DRIVE],
-             'Computación > Almacenamiento > Discos Duros SSD', 1.0],
-            ['Computación/Almacenamiento/Tarjetas_de_Memoria',
-             [MEMORY_CARD],
-             'Computación > Almacenamiento > Tarjetas de Memoria', 1.0],
-            ['Computación/Almacenamiento/Pendrives', [USB_FLASH_DRIVE],
-             'Computación > Almacenamiento > Pendrives', 1.0],
-            # ELECTROHOGAR
-            ['Electrohogar/Refrigeración', [REFRIGERATOR],
-             'Electrohogar > Refrigeración', 1.0],
-            ['Electrohogar/Refrigeración/No_Frost',
-             [REFRIGERATOR],
-             'Electrohogar > Refrigeración > No Frost', 1.0],
-            ['Electrohogar/Refrigeración/Frío_Directo',
-             [REFRIGERATOR],
-             'Electrohogar > Refrigeración > Frio Directo',
-             1.0],
-            ['Electrohogar/Refrigeración/Side_By_Side', [REFRIGERATOR],
-             'Electrohogar > Refrigeración > Side By Side',
-             1.0],
-            ['Electrohogar/Refrigeración/Freezer', [REFRIGERATOR],
-             'Electrohogar > Refrigeración > Freezer', 1.0],
-            ['Electrohogar/Refrigeración/Frigobar', [REFRIGERATOR],
-             'Electrohogar > Refrigeración > Frigobar', 1.0],
-            ['Electrohogar/Lavado_y_Planchado', [WASHING_MACHINE],
-             'Electrohogar > Lavado y Planchado', 1.0],
-            ['Electrohogar/Lavado_y_Planchado/Lavadoras', [WASHING_MACHINE],
-             'Electrohogar > Lavado y Planchado > Lavadoras', 1.0],
-            ['Electrohogar/Lavado_y_Planchado/Lavadoras_Secadoras',
-             [WASHING_MACHINE],
-             'Electrohogar > Lavado y Planchado > Lavadoras Secadoras', 1.0],
-            ['Electrohogar/Lavado_y_Planchado/Secadoras', [WASHING_MACHINE],
-             'Electrohogar > Lavado y Planchado > Secadoras', 1.0],
-            ['Electrohogar/Lavado_y_Planchado/Lavavajillas', [DISH_WASHER],
-             'Electrohogar > Lavado y Planchado > Lavavajillas', 1.0],
-            ['Electrohogar/Aspiradoras_y_Limpieza', [VACUUM_CLEANER],
-             'Electrohogar > Aspiradoras y Limpieza', 1.0],
-            ['Electrohogar/Electrodomésticos_Cocina/Hornos_Eléctricos', [OVEN],
-             'Electrohogar > Electrodomésticos Cocina > Hornos Eléctricos',
-             1.0],
-            ['Electrohogar/Electrodomésticos_Cocina/Microondas', [OVEN],
-             'Electrohogar > Electrodomésticos Cocina > Microondas', 1.0],
-            ['Electrohogar/Cocinas/Hornos_Empotrables', [OVEN],
-             'Electrohogar > Cocinas > Hornos Empotrables', 1.0],
-            ['Electrohogar/Climatización/Calefacción', [SPACE_HEATER],
-             'Electrohogar > Climatización > Calefacción', 1.0],
-            ['Electrohogar/Climatización/Termos_y_Calefonts', [WATER_HEATER],
-             'Electrohogar > Climatización > Termos y Calefonts', 1.0],
-            ['Electrohogar/Climatización/Ventilación/Aire_Acondicionado',
-             [AIR_CONDITIONER],
-             'Electrohogar > Climatización > Ventilación > Aire Acondicionado',
-             1.0],
-        ]
-
+        category_paths = cls.category_paths
         extra_args = extra_args or {}
         session = session_with_proxy(extra_args)
 
         session.headers = {
             'Content-Type': 'application/json',
-            'User-Agent': extra_args.get('user_agent', cls.DEFAULT_USER_AGENT)
+            'User-Agent': extra_args.get('user_agent', cls.DEFAULT_USER_AGENT),
+            'tenant': cls.tenant
         }
 
         product_entries = defaultdict(lambda: [])
@@ -231,9 +234,9 @@ class Lider(Store):
                     logging.warning('Empty category: ' + category_id)
 
                 for idx, entry in enumerate(data['products']):
-                    product_url = 'https://www.lider.cl/catalogo/' \
+                    product_url = 'https://www.lider.cl/{}/' \
                                   'product/sku/{}/{}'.format(
-                                      entry['sku'], entry.get('slug', 'a'))
+                                      cls.tenant, entry['sku'], entry.get('slug', 'a'))
                     if product_url not in local_product_entries:
                         local_product_entries[product_url] = {
                             'category_weight': category_weight,
@@ -252,6 +255,7 @@ class Lider(Store):
         session = session_with_proxy(extra_args)
         session.headers['User-Agent'] = extra_args.get('user_agent',
                                                        cls.DEFAULT_USER_AGENT)
+        session.headers['tenant'] = cls.tenant
         product_urls = []
 
         query_url = 'https://529cv9h7mw-dsn.algolia.net/1/indexes/*/' \
@@ -286,7 +290,8 @@ class Lider(Store):
         extra_args = extra_args or {}
         session = session_with_proxy(extra_args)
         session.headers = {
-            'User-Agent': extra_args.get('user_agent', cls.DEFAULT_USER_AGENT)
+            'User-Agent': extra_args.get('user_agent', cls.DEFAULT_USER_AGENT),
+            'tenant': cls.tenant
         }
         sku_id = url.split('/')[-2]
 
--- storescraper/stores/lider_supermarket.py
+++ storescraper/stores/lider_supermarket.py
@@ -1,125 +1,23 @@
-from decimal import Decimal
-
-import demjson
-from bs4 import BeautifulSoup
-
 from storescraper.categories import GROCERIES
-from storescraper.product import Product
-from storescraper.store import Store
-from storescraper.utils import session_with_proxy, check_ean13
+from storescraper.stores.lider import Lider
 
 
-class LiderSupermarket(Store):
+class LiderSupermarket(Lider):
+    tenant = 'supermercado'
+    category_paths = [
+        ['Despensa/Pastas y Salsas', [GROCERIES], 'Despensa > Pastas y Salsas', 1],
+        ['Despensa/Harinas', [GROCERIES], 'Despensa > Harinas', 1],
+        ['Despensa/Arroz y Legumbres', [GROCERIES], 'Despensa > Arroz y Legumbres', 1],
+        ['Despensa/Salsas', [GROCERIES], 'Despensa > Salsas', 1],
+        ['Despensa/Aceites y Aderezos', [GROCERIES], 'Despensa > Aceites y Aderezos', 1],
+        ['Despensa/Cóctel y Snack', [GROCERIES], 'Despensa > Cóctel y Snack', 1],
+        ['Despensa/Conservas', [GROCERIES], 'Despensa > Conservas', 1],
+        ['Despensa/Alimentos Instantáneos', [GROCERIES], 'Despensa > Alimentos Instantáneos', 1],
+        ['Despensa/Comida Étnica', [GROCERIES], 'Despensa > Comida Étnica', 1],
+    ]
+
     @classmethod
     def categories(cls):
         return [
             GROCERIES
         ]
-
-    @classmethod
-    def discover_urls_for_category(cls, category, extra_args=None):
-        if category != GROCERIES:
-            return []
-
-        session = session_with_proxy(extra_args)
-        session.headers['User-Agent'] = 'solotodobot'
-
-        # Get a starting page just to get the section IDs
-        url = 'https://www.lider.cl/supermercado/category/a/_/N-qs16h7'
-        res = session.get(url)
-        soup = BeautifulSoup(res.text, 'html.parser')
-        sections = soup.find(
-            'div', 'sidebar-categorias').findAll('div', 'panel-group')
-
-        product_urls = []
-
-        for section in sections:
-            section_name = section.find('b').text.strip().upper()
-            if section_name != 'DESPENSA':
-                continue
-
-            section_links = section.findAll('a')
-            for section_link in section_links:
-                print(section_link.text.strip())
-                section_href = section_link['href']
-
-                if section_href == '#':
-                    print('=============')
-                    continue
-
-                if section_href.startswith('https://'):
-                    url = section_href
-                else:
-                    url = 'https://www.lider.cl{}?Nrpp=1000'.format(
-                        section_href)
-
-                # print(url)
-                res = session.get(url)
-                soup = BeautifulSoup(res.text, 'html.parser')
-
-                product_entries = soup.findAll('div', 'box-product')
-                for product_entry in product_entries:
-                    product_urls.append('https://www.lider.cl' +
-                                        product_entry.find('a')['href'])
-
-        return product_urls
-
-    @classmethod
-    def products_for_url(cls, url, category=None, extra_args=None):
-        print(url)
-        session = session_with_proxy(extra_args)
-        session.headers['User-Agent'] = 'solotodobot'
-        res = session.get(url)
-        soup = BeautifulSoup(res.text, 'html.parser')
-        name = soup.find('h1').text.strip()
-
-        json_tag = soup.find('script', {'type': 'application/ld+json'})
-
-        if not json_tag:
-            return []
-
-        json_data = demjson.decode(json_tag.text)
-
-        if 'lowPrice' not in json_data['offers']:
-            return []
-
-        price = Decimal(json_data['offers']['lowPrice'])
-        sku = json_data['sku']
-        description = json_data['description']
-
-        ean = None
-        if 'gtin13' in json_data:
-            ean = json_data['gtin13']
-        if not check_ean13(ean):
-            ean = None
-
-        picture_urls = [json_data['image']]
-
-        stock_url = 'https://www.lider.cl/supermercado/includes/inventory/' \
-                    'inventoryInformation.jsp?productNumber={}&' \
-                    'consolidate=true'.format(sku)
-        stock_data = session.get(stock_url).json()
-
-        if stock_data[0]['stockLevel'] == '0':
-            stock = 0
-        else:
-            stock = -1
-
-        p = Product(
-            name,
-            cls.__name__,
-            category,
-            url,
-            url,
-            sku,
-            stock,
-            price,
-            price,
-            'CLP',
-            sku=sku,
-            ean=ean,
-            picture_urls=picture_urls,
-            description=description,
-        )
-
-        return [p]
