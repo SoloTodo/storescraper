3095b78c6ebaf1beda0adcf077aa70a6ca187d4c
Max Edwards <max@solotodo.com>
2022-10-25 09:44:37 -0300

    fix product fetch

--- storescraper/stores/marcimex.py
+++ storescraper/stores/marcimex.py
@@ -1,7 +1,5 @@
 import json
 import logging
-import urllib
-import re
 
 from decimal import Decimal
 
@@ -9,7 +7,7 @@ from bs4 import BeautifulSoup
 
 from storescraper.product import Product
 from storescraper.store import Store
-from storescraper.utils import html_to_markdown, session_with_proxy
+from storescraper.utils import session_with_proxy
 from storescraper.categories import AIR_CONDITIONER, OVEN, WASHING_MACHINE, \
     REFRIGERATOR, STEREO_SYSTEM, TELEVISION
 
@@ -29,13 +27,7 @@ class Marcimex(Store):
     @classmethod
     def discover_urls_for_category(cls, category, extra_args=None):
         category_paths = [
-            ['C:/2/14/', AIR_CONDITIONER],
-            ['C:/2/11/', OVEN],
-            ['C:/2/12/', WASHING_MACHINE],
-            ['C:/2/13/', REFRIGERATOR],
-            ['C:/3/28/', STEREO_SYSTEM],
-            ['C:/3/29/', TELEVISION],
-            ['C:/5/50/', OVEN]
+            ['lg', WASHING_MACHINE],
         ]
 
         session = session_with_proxy(extra_args)
@@ -51,14 +43,13 @@ class Marcimex(Store):
                 if page > 30:
                     raise Exception('Page overflow')
 
-                url = 'https://www.marcimex.com/buscapagina?fq={}&' \
-                      'PS=12&sl=2d6db67e-7134-4424-be62-6bbea419c476&cc=12&' \
-                      'sm=0&PageNumber={}&fq=B:2000002'.format(
-                          urllib.parse.quote_plus(category_path), page)
+                url = 'https://www.marcimex.com/{}?page={}'.format(
+                    category_path, page)
 
                 soup = BeautifulSoup(session.get(url).text, 'html.parser')
 
-                products = soup.findAll('div', 'productVitrine')
+                products = soup.findAll(
+                    'section', 'vtex-product-summary-2-x-container')
 
                 if not products:
                     if page == 1:
@@ -67,7 +58,8 @@ class Marcimex(Store):
 
                 for product in products:
                     product_url = product.find('a')['href']
-                    product_urls.append(product_url)
+                    product_urls.append(
+                        'https://www.marcimex.com' + product_url)
 
                 page += 1
 
@@ -80,31 +72,30 @@ class Marcimex(Store):
         response = session.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
 
-        scripts = soup.findAll('script')
-        product_data = [s for s in scripts if 'var skuJson' in s.text]
-
-        if product_data:
-            product_data = product_data[0].text
-        else:
-            raise Exception('No Data')
-
-        product_json = json.loads(re.search(
-            r'var skuJson_0 = ([\S\s]+?);', product_data).groups()[0])
+        product_json = json.loads(
+            soup.find('script', {'type': 'application/ld+json'}).text)
 
         name = product_json['name']
-        sku = str(product_json['skus'][0]['sku'])
-        stock = -1
-        if not product_json['available']:
-            # Products with no stock have wrong prices for some reason
-            return []
-        price = (
-            Decimal(product_json['skus'][0]['bestPrice']/100.0) +
-            Decimal(product_json['skus'][0]['taxAsInt']/100.0)
-            ).quantize(Decimal('.01'))
-        picture_urls = [product_json['skus'][0]['image']]
+        sku = str(product_json['sku'])
+        description = product_json['description']
 
-        description = html_to_markdown(
-            str(soup.find('div', {'id': 'caracteristicas'})))
+        stock = int(
+            soup.find('span',
+                      'vtex-product-availability-0-x-lowStockHighlight').text)
+
+        product_div = soup.find(
+            'div', 'vtex-flex-layout-0-x-flexRow--quantity')
+        product_price = product_div.find(
+            'span', 'vtex-product-price-1-x-sellingPrice')
+
+        price = Decimal(product_price.find(
+            'span', 'vtex-product-price-1-x-currencyContainer')
+            .text.replace('$', '').replace(',', '.'))
+
+        picture_urls = []
+        picture_container = soup.findAll('div', 'swiper-wrapper')[1]
+        for i in picture_container.findAll('img'):
+            picture_urls.append(i['src'])
 
         p = Product(
             name,
