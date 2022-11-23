b7e2d3588d65c508deaf91e2fda51abd8eda71b5
Max Edwards <max@solotodo.com>
2022-11-23 12:43:27 -0300

    update scraper

--- storescraper/stores/crazy_games_en_chile.py
+++ storescraper/stores/crazy_games_en_chile.py
@@ -4,11 +4,11 @@ from decimal import Decimal
 
 from bs4 import BeautifulSoup
 
-from storescraper.categories import GAMING_DESK, HEADPHONES, COMPUTER_CASE, \
+from storescraper.categories import HEADPHONES, COMPUTER_CASE, \
     MICROPHONE, SOLID_STATE_DRIVE, RAM, MONITOR, MOUSE, GAMING_CHAIR, \
     KEYBOARD, POWER_SUPPLY, MOTHERBOARD, PROCESSOR, STEREO_SYSTEM, \
     USB_FLASH_DRIVE, VIDEO_CARD, CPU_COOLER, EXTERNAL_STORAGE_DRIVE, \
-    VIDEO_GAME_CONSOLE
+    KEYBOARD_MOUSE_COMBO
 from storescraper.product import Product
 from storescraper.store import Store
 from storescraper.utils import session_with_proxy, html_to_markdown
@@ -32,40 +32,37 @@ class CrazyGamesenChile(Store):
             VIDEO_CARD,
             CPU_COOLER,
             EXTERNAL_STORAGE_DRIVE,
-            VIDEO_GAME_CONSOLE,
             MICROPHONE,
             STEREO_SYSTEM,
-            GAMING_DESK,
-            USB_FLASH_DRIVE
+            USB_FLASH_DRIVE,
+            KEYBOARD_MOUSE_COMBO
         ]
 
     @classmethod
     def discover_urls_for_category(cls, category, extra_args=None):
         url_extensions = [
-            ['audifonos-gamers', HEADPHONES],
-            ['audifonos', HEADPHONES],
-            ['audifonos-home-office', HEADPHONES],
-            ['microfonos', MICROPHONE],
-            ['parlantes', STEREO_SYSTEM],
-            ['teclados-ps4-y-xbox-one', KEYBOARD],
-            ['teclados', KEYBOARD],
-            ['mouse-gamer', MOUSE],
-            ['mouse', MOUSE],
+            ['kit-gamer', KEYBOARD_MOUSE_COMBO],
             ['monitores', MONITOR],
-            ['escritorios-gamer', GAMING_DESK],
+            ['mouse-gamer', MOUSE],
+            ['mouse-oficina', MOUSE],
             ['sillas-gamer', GAMING_CHAIR],
+            ['teclados-gamer', KEYBOARD],
+            ['teclados-oficina', KEYBOARD],
+            ['audifonos-gamer', HEADPHONES],
+            ['audifonos-de-musica', HEADPHONES],
+            ['audifonos-home-office', HEADPHONES],
+            ['microfonos-1', MICROPHONE],
+            ['parlantes', STEREO_SYSTEM],
+            ['discos-internos', SOLID_STATE_DRIVE],
+            ['discos-externos', EXTERNAL_STORAGE_DRIVE],
+            ['memorias-y-pendrive', USB_FLASH_DRIVE],
             ['fuentes-de-poder', POWER_SUPPLY],
-            ['gabinetes-pc', COMPUTER_CASE],
-            ['gabinetes-gamer', COMPUTER_CASE],
+            ['gabinetes', COMPUTER_CASE],
             ['memorias-ram', RAM],
             ['placas-madres', MOTHERBOARD],
             ['procesadores', PROCESSOR],
             ['tarjetas-de-video', VIDEO_CARD],
-            ['fan-cooler-pc', CPU_COOLER],
-            ['discos-duros-internos', SOLID_STATE_DRIVE],
-            ['discos-duros-externos', EXTERNAL_STORAGE_DRIVE],
-            ['memorias-pendrives', USB_FLASH_DRIVE],
-            ['consolas', VIDEO_GAME_CONSOLE],
+            ['ventiladores-y-refrigeracion-liquida', CPU_COOLER],
         ]
         session = session_with_proxy(extra_args)
         product_urls = []
@@ -73,7 +70,7 @@ class CrazyGamesenChile(Store):
             if local_category != category:
                 continue
 
-            url_webpage = 'https://www.crazygamesenchile.com/{}?page=20' \
+            url_webpage = 'https://www.crazygamesenchile.com/collections/{}' \
                 .format(url_extension)
             print(url_webpage)
             res = session.get(url_webpage)
@@ -82,14 +79,14 @@ class CrazyGamesenChile(Store):
                 raise Exception('Invalid category: ' + url_extension)
 
             soup = BeautifulSoup(res.text, 'html.parser')
-            product_containers = soup.findAll('li', {
-                'data-hook': 'product-list-grid-item'})
+            product_containers = soup.findAll('li', 'grid__item')
             if not product_containers:
                 logging.warning('Empty category: ' + url_extension)
                 continue
             for container in product_containers:
                 product_url = container.find('a')['href']
-                product_urls.append(product_url)
+                product_urls.append(
+                    'https://www.crazygamesenchile.com/' + product_url)
         return product_urls
 
     @classmethod
@@ -98,73 +95,40 @@ class CrazyGamesenChile(Store):
         session = session_with_proxy(extra_args)
         response = session.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
-        sku_container = json.loads(
-            soup.find('script', {'id': 'wix-warmup-data'}).text)
-        sku_key_1 = list(sku_container['appsWarmupData'].keys())[0]
-        sku_key_2 = list(sku_container['appsWarmupData'][sku_key_1].keys())[0]
-        product_data = sku_container['appsWarmupData'][sku_key_1][
-            sku_key_2]['catalog']['product']
-        base_name = product_data['name']
-        description = html_to_markdown(product_data['description'])
-        base_picture_urls = [x['fullUrl'] for x in product_data['media']]
-        if len(product_data['productItems']) > 1:
-            assert len(product_data['options']) == 1
-            variation_options = {x['id']: x for x in product_data[
-                'options'][0]['selections']}
-            products = []
 
-            for sku_data in product_data['productItems']:
-                variation_key = sku_data['id']
-                variation_sku = sku_data['sku'] or None
-                variation_stock = sku_data['inventory']['quantity']
-                variation_price = Decimal(sku_data['comparePrice']) or \
-                    Decimal(sku_data['price'])
-                assert variation_price or not variation_stock
-                assert len(sku_data['optionsSelections']) == 1
-                variation_option_key = sku_data['optionsSelections'][0]
-                variation_option = variation_options[variation_option_key]
-                variation_name = '{} - ({})'.format(
-                    base_name, variation_option['key'])
-                picture_urls = [x['fullUrl'] for x in
-                                variation_option['linkedMediaItems'] or []]
-                p = Product(
-                    variation_name,
-                    cls.__name__,
-                    category,
-                    url,
-                    url,
-                    variation_key,
-                    variation_stock,
-                    variation_price,
-                    variation_price,
-                    'CLP',
-                    sku=variation_sku,
-                    picture_urls=picture_urls or base_picture_urls,
-                    description=description
-                )
-                products.append(p)
-            return products
+        key = soup.find('input', {'name': 'id'})['value']
 
+        json_data = json.loads(soup.findAll(
+            'script', {'type': 'application/ld+json'})[-1].text)
+
+        name = json_data['name']
+        sku = json_data['sku']
+        description = json_data['description']
+        price = Decimal(str(json_data['offers'][0]['price']))
+
+        if 'disabled' in soup.find('button', 'product-form__submit').attrs:
+            stock = 0
         else:
-            assert len(product_data['productItems']) == 1
-            price = Decimal(product_data['discountedPrice'])
-            key = product_data['id']
-            sku = product_data['sku']
-            stock = product_data['inventory']['quantity']
-            assert price or not stock
-            p = Product(
-                base_name,
-                cls.__name__,
-                category,
-                url,
-                url,
-                key,
-                stock,
-                price,
-                price,
-                'CLP',
-                sku=sku,
-                picture_urls=base_picture_urls,
-                description=description
-            )
-            return [p]
+            stock = -1
+
+        picture_container = soup.find('ul', 'product__media-list')
+        picture_urls = []
+        for a in picture_container.findAll('li'):
+            picture_urls.append('https:' + a.find('img')['src'])
+
+        p = Product(
+            name,
+            cls.__name__,
+            category,
+            url,
+            url,
+            sku,
+            stock,
+            price,
+            price,
+            'CLP',
+            sku=key,
+            picture_urls=picture_urls,
+            description=description
+        )
+        return [p]
