5c05105458ad695e942e5c1be937f4ca629ee66b
Max Edwards <max@solotodo.com>
2022-12-14 16:27:15 -0300

    fix stock and variants

--- storescraper/stores/mutant.py
+++ storescraper/stores/mutant.py
@@ -1,4 +1,5 @@
 from decimal import Decimal
+import json
 import logging
 from bs4 import BeautifulSoup
 from storescraper.categories import CPU_COOLER, MOUSE
@@ -50,19 +51,7 @@ class Mutant(Store):
         response = session.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
 
-        key = soup.find('link', {'rel': 'shortlink'})['href'].split('p=')[1]
-
         name = soup.find('h1', 'product_title').text.strip()
-
-        price_container = soup.find('p', 'price')
-        price = Decimal(remove_words(
-            price_container.findAll('span', 'amount')[-1].text))
-
-        stock_container = soup.find('p', 'stock in-stock')
-        if stock_container:
-            stock = int(stock_container.text.split(' ')[0])
-        else:
-            stock = 0
 
         description = html_to_markdown(
             soup.find('div', {'id': 'tab-description'}).text)
@@ -73,18 +62,64 @@ class Mutant(Store):
         for a in picture_container.findAll('a'):
             picture_urls.append(a['href'])
 
-        p = Product(
-            name,
-            cls.__name__,
-            category,
-            url,
-            url,
-            key,
-            stock,
-            price,
-            price,
-            'CLP',
-            picture_urls=picture_urls,
-            description=description
-        )
-        return [p]
+        variations = soup.find('form', 'variations_form')
+        if variations:
+            products = []
+            variants = json.loads(variations['data-product_variations'])
+            for v in variants:
+                key = str(v['variation_id'])
+                attr = ' '.join(list(v['attributes'].values()))
+                variant_name = '{} - {}'.format(name, attr)
+                sku = v['sku']
+                price = Decimal(v['display_price'])
+                stock = v['max_qty']
+
+                p = Product(
+                    variant_name,
+                    cls.__name__,
+                    category,
+                    url,
+                    url,
+                    key,
+                    stock,
+                    price,
+                    price,
+                    'CLP',
+                    sku=sku,
+                    picture_urls=picture_urls,
+                    description=description
+                )
+                products.append(p)
+
+            return products
+        else:
+            key = soup.find('link', {'rel': 'shortlink'})[
+                'href'].split('p=')[1]
+
+            price_container = soup.find('p', 'price')
+            price = Decimal(remove_words(
+                price_container.findAll('span', 'amount')[-1].text))
+
+            stock_container = soup.find('p', 'stock in-stock')
+            if stock_container:
+                stock = int(stock_container.text.split(' ')[0])
+            elif soup.find('button', {'name': 'add-to-cart'}):
+                stock = -1
+            else:
+                stock = 0
+
+            p = Product(
+                name,
+                cls.__name__,
+                category,
+                url,
+                url,
+                key,
+                stock,
+                price,
+                price,
+                'CLP',
+                picture_urls=picture_urls,
+                description=description
+            )
+            return [p]
