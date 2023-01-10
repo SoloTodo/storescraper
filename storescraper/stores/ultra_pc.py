5abcb26d1499c3051653ea3841af75dcd40c987c
Vijay Khemlani <vj@solotodo.com>
2023-01-10 11:01:58 -0300

    * Updated UltraPC

--- storescraper/stores/ultra_pc.py
+++ storescraper/stores/ultra_pc.py
@@ -1,3 +1,5 @@
+import html
+import json
 from decimal import Decimal
 
 from bs4 import BeautifulSoup
@@ -62,56 +64,95 @@ class UltraPc(Store):
             '(KHTML, like Gecko) Chrome/62.0.3202.62 Safari/537.36'
         response = session.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
-        name = soup.find('h1', 'product_title').text
-        sku = soup.find('link', {'type': 'application/json'})['href'].split(
-            '/')[-1]
-        if soup.find('span', 'electro-stock-availability').find('p', 'stock'):
-            stock = -1
-        else:
-            stock = 0
+        base_name = soup.find('h1', 'product_title').text
 
-        offer_price_tags = ['product:sale_price:amount',
-                            'product:price:amount']
-        for tag in offer_price_tags:
-            price_tag = soup.find('meta', {'property': tag})
-            if price_tag:
-                offer_price = Decimal(price_tag['content'])
-                break
-        else:
-            raise Exception('No offer price found')
+        variants = soup.find('form', 'variations_form')
+        products = []
 
-        normal_price_tags = ['precio_oferta', 'precio_con_iva_tbk']
-        for tag in normal_price_tags:
-            price_tag = soup.find('span', tag)
-            if price_tag:
-                normal_price = Decimal(remove_words(price_tag.text))
-                break
-        else:
-            raise Exception('No normal price found')
+        if variants:
+            container_products = json.loads(
+                html.unescape(variants['data-product_variations']))
 
-        picture_urls = [tag['src'] for tag in soup.find(
-            'div', 'woocommerce-product-gallery').findAll(
-            'img')]
-        condition_text = soup.find(
-            'span', 'condicion_item_ultrapc').text.strip()
-        if condition_text == 'NUEVO':
-            condition = 'https://schema.org/NewCondition'
+            for product in container_products:
+                variant_name = base_name + " - " + next(
+                    iter(product['attributes'].values()))
+                if product['is_in_stock']:
+                    stock = int(product['max_qty'])
+                else:
+                    stock = 0
+                sku = product['sku']
+                key = str(product['variation_id'])
+                price = Decimal(product['display_price'])
+                if product['image']['src'] == '':
+                    picture_urls = [tag['src'] for tag in
+                                    soup.find('div', 'woocommerce-product'
+                                                     '-gallery').findAll(
+                                        'img')]
+                else:
+                    picture_urls = [product['image']['src']]
+                products.append(Product(
+                    variant_name,
+                    cls.__name__,
+                    category,
+                    url,
+                    url,
+                    key,
+                    stock,
+                    price,
+                    price,
+                    'CLP',
+                    sku=sku,
+                    picture_urls=picture_urls
+                ))
         else:
-            condition = 'https://schema.org/RefurbishedCondition'
+            key = soup.find('link', {'type': 'application/json'})['href'].split(
+                '/')[-1]
+            json_data = json.loads(soup.findAll(
+                'script', {'type': 'application/ld+json'})[1].text)['@graph'][1]
+            sku = json_data['sku']
+            description = json_data['description']
 
-        p = Product(
-            name,
-            cls.__name__,
-            category,
-            url,
-            url,
-            sku,
-            stock,
-            normal_price,
-            offer_price,
-            'CLP',
-            sku=sku,
-            picture_urls=picture_urls,
-            condition=condition
-        )
-        return [p]
+            if soup.find('span', 'electro-stock-availability').find('p', 'stock'):
+                stock = -1
+            else:
+                stock = 0
+
+            offer_price = Decimal(
+                json_data['offers'][0]['priceSpecification']['price'])
+
+            normal_price_tags = ['precio_oferta', 'precio_con_iva_tbk']
+            for tag in normal_price_tags:
+                price_tag = soup.find('span', tag)
+                if price_tag:
+                    normal_price = Decimal(remove_words(price_tag.text))
+                    break
+            else:
+                raise Exception('No normal price found')
+
+            picture_urls = [tag['src'] for tag in soup.find(
+                'div', 'woocommerce-product-gallery').findAll(
+                'img')]
+            condition_text = soup.find(
+                'span', 'condicion_item_ultrapc').text.strip()
+            if condition_text == 'NUEVO':
+                condition = 'https://schema.org/NewCondition'
+            else:
+                condition = 'https://schema.org/RefurbishedCondition'
+
+            products.append(Product(
+                base_name,
+                cls.__name__,
+                category,
+                url,
+                url,
+                key,
+                stock,
+                normal_price,
+                offer_price,
+                'CLP',
+                sku=sku,
+                picture_urls=picture_urls,
+                condition=condition,
+                description=description
+            ))
+        return products
