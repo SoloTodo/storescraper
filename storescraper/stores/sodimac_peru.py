9a9d2af5dad7f39f484769a2c2f8d8a100359abd
Vijay Khemlani <vj@solotodo.com>
2023-01-18 17:04:29 -0300

    * Updated Sodimac, Tottus

--- storescraper/stores/sodimac_peru.py
+++ storescraper/stores/sodimac_peru.py
@@ -6,7 +6,8 @@ class SodimacPeru(FalabellaPeru):
 
     @classmethod
     def products_for_url(cls, url, category=None, extra_args=None):
-        products = super(SodimacPeru, cls).products_for_url(url, category=None, extra_args=None)
+        products = super(SodimacPeru, cls).products_for_url(
+            url, category=category, extra_args=extra_args)
         for product in products:
             product.seller = None
         return products
--- storescraper/stores/tottus_peru.py
+++ storescraper/stores/tottus_peru.py
@@ -1,10 +1,12 @@
 from .falabella_peru import FalabellaPeru
 
+
 class TottusPeru(FalabellaPeru):
     seller = 'TOTTUS'
     @classmethod
     def products_for_url(cls, url, category=None, extra_args=None):
-        products = super(SodimacPeru, cls).products_for_url(url, category=None, extra_args=None)
+        products = super(TottusPeru, cls).products_for_url(
+            url, category=category, extra_args=extra_args)
         for product in products:
             product.seller = None
         return products
