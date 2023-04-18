e41f32656ee654d562ee55f4b0b1d9455ad1d68c
Max Edwards <max@solotodo.com>
2023-04-18 12:50:54 -0400

    refactor scraper

--- storescraper/stores/a_z_tech.py
+++ storescraper/stores/a_z_tech.py
@@ -1,11 +1,12 @@
 from decimal import Decimal
-import demjson
+import json
 import logging
 from bs4 import BeautifulSoup
-from storescraper.categories import ALL_IN_ONE, CELL, EXTERNAL_STORAGE_DRIVE, \
-    HEADPHONES, MONITOR, MOUSE, NOTEBOOK, POWER_SUPPLY, PRINTER, PROCESSOR, \
-    SOLID_STATE_DRIVE, STEREO_SYSTEM, TABLET, TELEVISION, VIDEO_CARD, \
-    VIDEO_GAME_CONSOLE, WEARABLE, RAM
+from storescraper.categories import ALL_IN_ONE, CELL, COMPUTER_CASE, \
+    CPU_COOLER, HEADPHONES, KEYBOARD, KEYBOARD_MOUSE_COMBO, MEMORY_CARD, \
+    MICROPHONE, MONITOR, MOTHERBOARD, MOUSE, NOTEBOOK, POWER_SUPPLY, PRINTER, \
+    PROCESSOR, SOLID_STATE_DRIVE, STEREO_SYSTEM, TABLET, TELEVISION, \
+    USB_FLASH_DRIVE, VIDEO_CARD, VIDEO_GAME_CONSOLE, WEARABLE, RAM
 from storescraper.product import Product
 from storescraper.store import Store
 from storescraper.utils import remove_words, session_with_proxy
@@ -16,62 +17,76 @@ class AZTech(Store):
     def categories(cls):
         return [
             NOTEBOOK,
-            ALL_IN_ONE,
-            TABLET,
+            USB_FLASH_DRIVE,
             SOLID_STATE_DRIVE,
+            MEMORY_CARD,
+            ALL_IN_ONE,
+            KEYBOARD_MOUSE_COMBO,
+            MOUSE,
+            KEYBOARD,
+            COMPUTER_CASE,
+            TABLET,
             MONITOR,
-            WEARABLE,
-            EXTERNAL_STORAGE_DRIVE,
+            MOTHERBOARD,
             PROCESSOR,
             RAM,
-            VIDEO_CARD,
             POWER_SUPPLY,
-            VIDEO_GAME_CONSOLE,
+            VIDEO_CARD,
+            CPU_COOLER,
+            WEARABLE,
+            HEADPHONES,
             TELEVISION,
             PRINTER,
             STEREO_SYSTEM,
             CELL,
-            MOUSE,
-            HEADPHONES
+            VIDEO_GAME_CONSOLE,
+            MICROPHONE,
         ]
 
     @classmethod
     def discover_urls_for_category(cls, category, extra_args=None):
         url_extensions = [
-            ['computacion/procesadores', PROCESSOR],
-            ['computacion/notebook', NOTEBOOK],
-            ['computacion/escritorio', ALL_IN_ONE],
-            ['computacion/memorias-ram', RAM],
-            ['computacion/tablet', TABLET],
-            ['computacion/perifericos', MOUSE],
-            ['computacion/tarjetas-de-video', VIDEO_CARD],
-            ['computacion/fuente-de-poder', POWER_SUPPLY],
-            ['computacion/almacenamiento-datos', SOLID_STATE_DRIVE],
-            ['computacion/monitores', MONITOR],
-            ['mundo-apple/mac/portatil/macbook-pro', NOTEBOOK],
-            ['mundo-apple/mac/portatil/macbook-air', NOTEBOOK],
-            ['mundo-apple/mac/escritorio/imac', ALL_IN_ONE],
-            ['mundo-apple/ipad/ipad-pro', TABLET],
-            ['mundo-apple/ipad/ipad-air', TABLET],
-            ['mundo-apple/ipad/ipad-102', TABLET],
-            ['mundo-apple/ipad/ipad-mini', TABLET],
+            ['notebooks', NOTEBOOK],
+            ['pendrive', USB_FLASH_DRIVE],
+            ['discos-duros', SOLID_STATE_DRIVE],
+            ['tarjeta-flash-micro-sd', MEMORY_CARD],
+            ['pc-all-in-one', ALL_IN_ONE],
+            ['kits', KEYBOARD_MOUSE_COMBO],
+            ['mouse', MOUSE],
+            ['teclados', KEYBOARD],
+            ['gabinetes', COMPUTER_CASE],
+            ['tablets', TABLET],
+            ['monitor', MONITOR],
+            ['placas-madre', MOTHERBOARD],
+            ['procesadores', PROCESSOR],
+            ['memorias-ram', RAM],
+            ['fuentes-de-poder', POWER_SUPPLY],
+            ['tarjetas-de-video', VIDEO_CARD],
+            ['refrigeracion-y-ventilacion', CPU_COOLER],
+            ['macbook-pro', NOTEBOOK],
+            ['macbook-air', NOTEBOOK],
+            ['imac', ALL_IN_ONE],
+            ['ipad-pro', TABLET],
+            ['ipad-air', TABLET],
+            ['ipad-10-2', TABLET],
+            ['ipad-mini', TABLET],
             ['apple-watch-ultra', WEARABLE],
             ['apple-watch-series-8', WEARABLE],
-            ['mundo-apple/watch/apple-watch-se-2da-gen', WEARABLE],
-            ['mundo-apple/watch/apple-watch-series-7', WEARABLE],
-            ['mundo-apple/airpods', HEADPHONES],
-            ['mundo-apple/accesorios/almacenamiento', EXTERNAL_STORAGE_DRIVE],
-            ['hogar-y-oficina/television', TELEVISION],
-            ['hogar-y-oficina/impresoras', PRINTER],
-            ['musica/parlantes', STEREO_SYSTEM],
-            ['musica/audifonos/in-ear', HEADPHONES],
-            ['musica/on-ear', HEADPHONES],
-            ['musica/audifonos/over-ear', HEADPHONES],
-            ['musica/audifonos/gamers-y-streamers', HEADPHONES],
-            ['celulares/marcas-1', CELL],
-            ['outdoor-tech/wearables', WEARABLE],
-            ['mundo-gamers/consolas', VIDEO_GAME_CONSOLE],
-            ['mundo-gamers/perifericos', MOUSE],
+            ['apple-watch-se-2-gen', WEARABLE],
+            ['airpods', HEADPHONES],
+            ['smart-tv', TELEVISION],
+            ['impresoras-laser', PRINTER],
+            ['multifuncionales', PRINTER],
+            ['impresoras-de-tinta', PRINTER],
+            ['parlantes', STEREO_SYSTEM],
+            ['audifonos', HEADPHONES],
+            ['marcas', CELL],
+            ['wearables', WEARABLE],
+            ['consolas', VIDEO_GAME_CONSOLE],
+            ['microfonos-streamers', MICROPHONE],
+            ['audifonos-gamer', HEADPHONES],
+            ['mouse-gamer', MOUSE],
+            ['teclados-gamer', KEYBOARD],
         ]
 
         session = session_with_proxy(extra_args)
@@ -83,81 +98,67 @@ class AZTech(Store):
             while True:
                 if page > 10:
                     raise Exception('Page overflow: ' + url_extension)
-                url_webpage = 'https://azt.cl/{}?page={}'.format(
-                    url_extension, page)
+                url_webpage = 'https://onesolution.cl/collections/{}?page' \
+                    '={}'.format(url_extension, page)
                 data = session.get(url_webpage).text
                 soup = BeautifulSoup(data, 'html.parser')
                 product_containers = soup.findAll(
-                    'div', 'product-block__wrapper')
+                    'li', 'product')
                 if not product_containers or len(product_containers) == 0:
                     if page == 1:
                         logging.warning('Empty category: ' + url_extension)
                     break
                 for container in product_containers:
                     product_url = container.find('a')['href']
-                    product_urls.append('https://azt.cl' + product_url)
+                    product_urls.append('https://onesolution.cl' + product_url)
                 page += 1
         return product_urls
 
     @classmethod
     def products_for_url(cls, url, category=None, extra_args=None):
         print(url)
-        extra_args = extra_args or {}
-        if 'proxy' not in extra_args:
-            raise Exception('AZTech lists prices without IVA when accessed '
-                            'from outside Chile, so a chilean proxy is '
-                            'mandatory for this scraper')
         session = session_with_proxy(extra_args)
         response = session.get(url)
 
         soup = BeautifulSoup(response.text, 'html.parser')
-        json_data = None
         try:
-            json_data = demjson.decode(soup.find(
-                'script', {'type': 'application/ld+json'}).text)
+            json_data = json.loads(
+                soup.findAll('script', {'type': 'application/ld+json'}
+                             )[1].text)
         except Exception as e:
-            pass
-
-        key = soup.find('meta', {'property': 'og:id'})['content']
-        description = soup.find('meta', {'name': 'description'})['content']
-        if 'instock' in soup.find(
-                'meta', {'property': 'product:availability'})['content']:
-            stock = -1
-        else:
-            stock = 0
-
-        picture_urls = []
-        picture_container = soup.find('div', 'product-gallery__slider')
-        if picture_container:
-            for i in picture_container.findAll('img'):
-                picture_urls.append(i['src'])
-        else:
-            picture_urls.append(
-                soup.find('img', 'product-gallery__image')['data-src'])
+            json_data = None
 
         if json_data:
-            for entry in json_data:
-                if entry['@type'] == 'Product':
-                    product_data = entry
-                    break
-            else:
-                raise Exception('No JSON product data found')
+            key = json_data['productID']
+            name = json_data['name']
+            sku = json_data['sku']
+            description = json_data['description']
+            price = Decimal(remove_words(json_data['offers']['price']))
 
-            name = product_data['name']
-            sku = product_data['sku']
-            price = Decimal(product_data['offers']['price']).quantize(0)
-        else:
-            name = soup.find('h1', 'product-heading__title').text
-            sku = soup.find(
-                'span',
-                'product-heading__detail--sku').text.replace('SKU: ', '')
-            price_discount_tag = soup.find(
-                'h2', 'product-heading__pricing--has-discount')
-            if price_discount_tag:
-                price_text = price_discount_tag.find('span').text
+            if json_data['offers']['availability'] == \
+                    'http://schema.org/InStock':
+                stock = -1
             else:
-                price_text = soup.find('h2', 'product-heading__pricing').text
-            price = Decimal(remove_words(price_text))
+                stock = 0
+        else:
+            product_div = soup.find('div', 'productView-price')
+            key = product_div['id'].split('-')[-1]
+            name = soup.find('h1', 'productView-title').text.strip()
+            sku = soup.find('div', {'data-sku': True}).findAll('span')[-1].text
+            description = soup.find('div', 'productView-desc').text.strip()
+            price = Decimal(remove_words(
+                product_div.find('span', 'price-item').text))
+
+            if soup.find('div', {'data-inventory': True}
+                         ).findAll('span')[-1].text.upper() == 'EN STOCK':
+                stock = -1
+            else:
+                stock = 0
+
+        picture_urls = []
+        picture_container = soup.find('div', 'productView-image-wrapper')
+        for i in picture_container.findAll('img'):
+            picture_urls.append('https:' + i['src'])
 
         p = Product(
             name,
@@ -165,12 +166,12 @@ class AZTech(Store):
             category,
             url,
             url,
-            key,
+            sku,
             stock,
             price,
             price,
             'CLP',
-            sku=sku,
+            sku=key,
             part_number=sku,
             picture_urls=picture_urls,
             description=description
