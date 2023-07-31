5a0962186faaeaca04b6379598b01dea16be8826
Vijay Khemlani <vj@solotodo.com>
2023-07-31 10:25:18 -0400

    * Updated Lifemax

--- storescraper/stores/lifemax.py
+++ storescraper/stores/lifemax.py
@@ -9,118 +9,87 @@ from storescraper.categories import COMPUTER_CASE, EXTERNAL_STORAGE_DRIVE, \
     RAM, SOLID_STATE_DRIVE, STEREO_SYSTEM, STORAGE_DRIVE, USB_FLASH_DRIVE, \
     VIDEO_CARD, TABLET, ALL_IN_ONE
 from storescraper.product import Product
-from storescraper.store import Store
+from storescraper.store_with_url_extensions import StoreWithUrlExtensions
 from storescraper.utils import session_with_proxy, html_to_markdown
 
 
-class Lifemax(Store):
-    @classmethod
-    def categories(cls):
-        return [
-            COMPUTER_CASE,
-            EXTERNAL_STORAGE_DRIVE,
-            GAMING_CHAIR, HEADPHONES,
-            KEYBOARD,
-            KEYBOARD_MOUSE_COMBO,
-            MEMORY_CARD,
-            MONITOR,
-            MOTHERBOARD,
-            MOUSE,
-            NOTEBOOK,
-            POWER_SUPPLY,
-            PRINTER,
-            PROCESSOR,
-            RAM,
-            SOLID_STATE_DRIVE,
-            STEREO_SYSTEM,
-            STORAGE_DRIVE,
-            USB_FLASH_DRIVE,
-            VIDEO_CARD,
-            TABLET,
-            ALL_IN_ONE,
-        ]
+class Lifemax(StoreWithUrlExtensions):
+    url_extensions = [
+        ['audifonos-bluetooth', HEADPHONES],
+        ['audifonos-con-cable', HEADPHONES],
+        ['tarjeta-de-memoria-microsdxc', MEMORY_CARD],
+        ['tarjeta-de-memoria-microsd', MEMORY_CARD],
+        ['tarjeta-de-memoria-microsdhc', MEMORY_CARD],
+        ['discos-ssd', SOLID_STATE_DRIVE],
+        ['discos-ssd-m-2', SOLID_STATE_DRIVE],
+        ['discos-duros-externos', EXTERNAL_STORAGE_DRIVE],
+        ['memoria-ram-notebook', RAM],
+        ['memoria-ram-pc', RAM],
+        ['disco-duro-interno', STORAGE_DRIVE],
+        ['disco-ssd-externo', EXTERNAL_STORAGE_DRIVE],
+        ['monitores', MONITOR],
+        ['notebook-corporativos', NOTEBOOK],
+        ['impresoras-multifuncionales-tinta', PRINTER],
+        ['impresoras-laser', PRINTER],
+        ['pendrives', USB_FLASH_DRIVE],
+        ['tablets', TABLET],
+        ['all-in-one', ALL_IN_ONE],
+        ['mouse', MOUSE],
+        ['kits-de-mouse-y-teclado', KEYBOARD_MOUSE_COMBO],
+        ['teclados', KEYBOARD],
+        ['memorias-ram-pc-gaming', RAM],
+        ['memoria-ram-notebook-gamers', RAM],
+        ['ssd-m-2-especial-gamers', SOLID_STATE_DRIVE],
+        ['monitores-gamer', MONITOR],
+        ['procesadores', PROCESSOR],
+        ['gabinetes-gamer', COMPUTER_CASE],
+        ['placas-madre', MOTHERBOARD],
+        ['mouse-gamers', MOUSE],
+        ['fuentes-de-poder', POWER_SUPPLY],
+        ['sillas-gamer', GAMING_CHAIR],
+        ['tarjeta-de-video', VIDEO_CARD],
+        ['audifonos-gamers', HEADPHONES],
+        ['disco-duro-externo-especial-gamer', EXTERNAL_STORAGE_DRIVE],
+        ['parlantes-y-subwoofers', STEREO_SYSTEM],
+        ['audifonos-c-microfono', HEADPHONES],
+    ]
 
     @classmethod
-    def discover_urls_for_category(cls, category, extra_args=None):
-        category_paths = [
-            ['audifonos-bluetooth', HEADPHONES],
-            ['audifonos-con-cable', HEADPHONES],
-            ['tarjeta-de-memoria-microsdxc', MEMORY_CARD],
-            ['tarjeta-de-memoria-microsd', MEMORY_CARD],
-            ['tarjeta-de-memoria-microsdhc', MEMORY_CARD],
-            ['discos-ssd', SOLID_STATE_DRIVE],
-            ['discos-ssd-m-2', SOLID_STATE_DRIVE],
-            ['discos-duros-externos', EXTERNAL_STORAGE_DRIVE],
-            ['memoria-ram-notebook', RAM],
-            ['memoria-ram-pc', RAM],
-            ['disco-duro-interno', STORAGE_DRIVE],
-            ['disco-ssd-externo', EXTERNAL_STORAGE_DRIVE],
-            ['monitores', MONITOR],
-            ['notebook-corporativos', NOTEBOOK],
-            ['impresoras-multifuncionales-tinta', PRINTER],
-            ['impresoras-laser', PRINTER],
-            ['pendrives', USB_FLASH_DRIVE],
-            ['tablets', TABLET],
-            ['all-in-one', ALL_IN_ONE],
-            ['mouse', MOUSE],
-            ['kits-de-mouse-y-teclado', KEYBOARD_MOUSE_COMBO],
-            ['teclados', KEYBOARD],
-            ['memorias-ram-pc-gaming', RAM],
-            ['memoria-ram-notebook-gamers', RAM],
-            ['ssd-m-2-especial-gamers', SOLID_STATE_DRIVE],
-            ['monitores-gamer', MONITOR],
-            ['procesadores', PROCESSOR],
-            ['gabinetes-gamer', COMPUTER_CASE],
-            ['placas-madre', MOTHERBOARD],
-            ['mouse-gamers', MOUSE],
-            ['fuentes-de-poder', POWER_SUPPLY],
-            ['sillas-gamer', GAMING_CHAIR],
-            ['tarjeta-de-video', VIDEO_CARD],
-            ['audifonos-gamers', HEADPHONES],
-            ['disco-duro-externo-especial-gamer', EXTERNAL_STORAGE_DRIVE],
-            ['parlantes-y-subwoofers', STEREO_SYSTEM],
-            ['audifonos-c-microfono', HEADPHONES],
-        ]
-
+    def discover_urls_for_url_extension(cls, url_extension, extra_args):
         session = session_with_proxy(extra_args)
         product_urls = []
+        page = 1
 
-        for category_path, local_category in category_paths:
-            if category != local_category:
-                continue
+        while True:
+            if page > 10:
+                raise Exception('Page overflow: ' + url_extension)
 
-            page = 1
+            page_url = 'https://www.lifemaxstore.cl/collection/{}/' \
+                       '?page={}'.format(url_extension, page)
+            print(page_url)
+            response = session.get(page_url)
+            data = response.text
+            soup = BeautifulSoup(data, 'html5lib')
+            product_container = soup.find('div', 'bs-collection')
 
-            while True:
-                if page > 10:
-                    raise Exception('Page overflow: ' + category_path)
+            if not product_container:
+                if page == 1:
+                    logging.warning('Empty category: ' + url_extension)
+                break
 
-                page_url = 'https://www.lifemaxstore.cl/collection/{}/' \
-                           '?page={}'.format(category_path, page)
-                print(page_url)
-                response = session.get(page_url)
-                data = response.text
-                soup = BeautifulSoup(data, 'html5lib')
-                product_container = soup.find('div', 'bs-collection')
+            product_containers = product_container.findAll(
+                'div', 'bs-collection__product')
+            if not product_containers:
+                if page == 1:
+                    logging.warning('Empty category: ' + url_extension)
+                break
 
-                if not product_container:
-                    if page == 1:
-                        logging.warning('Empty category: ' + category_path)
-                    break
+            for container in product_containers:
+                product_url = container.find('a')['href']
+                product_urls.append(
+                    'https://www.lifemaxstore.cl' + product_url)
 
-                product_containers = product_container.findAll(
-                    'div', 'bs-collection__product')
-                if not product_containers:
-                    if page == 1:
-                        logging.warning('Empty category: ' + category_path)
-                    break
-
-                for container in product_containers:
-                    product_url = container.find('a')['href']
-                    product_urls.append(
-                        'https://www.lifemaxstore.cl' + product_url)
-
-                page += 1
+            page += 1
         return product_urls
 
     @classmethod
