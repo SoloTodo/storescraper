924da683860453fda83df2a4fd5f8d44df508998
Vijay Khemlani <vj@solotodo.com>
2023-08-01 12:24:14 -0400

    * Updated Eyc

--- storescraper/stores/eyc.py
+++ storescraper/stores/eyc.py
@@ -5,84 +5,67 @@ from bs4 import BeautifulSoup
 
 from storescraper.categories import KEYBOARD, NOTEBOOK, TABLET, \
     STORAGE_DRIVE, RAM, PROCESSOR, PRINTER, UPS, ALL_IN_ONE, MONITOR, \
-    MOTHERBOARD, POWER_SUPPLY, MEMORY_CARD, USB_FLASH_DRIVE, HEADPHONES
+    MOTHERBOARD, POWER_SUPPLY, MEMORY_CARD, USB_FLASH_DRIVE, HEADPHONES, \
+    VIDEO_CARD, COMPUTER_CASE, SOLID_STATE_DRIVE
 from storescraper.product import Product
-from storescraper.store import Store
+from storescraper.store_with_url_extensions import StoreWithUrlExtensions
 from storescraper.utils import session_with_proxy
 
 
-class Eyc(Store):
-    @classmethod
-    def categories(cls):
-        return [
-            NOTEBOOK,
-            TABLET,
-            STORAGE_DRIVE,
-            RAM,
-            PROCESSOR,
-            PRINTER,
-            UPS,
-            ALL_IN_ONE,
-            MONITOR,
-            MOTHERBOARD,
-            POWER_SUPPLY,
-            MEMORY_CARD,
-            USB_FLASH_DRIVE,
-            HEADPHONES,
-            KEYBOARD
-        ]
+class Eyc(StoreWithUrlExtensions):
+    url_extensions = [
+        ['294-notebooks', NOTEBOOK],
+        ['295-workstation', ALL_IN_ONE],
+        ['313-tablet', TABLET],
+        ['330-discos', STORAGE_DRIVE],
+        ['306-discos', STORAGE_DRIVE],
+        ['304-memorias', RAM],
+        ['305-procesadores', PROCESSOR],
+        ['331-impresoras', PRINTER],
+        ['300-ups', UPS],
+        ['339-memorias-ram', RAM],
+        ['342-monitores', MONITOR],
+        ['344-placas-madre', MOTHERBOARD],
+        ['345-procesadores', PROCESSOR],
+        ['346-fuentes-de-poder', POWER_SUPPLY],
+        ['351-microsd', MEMORY_CARD],
+        ['350-pendrives', USB_FLASH_DRIVE],
+        ['353-audifonos', HEADPHONES],
+        ['392-teclados', KEYBOARD],
+        ['348-tarjetas-de-video', VIDEO_CARD],
+        ['378-gabinetes', COMPUTER_CASE],
+        ['395-discos-ssd', SOLID_STATE_DRIVE],
+        ['361-all-in-one', ALL_IN_ONE],
+    ]
 
     @classmethod
-    def discover_urls_for_category(cls, category, extra_args=None):
-        url_extensions = [
-            ['294-notebooks', NOTEBOOK],
-            ['295-workstation', ALL_IN_ONE],
-            ['313-tablets', TABLET],
-            ['330-discos', STORAGE_DRIVE],
-            ['306-discos', STORAGE_DRIVE],
-            ['304-memorias', RAM],
-            ['305-procesadores', PROCESSOR],
-            ['331-impresoras', PRINTER],
-            ['300-ups', UPS],
-            ['339-memorias-ram', RAM],
-            ['342-monitores', MONITOR],
-            ['344-placas-madre', MOTHERBOARD],
-            ['345-procesadores', PROCESSOR],
-            ['346-fuentes-de-poder', POWER_SUPPLY],
-            ['351-microsd', MEMORY_CARD],
-            ['350-pendrives', USB_FLASH_DRIVE],
-            ['353-audifonos', HEADPHONES],
-            ['392-teclados', KEYBOARD],
-        ]
+    def discover_urls_for_url_extension(cls, url_extension, extra_args):
         session = session_with_proxy(extra_args)
         product_urls = []
-        for url_extension, local_category in url_extensions:
-            if local_category != category:
-                continue
-            page = 1
-            while True:
-                if page > 20:
-                    raise Exception('page overflow: ' + url_extension)
-                url_webpage = 'https://tienda.eyc.cl/es/{}?page={}'. \
-                    format(url_extension, page)
-                data = session.get(url_webpage, verify=False).text
-                soup = BeautifulSoup(data, 'html.parser')
-                product_container = soup.findAll('article',
-                                                 'product-miniature')
-                if not product_container:
-                    if page == 1:
-                        logging.warning('Empty category: ' + url_extension)
-                    break
-                for container in product_container:
-                    product_url = container.find('a')['href']
-                    product_urls.append(product_url)
-                page += 1
+        page = 1
+        while True:
+            if page > 20:
+                raise Exception('page overflow: ' + url_extension)
+            url_webpage = 'https://tienda.eyc.cl/es/{}?page={}'. \
+                format(url_extension, page)
+            data = session.get(url_webpage).text
+            soup = BeautifulSoup(data, 'html.parser')
+            product_container = soup.findAll('article',
+                                             'product-miniature')
+            if not product_container:
+                if page == 1:
+                    logging.warning('Empty category: ' + url_extension)
+                break
+            for container in product_container:
+                product_url = container.find('a')['href']
+                product_urls.append(product_url)
+            page += 1
         return product_urls
 
     @classmethod
     def products_for_url(cls, url, category=None, extra_args=None):
         session = session_with_proxy(extra_args)
-        response = session.get(url, verify=False)
+        response = session.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
         name = soup.find('div', 'product-reference_top').find(
             'span').text + ' - ' + soup.find('h1', 'product_name').text
