2abd9160ddc025e5c05b1c459e973f619cfe77a2
Vijay Khemlani <vj@solotodo.com>
2023-08-03 18:02:25 -0400

    * Updated Dreamtec

--- storescraper/stores/dreamtec.py
+++ storescraper/stores/dreamtec.py
@@ -1,77 +1,49 @@
 from decimal import Decimal
-import base64
 import logging
 from bs4 import BeautifulSoup
 from storescraper.categories import ALL_IN_ONE, GAMING_CHAIR, HEADPHONES, \
     KEYBOARD, MONITOR, NOTEBOOK, PRINTER, STEREO_SYSTEM, TABLET, TELEVISION, \
     VIDEO_GAME_CONSOLE, WEARABLE, RAM, MOUSE
 from storescraper.product import Product
-from storescraper.store import Store
+from storescraper.store_with_url_extensions import StoreWithUrlExtensions
 from storescraper.utils import remove_words, session_with_proxy
 
 
-class Dreamtec(Store):
-    @classmethod
-    def categories(cls):
-        return [
-            MONITOR,
-            NOTEBOOK,
-            VIDEO_GAME_CONSOLE,
-            HEADPHONES,
-            GAMING_CHAIR,
-            KEYBOARD,
-            ALL_IN_ONE,
-            PRINTER,
-            TELEVISION,
-            STEREO_SYSTEM,
-            TABLET,
-            WEARABLE,
-            HEADPHONES,
-            RAM,
-            MOUSE
-        ]
+class Dreamtec(StoreWithUrlExtensions):
+    url_extensions = [
+        ['gamer-zone/accesorios-gamer-zone', MOUSE],
+        ['gamer-zone/audifonos-gamer-zone', HEADPHONES],
+        ['gamer-zone/audio', STEREO_SYSTEM],
+        ['gamer-zone/consolas', VIDEO_GAME_CONSOLE],
+        ['gamer-zone/monitores-gamer-zone', MONITOR],
+        ['gamer-zone/notebooks-gamer-zone', NOTEBOOK],
+        ['gamer-zone/sillas-gamer', GAMING_CHAIR],
+        ['home-office/accesorios-home-office', KEYBOARD],
+        ['home-office/all-in-one', ALL_IN_ONE],
+        ['home-office/impresoras', PRINTER],
+        ['home-office/monitores', MONITOR],
+        ['home-office/notebooks', NOTEBOOK],
+        ['smart-home/parlantes', STEREO_SYSTEM],
+        ['smart-home/smart-tv', TELEVISION],
+        ['smart-home/smart-watch', WEARABLE],
+        ['smart-home/tablets', TABLET],
+    ]
 
     @classmethod
-    def discover_urls_for_category(cls, category, extra_args=None):
-        url_extensions = [
-            ['gamer-zone/audifonos-gamer-zone/', HEADPHONES],
-            ['gamer-zone/consolas', VIDEO_GAME_CONSOLE],
-            ['gamer-zone/monitores-gamer-zone', MONITOR],
-            ['gamer-zone/notebooks-gamer-zone', NOTEBOOK],
-            ['gamer-zone/sillas-gamer', GAMING_CHAIR],
-            ['gamer-zone/accesorios-gamer-zone', MOUSE],
-            ['home-office/all-in-one', ALL_IN_ONE],
-            ['home-office/escaner', PRINTER],
-            ['home-office/impresoras', PRINTER],
-            ['home-office/macbook', NOTEBOOK],
-            ['home-office/monitores', MONITOR],
-            ['home-office/notebooks', NOTEBOOK],
-            ['home-office/accesorios-home-office', RAM],
-            ['smart-home/audifonos', HEADPHONES],
-            ['smart-home/ipad', TABLET],
-            ['smart-home/smart-tv', TELEVISION],
-            ['smart-home/smart-watch', WEARABLE],
-            ['smart-home/soundbar', STEREO_SYSTEM],
-            ['smart-home/tablets', TABLET],
-        ]
-
+    def discover_urls_for_url_extension(cls, url_extension, extra_args):
         session = session_with_proxy(extra_args)
         product_urls = []
-        for url_extension, local_category in url_extensions:
-            if local_category != category:
-                continue
+        url_webpage = 'http://www.dreamtec.cl/{}/' \
+            ''.format(url_extension)
+        data = session.get(url_webpage).text
+        soup = BeautifulSoup(data, 'html.parser')
+        product_containers = soup.findAll('div', 'foto-prod')
+        if not product_containers:
+            logging.warning('Empty category: ' + url_extension)
 
-            url_webpage = 'http://www.dreamtec.cl/{}/' \
-                ''.format(url_extension)
-            data = session.get(url_webpage).text
-            soup = BeautifulSoup(data, 'html.parser')
-            product_containers = soup.findAll('div', 'foto-prod')
-            if not product_containers:
-                logging.warning('Empty category: ' + url_extension)
-
-            for container in product_containers:
-                product_url = container.find('a')['href']
-                product_urls.append(product_url)
+        for container in product_containers:
+            product_url = container.find('a')['href']
+            product_urls.append(product_url)
         return product_urls
 
     @classmethod
