2f48136de6626127fac74eb0e6a996bae4d2bff1
Vijay Khemlani <vj@solotodo.com>
2023-08-14 10:46:43 -0400

    * Updated Mutant

--- storescraper/stores/mutant.py
+++ storescraper/stores/mutant.py
@@ -7,62 +7,43 @@ from storescraper.categories import COMPUTER_CASE, CPU_COOLER, KEYBOARD, \
     PROCESSOR, RAM, HEADPHONES
 from storescraper.product import Product
 from storescraper.store import Store
+from storescraper.store_with_url_extensions import StoreWithUrlExtensions
 from storescraper.utils import html_to_markdown, remove_words, \
     session_with_proxy
 
 
-class Mutant(Store):
-    @classmethod
-    def categories(cls):
-        return [
-            CPU_COOLER,
-            MOUSE,
-            VIDEO_CARD,
-            POWER_SUPPLY,
-            MOTHERBOARD,
-            SOLID_STATE_DRIVE,
-            COMPUTER_CASE,
-            MONITOR,
-            KEYBOARD,
-            PROCESSOR,
-            RAM,
-            HEADPHONES,
-        ]
+class Mutant(StoreWithUrlExtensions):
+    url_extensions = [
+        ['tarjeta-video', VIDEO_CARD],
+        ['fuente-de-poder', POWER_SUPPLY],
+        ['placa-madre', MOTHERBOARD],
+        ['discos-ssd', SOLID_STATE_DRIVE],
+        ['sistema-refrigeracion', CPU_COOLER],
+        ['gabinetes', COMPUTER_CASE],
+        ['monitor', MONITOR],
+        ['mouse', MOUSE],
+        ['cpu', PROCESSOR],
+        ['memoria-ram', RAM],
+        ['teclados-armados', KEYBOARD],
+        ['audifonos', HEADPHONES],
+    ]
 
     @classmethod
-    def discover_urls_for_category(cls, category, extra_args=None):
-        url_extensions = [
-            ['tarjeta-video', VIDEO_CARD],
-            ['fuente-de-poder', POWER_SUPPLY],
-            ['placa-madre', MOTHERBOARD],
-            ['discos-ssd', SOLID_STATE_DRIVE],
-            ['sistema-refrigeracion', CPU_COOLER],
-            ['gabinetes', COMPUTER_CASE],
-            ['monitor', MONITOR],
-            ['mouse', MOUSE],
-            ['cpu', PROCESSOR],
-            ['memoria-ram', RAM],
-            ['teclados-armados', KEYBOARD],
-            ['audifonos', HEADPHONES],
-        ]
+    def discover_urls_for_url_extension(cls, url_extension, extra_args):
         session = session_with_proxy(extra_args)
         product_urls = []
-        for url_extension, local_category in url_extensions:
-            if local_category != category:
-                continue
+        url_webpage = 'https://www.mutant.cl/{}/'.format(url_extension)
+        print(url_webpage)
+        data = session.get(url_webpage).text
+        soup = BeautifulSoup(data, 'html.parser')
+        product_containers = soup.findAll('div', 'jet-woo-products__item')
 
-            url_webpage = 'https://www.mutant.cl/{}/'.format(url_extension)
-            print(url_webpage)
-            data = session.get(url_webpage).text
-            soup = BeautifulSoup(data, 'html.parser')
-            product_containers = soup.findAll('div', 'jet-woo-products__item')
+        if not product_containers:
+            logging.warning('Empty category: ' + url_extension)
 
-            if not product_containers:
-                logging.warning('Empty category: ' + url_extension)
-
-            for container in product_containers:
-                product_url = container.find('a')['href']
-                product_urls.append(product_url)
+        for container in product_containers:
+            product_url = container.find('a')['href']
+            product_urls.append(product_url)
 
         return product_urls
 
@@ -77,6 +58,10 @@ class Mutant(Store):
 
         description = html_to_markdown(
             str(soup.find('div', {'id': 'tab-description'})))
+
+        short_description_tag = soup.find(
+            'div', 'woocommerce-product-details__short-description')
+        is_reserva = 'RESERVA' in str(short_description_tag).upper()
 
         picture_container = soup.find(
             'div', 'woocommerce-product-gallery__wrapper')
@@ -95,7 +80,10 @@ class Mutant(Store):
                 variant_name = '{} - {}'.format(name, attr)
                 sku = v['sku']
                 price = Decimal(v['display_price'])
-                if v['max_qty'] != "":
+
+                if is_reserva:
+                    stock = 0
+                elif v['max_qty'] != "":
                     stock = v['max_qty']
                 else:
                     stock = 0
@@ -127,7 +115,9 @@ class Mutant(Store):
                 price_container.findAll('span', 'amount')[-1].text))
 
             stock_container = soup.find('p', 'stock in-stock')
-            if stock_container:
+            if is_reserva:
+                stock = 0
+            elif stock_container:
                 stock = int(stock_container.text.split(' ')[0])
             elif soup.find('button', {'name': 'add-to-cart'}):
                 stock = -1
