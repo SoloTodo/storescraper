813b7044a9467d82fc71baefbb4b4d8942bec8bf
Vijay Khemlani <vj@solotodo.com>
2023-08-31 11:38:07 -0400

    * Updated Geant

--- storescraper/stores/geant.py
+++ storescraper/stores/geant.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 from storescraper.categories import TELEVISION
 from storescraper.product import Product
 from storescraper.store import Store
-from storescraper.utils import session_with_proxy
+from storescraper.utils import session_with_proxy, check_ean13
 
 
 class Geant(Store):
@@ -51,33 +51,82 @@ class Geant(Store):
 
     @classmethod
     def products_for_url(cls, url, category=None, extra_args=None):
+        print(url)
         session = session_with_proxy(extra_args)
         response = session.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
-        sku = soup.find('input', {'id': '___rc-p-id'})['value']
-        product_info = session.get('https://www.geant.com.uy/api/catalog_'
-                                   'system/pub/products/search/'
-                                   '?fq=productId:' + sku).json()[0]
-        name = product_info['productName']
-        stock = product_info['items'][0]['sellers'][0]['commertialOffer'][
-            'AvailableQuantity']
-        price = Decimal(str(
-            product_info['items'][0]['sellers'][0]['commertialOffer'][
-                'Price']))
-        picture_urls = [
-            product_info['items'][0]['images'][0]['imageUrl'].split('?')[0]]
+
+        product_data_tag = soup.find('template', {'data-varname': '__STATE__'})
+        product_data = json.loads(str(
+            product_data_tag.find('script').contents[0]))
+
+        if not product_data:
+            return []
+
+        base_json_key = list(product_data.keys())[0]
+        product_specs = product_data[base_json_key]
+
+        key = product_specs['productId']
+        name = product_specs['productName']
+        sku = product_specs['productReference']
+        description = product_specs.get('description', None)
+
+        pricing_key = '${}.items.0.sellers.0.commertialOffer'.format(
+            base_json_key)
+        pricing_data = product_data[pricing_key]
+
+        exchange_rate = extra_args['exchange_rate']
+        price = Decimal(str(pricing_data['Price'] / exchange_rate))
+
+        # if not price:
+        #     pricing_key = '{}.specificationGroups.2.specifications.0'.format(
+        #         base_json_key)
+        #     price = Decimal(remove_words(product_data[pricing_key]['name']))
+
+        stock = pricing_data['AvailableQuantity']
+        picture_list_key = '{}.items.0'.format(base_json_key)
+        picture_list_node = product_data[picture_list_key]
+        picture_ids = [x['id'] for x in picture_list_node['images']]
+
+        picture_urls = []
+        for picture_id in picture_ids:
+            picture_node = product_data[picture_id]
+            picture_urls.append(picture_node['imageUrl'].split('?')[0])
+
+        ean = None
+        for prop in product_specs['properties']:
+            if product_data[prop['id']]['name'] == 'EAN':
+                ean = product_data[prop['id']]['values']['json'][0]
+                if check_ean13(ean):
+                    break
+                else:
+                    ean = None
+
         p = Product(
             name,
             cls.__name__,
             category,
             url,
             url,
-            sku,
+            key,
             stock,
             price,
             price,
-            'UYU',
+            'USD',
             sku=sku,
-            picture_urls=picture_urls
+            picture_urls=picture_urls,
+            description=description,
+            ean=ean
         )
         return [p]
+
+    @classmethod
+    def preflight(cls, extra_args=None):
+        session = session_with_proxy(extra_args)
+        res = session.get('https://www.geant.com.uy/api/dataentities/DO/'
+                          'search?_sort=date%20DESC&_fields=exchange')
+        json_data = res.json()
+        exchange_rate = json_data[0]['exchange']
+        return {
+            'exchange_rate': exchange_rate
+        }
