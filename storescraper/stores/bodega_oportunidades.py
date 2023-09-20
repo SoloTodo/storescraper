662798c451346f867ae49c0b94708e8d982f86f3
Vijay Khemlani <vj@solotodo.com>
2023-09-20 15:13:57 -0300

    * Updated BodegaOportunidades

--- storescraper/stores/bodega_oportunidades.py
+++ storescraper/stores/bodega_oportunidades.py
@@ -1,86 +1,63 @@
+import urllib
 from decimal import Decimal
 import json
 import logging
 from bs4 import BeautifulSoup
-from storescraper.categories import GAMING_CHAIR, MICROPHONE, NOTEBOOK, OVEN, \
-    PRINTER, RAM, REFRIGERATOR, STEREO_SYSTEM, STORAGE_DRIVE, TELEVISION, \
-    WEARABLE, CELL, VIDEO_GAME_CONSOLE, SPACE_HEATER, AIR_CONDITIONER
+from storescraper.categories import (
+    GAMING_CHAIR, NOTEBOOK, OVEN, PRINTER, RAM, REFRIGERATOR, STEREO_SYSTEM,
+    TELEVISION, WEARABLE, CELL, VIDEO_GAME_CONSOLE, SPACE_HEATER, HEADPHONES,
+    WASHING_MACHINE)
 from storescraper.product import Product
-from storescraper.store import Store
+from storescraper.store_with_url_extensions import StoreWithUrlExtensions
 from storescraper.utils import html_to_markdown, session_with_proxy
 
 
-class BodegaOportunidades(Store):
-    @classmethod
-    def categories(cls):
-        return [
-            CELL,
-            NOTEBOOK,
-            RAM,
-            STEREO_SYSTEM,
-            PRINTER,
-            STORAGE_DRIVE,
-            WEARABLE,
-            TELEVISION,
-            GAMING_CHAIR,
-            OVEN,
-            REFRIGERATOR,
-            MICROPHONE,
-            VIDEO_GAME_CONSOLE,
-            SPACE_HEATER,
-            AIR_CONDITIONER
-        ]
+class BodegaOportunidades(StoreWithUrlExtensions):
+    url_extensions = [
+        ['Calefacción', SPACE_HEATER],
+        ['Celulares y telefonía', CELL],
+        ['Computación', NOTEBOOK],
+        ['Consolas y videojuegos', VIDEO_GAME_CONSOLE],
+        ['Discos y accesorios', RAM],
+        ['Electrónica, audio y video', STEREO_SYSTEM],
+        ['Hornos y microondas', OVEN],
+        ['Hornos, cocinas y encimeras', OVEN],
+        ['Impresoras', PRINTER],
+        ['Lavado', WASHING_MACHINE],
+        ['Muebles', GAMING_CHAIR],
+        ['Refrigeradores y frigobar', REFRIGERATOR],
+        ['Smarwatch y Smartband', WEARABLE],
+        ['Tecnología gamer', HEADPHONES],
+        ['Televisores', TELEVISION],
+    ]
 
     @classmethod
-    def discover_urls_for_category(cls, category, extra_args=None):
-        url_extensions = [
-            ['Celulares y telefonía', CELL],
-            ['Computación', NOTEBOOK],
-            ['Consolas y videojuegos', VIDEO_GAME_CONSOLE],
-            ['Discos y accesorios', RAM],
-            ['Electrónica, audio y video', STEREO_SYSTEM],
-            ['Hornos y microondas', OVEN],
-            ['Hornos, cocinas y encimeras', OVEN],
-            ['Impresoras', PRINTER],
-            ['Micrófonos y Amplificadores', MICROPHONE],
-            ['Muebles', GAMING_CHAIR],
-            ['Refrigeradores y frigobar', REFRIGERATOR],
-            ['Smarwatch y Smartband', WEARABLE],
-            ['Televisores', TELEVISION],
-            ['Calefacción', SPACE_HEATER],
-            ['Climatización y energía', SPACE_HEATER],
-            ['Humidificación', AIR_CONDITIONER],
-            ['Ventilación', AIR_CONDITIONER],
-        ]
-
+    def discover_urls_for_url_extension(cls, url_extension, extra_args=None):
         session = session_with_proxy(extra_args)
         session.headers['user-agent'] = \
             'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 ' \
             '(KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
         product_urls = []
-        for product_type, local_category in url_extensions:
-            if local_category != category:
-                continue
-            page = 1
-            while True:
-                if page > 10:
-                    raise Exception('Page overflow: ' + product_type)
-                url_webpage = 'https://bodegaoportunidades.cl/collections/' \
-                              'todos-los-productos?filter.p.product_type=' \
-                              '{}&page={}'.format(product_type, page)
-                print(url_webpage)
-                data = session.get(url_webpage).text
-                soup = BeautifulSoup(data, 'html.parser')
-                product_containers = soup.findAll('div', 'product-item')
-                if not product_containers:
-                    if page == 1:
-                        logging.warning('Empty category: ' + product_type)
-                    break
-                for container in product_containers:
-                    product_url = container.find('a')['href'].split('?')[0]
-                    product_urls.append(
-                        'https://bodegaoportunidades.cl' + product_url)
-                page += 1
+        page = 1
+        while True:
+            if page > 10:
+                raise Exception('Page overflow: ' + url_extension)
+            url_webpage = 'https://bodegaoportunidades.cl/collections/' \
+                          'todos-los-productos?filter.p.product_type=' \
+                          '{}&page={}'.format(urllib.parse.quote(url_extension), page)
+            print(url_webpage)
+            data = session.get(url_webpage).text
+            soup = BeautifulSoup(data, 'html.parser')
+            product_containers = soup.findAll('div', 'product-item')
+            if not product_containers:
+                if page == 1:
+                    logging.warning('Empty category: ' + url_extension)
+                break
+            for container in product_containers:
+                product_url = container.find('a')['href'].split('?')[0]
+                product_urls.append(
+                    'https://bodegaoportunidades.cl' + product_url)
+            page += 1
         return product_urls
 
     @classmethod
