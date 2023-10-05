bcf0c2501cd0f57ac7c630cad122a53cc7c24027
Vijay Khemlani <vj@solotodo.com>
2023-10-05 11:37:42 -0300

    * Updated Centrale

--- storescraper/stores/centrale.py
+++ storescraper/stores/centrale.py
@@ -1,110 +1,74 @@
+import json
 import logging
 from decimal import Decimal
 
 from bs4 import BeautifulSoup
 
 from storescraper.categories import SOLID_STATE_DRIVE, \
-    EXTERNAL_STORAGE_DRIVE, USB_FLASH_DRIVE, POWER_SUPPLY, RAM, \
+    EXTERNAL_STORAGE_DRIVE, POWER_SUPPLY, RAM, \
     MOTHERBOARD, PROCESSOR, VIDEO_CARD, NOTEBOOK, TABLET, \
     MONITOR, PRINTER, UPS, MOUSE, COMPUTER_CASE, HEADPHONES, STEREO_SYSTEM, \
     ALL_IN_ONE, VIDEO_GAME_CONSOLE, CELL, WEARABLE, TELEVISION, GAMING_CHAIR, \
     KEYBOARD, CPU_COOLER, MICROPHONE
 from storescraper.product import Product
-from storescraper.store import Store
+from storescraper.store_with_url_extensions import StoreWithUrlExtensions
 from storescraper.utils import session_with_proxy, remove_words
 
 
-class Centrale(Store):
-    @classmethod
-    def categories(cls):
-        return [
-            EXTERNAL_STORAGE_DRIVE,
-            SOLID_STATE_DRIVE,
-            USB_FLASH_DRIVE,
-            POWER_SUPPLY,
-            RAM,
-            MOTHERBOARD,
-            PROCESSOR,
-            VIDEO_CARD,
-            NOTEBOOK,
-            TABLET,
-            MONITOR,
-            PRINTER,
-            UPS,
-            MOUSE,
-            COMPUTER_CASE,
-            HEADPHONES,
-            STEREO_SYSTEM,
-            ALL_IN_ONE,
-            VIDEO_GAME_CONSOLE,
-            CELL,
-            WEARABLE,
-            TELEVISION,
-            GAMING_CHAIR,
-            CPU_COOLER,
-            KEYBOARD,
-            MICROPHONE
+class Centrale(StoreWithUrlExtensions):
+    url_extensions = [
+        ['audifonos', HEADPHONES],
+        ['sistemas-de-audio', STEREO_SYSTEM],
+        ['all-in-one', ALL_IN_ONE],
+        ['almacenamiento-externo', EXTERNAL_STORAGE_DRIVE],
+        ['kits-teclados-y-mouses', MOUSE],
+        ['mouses', MOUSE],
+        ['notebooks', NOTEBOOK],
+        ['tablets', TABLET],
+        ['teclados', KEYBOARD],
+        ['ups-y-reguladores', UPS],
+        ['smartwatches-electro', WEARABLE],
+        ['televisores', TELEVISION],
+        ['consolas', VIDEO_GAME_CONSOLE],
+        ['impresoras', PRINTER],
+        ['plotters', PRINTER],
+        ['monitores', MONITOR],
+        ['sillas-muebles-y-sillas', GAMING_CHAIR],
+        ['plotters', PRINTER],
+        ['almacenamiento', SOLID_STATE_DRIVE],
+        ['fuentes-de-poder', POWER_SUPPLY],
+        ['gabinetes-desktop', COMPUTER_CASE],
+        ['memorias-ram', RAM],
+        ['placas-madres', MOTHERBOARD],
+        ['procesadores-pc', PROCESSOR],
+        ['refrigeración', CPU_COOLER],
+        ['tarjetas-de-video', VIDEO_CARD],
+        ['smartphones', CELL],
+        ['micrófono', MICROPHONE]
+    ]
 
-        ]
-
     @classmethod
-    def discover_urls_for_category(cls, category, extra_args=None):
-        url_extensions = [
-            ['tecnología/audio/audifonos', HEADPHONES],
-            ['tecnología/audio/sistemas-de-audio', STEREO_SYSTEM],
-            ['tecnología/computación/all-in-one/', ALL_IN_ONE],
-            ['tecnología/computación/almacenamiento-externo/',
-             EXTERNAL_STORAGE_DRIVE],
-            ['tecnología/computación/kits-teclados-y-mouses', MOUSE],
-            ['tecnología/computación/mouses', MOUSE],
-            ['tecnología/computación/notebooks', NOTEBOOK],
-            ['tecnología/computación/tablets/', TABLET],
-            ['tecnología/computación/teclados/', KEYBOARD],
-            ['tecnología/computación/ups-y-reguladores', UPS],
-            ['tecnología/electro/smartwatches-electro', WEARABLE],
-            ['tecnología/electro/televisores', TELEVISION],
-            ['tecnologia/entretención/consolas', VIDEO_GAME_CONSOLE],
-            ['tecnologia/impresión-y-oficina/impresoras', PRINTER],
-            ['tecnologia/impresión-y-oficina/plotters', PRINTER],
-            ['tecnologia/monitores-y-proyectores/monitores', MONITOR],
-            ['tecnologia/muebles-y-sillas/sillas-muebles-y-sillas',
-             GAMING_CHAIR],
-            ['tecnología/impresión-y-oficina/plotters', PRINTER],
-            ['tecnología/partes-y-piezas/almacenamiento',
-             SOLID_STATE_DRIVE],
-            ['tecnología/partes-y-piezas/fuentes-de-poder', POWER_SUPPLY],
-            ['tecnología/partes-y-piezas/gabinetes-desktop', COMPUTER_CASE],
-            ['tecnología/partes-y-piezas/memorias-ram', RAM],
-            ['tecnología/partes-y-piezas/placas-madres', MOTHERBOARD],
-            ['tecnología/partes-y-piezas/procesadores-pc', PROCESSOR],
-            ['tecnología/partes-y-piezas/refrigeración', CPU_COOLER],
-            ['tecnología/partes-y-piezas/tarjetas-de-video', VIDEO_CARD],
-            ['tecnología/telefonía/smartphones', CELL],
-            ['tecnologia/audio/micrófono', MICROPHONE]
-        ]
+    def discover_urls_for_url_extension(cls, url_extension, extra_args=None):
         session = session_with_proxy(extra_args)
         product_urls = []
-        for url_extension, local_category in url_extensions:
-            if local_category != category:
-                continue
-            page = 1
-            while True:
-                if page > 20:
-                    raise Exception('page overflow: ' + url_extension)
-                url_webpage = 'https://centrale.cl/categoria-producto' \
-                              '/{}/page/{}'.format(url_extension,
-                                                   page)
-                data = session.get(url_webpage).text
-                soup = BeautifulSoup(data, 'html.parser')
-                product_containers = soup.findAll('div', 'product-small box')
-                if not product_containers:
-                    if page == 1:
-                        logging.warning('Empty category; ' + url_webpage)
-                    break
-                for container in product_containers:
-                    product_url = container.find('a')['href']
-                    product_urls.append(product_url)
-                page += 1
+        page = 1
+        while True:
+            if page > 20:
+                raise Exception('page overflow: ' + url_extension)
+            url_webpage = 'https://centrale.cl/categoria-producto' \
+                          '/{}/page/{}'.format(url_extension, page)
+            print(url_webpage)
+            data = session.get(url_webpage).text
+            soup = BeautifulSoup(data, 'html.parser')
+            product_containers = soup.findAll('div', 'product-small box')
+            if not product_containers:
+                if page == 1:
+                    logging.warning('Empty category; ' + url_webpage)
+                break
+            for container in product_containers:
+                product_url = container.find('a')['href']
+                product_urls.append(product_url)
+            page += 1
         return product_urls
 
     @classmethod
@@ -113,12 +77,20 @@ class Centrale(Store):
         session = session_with_proxy(extra_args)
         response = session.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
-        part_number = soup.find('span', {'id': 'solotodo'}).contents[1].strip()
 
-        if not part_number:
-            part_number = None
+        json_data = json.loads(soup.find(
+            'script', {'type': 'application/ld+json'}).text)
 
-        name = soup.find('h1', 'product-title').text.strip()
+        for entry in json_data['@graph']:
+            if entry['@type'] == 'Product':
+                product_data = entry
+                break
+        else:
+            raise Exception('No JSON product data found')
+
+        part_number = product_data['mpn'] or None
+        name = product_data['name'].strip()
+        sku = product_data['sku'].strip()
         key = soup.find('link', {'rel': 'shortlink'})['href'].split('p=')[-1]
         if soup.find('p', 'stock in-stock'):
             stock = int(soup.find('p', 'stock in-stock').text.split()[0])
@@ -133,13 +105,10 @@ class Centrale(Store):
         picture_urls = []
         picture_container = soup.find('div', 'product-thumbnails')
         if picture_container:
-            for tag in picture_container.findAll('img',
-                                                 'attachment'
-                                                 '-woocommerce_thumbnail '
-                                                 'lazyload'):
-                if tag['data-src'].replace('-300x300', '') not in picture_urls:
-                    picture_urls.append(
-                        tag['data-src'].replace('-300x300', ''))
+            for tag in picture_container.findAll(
+                    'img', 'attachment-woocommerce_thumbnail'):
+                picture_urls.append(
+                    tag['src'].replace('-300x300', ''))
         elif soup.find('div', 'woocommerce-product-gallery__image'):
             picture_urls.append(
                 soup.find('div', 'woocommerce-product-gallery__image').find(
@@ -155,7 +124,7 @@ class Centrale(Store):
             normal_price,
             offer_price,
             'CLP',
-            sku=part_number,
+            sku=sku,
             picture_urls=picture_urls,
             part_number=part_number
         )
