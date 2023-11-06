c2bf2b52b8101e4037411c3dc259bd1894e53e45
Vijay Khemlani <vj@solotodo.com>
2023-11-06 10:49:49 -0300

    * Updated LGPe

--- storescraper/stores/lg_pe.py
+++ storescraper/stores/lg_pe.py
@@ -1,64 +1,59 @@
-from .lg_v5 import LgV5
-from storescraper.categories import TELEVISION, OPTICAL_DISK_PLAYER, \
-    STEREO_SYSTEM, HEADPHONES, REFRIGERATOR, WASHING_MACHINE, OVEN, \
-    MONITOR, STOVE, ALL_IN_ONE, \
-    NOTEBOOK, OPTICAL_DRIVE, AIR_CONDITIONER
+from .lg_v6 import LgV6
+from storescraper.categories import (TELEVISION, STEREO_SYSTEM, REFRIGERATOR,
+                                     WASHING_MACHINE, OVEN, MONITOR, STOVE,
+                                     ALL_IN_ONE, NOTEBOOK, AIR_CONDITIONER)
 
 
-class LgPe(LgV5):
-    region_code = 'pe'
+class LgPe(LgV6):
+    region_code = 'PE'
     currency = 'PEN'
     price_approximation = '0.01'
 
     @classmethod
     def _category_paths(cls):
         return [
-            # Televisores
-            ('CT20190005', TELEVISION, True),
-            ('CT20190005', TELEVISION, False),
-            # TVs LIFESTYLE
-            ('CT40016301', TELEVISION, True),
-            ('CT40016301', TELEVISION, False),
-            # Equipos de sonido
-            ('CT20190015', STEREO_SYSTEM, True),
-            ('CT20190015', STEREO_SYSTEM, False),
-            # AUDÍFONOS
-            ('CT40010303', HEADPHONES, True),
-            ('CT40010303', HEADPHONES, False),
-            # Video (DVD / BLU-RAY)
-            ('CT20190011', OPTICAL_DISK_PLAYER, True),
-            ('CT20190011', OPTICAL_DISK_PLAYER, False),
-            # Refrigeradoras
-            ('CT20190033', REFRIGERATOR, True),
-            ('CT20190033', REFRIGERATOR, False),
-            # Lavadoras
-            ('CT20190041', WASHING_MACHINE, True),
-            ('CT20190041', WASHING_MACHINE, False),
-            # Microondas
-            ('CT20190038', OVEN, True),
-            ('CT20190038', OVEN, False),
+            # To do TV y Soundbars
+            ('CT52006227', TELEVISION),
+            # Object collection Pose
+            ('CT52006483', TELEVISION),
+            # Flex
+            ('CT52006243', TELEVISION),
+            # StandbyMe Go
+            ('CT52006484', TELEVISION),
+            # To do equipos de sonido
+            ('CT52006251', STEREO_SYSTEM),
+            # Refrigeradoras French Door
+            ('CT52006489', REFRIGERATOR),
+            # Refrigeradoras Side by Side
+            ('CT52006491', REFRIGERATOR),
+            # Refrigeradoras Bottom Freezer
+            ('CT52006492', REFRIGERATOR),
+            # Refrigeradoras Top Freezer
+            ('CT52006493', REFRIGERATOR),
+            # Washtower
+            ('CT52006285', WASHING_MACHINE),
+            # Lavadoras Carga Frontal
+            ('CT52006494', WASHING_MACHINE),
+            # Lavadoras Carga Superior
+            ('CT52006495', WASHING_MACHINE),
+            # Secadoras de Ropa
+            ('CT52006279', WASHING_MACHINE),
+            # Microondas sin dorador
+            ('CT52006496', OVEN),
+            # Microondas con dorador
+            ('CT52006342', OVEN),
             # Cocinas
-            ('CT32002801', STOVE, True),
-            ('CT32002801', STOVE, False),
-            # All in one
-            ('CT30018781', ALL_IN_ONE, True),
-            ('CT30018781', ALL_IN_ONE, False),
-            # LG gram
-            ('CT32001321', NOTEBOOK, True),
-            ('CT32001321', NOTEBOOK, False),
+            ('CT52006346', STOVE),
             # Monitores
-            ('CT20190057', MONITOR, True),
-            ('CT20190057', MONITOR, False),
-            # Almacenamiento Óptico
-            ('CT20190064', OPTICAL_DRIVE, True),
-            ('CT20190064', OPTICAL_DRIVE, False),
-            # PANTALLAS COMERCIALES
-            ('CT40015395', MONITOR, True),
-            ('CT40015395', MONITOR, False),
-            # Aire Acondicionado Residencial
-            ('CT20190086', AIR_CONDITIONER, True),
-            ('CT20190086', AIR_CONDITIONER, False),
+            ('CT52006419', MONITOR),
+            # All in One
+            ('CT52006446', ALL_IN_ONE),
+            # Laptops
+            ('CT52006430', NOTEBOOK),
+            # Pantallas comerciales
+            ('CT52006441', TELEVISION),
+            # To do aire acondicionado residencial
+            ('CT52006359', AIR_CONDITIONER),
             # Aire acondicionado comercial
-            ('CT20190090', AIR_CONDITIONER, True),
-            ('CT20190090', AIR_CONDITIONER, False),
+            ('CT52006407', AIR_CONDITIONER),
         ]
