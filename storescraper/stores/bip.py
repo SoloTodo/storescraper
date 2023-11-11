bbf52107eb0474aaf4a788bef1f7e1002db2e5d4
Vijay Khemlani <vj@solotodo.com>
2023-11-11 15:22:28 -0300

    * Updated Bip

--- storescraper/stores/bip.py
+++ storescraper/stores/bip.py
@@ -8,140 +8,75 @@ from storescraper.categories import GAMING_CHAIR, CPU_COOLER, CELL, NOTEBOOK, \
     EXTERNAL_STORAGE_DRIVE, USB_FLASH_DRIVE, MEMORY_CARD, MOUSE, PRINTER, \
     KEYBOARD, HEADPHONES, STEREO_SYSTEM, UPS, TELEVISION
 from storescraper.product import Product
-from storescraper.store import Store
+from storescraper.store_with_url_extensions import StoreWithUrlExtensions
 from storescraper.utils import html_to_markdown, \
     session_with_proxy
 
 
-class Bip(Store):
-    @classmethod
-    def categories(cls):
-        return [
-            NOTEBOOK,
-            VIDEO_CARD,
-            PROCESSOR,
-            MONITOR,
-            MOTHERBOARD,
-            RAM,
-            STORAGE_DRIVE,
-            SOLID_STATE_DRIVE,
-            POWER_SUPPLY,
-            COMPUTER_CASE,
-            CPU_COOLER,
-            TABLET,
-            EXTERNAL_STORAGE_DRIVE,
-            USB_FLASH_DRIVE,
-            MEMORY_CARD,
-            TELEVISION,
-            MOUSE,
-            PRINTER,
-            ALL_IN_ONE,
-            KEYBOARD,
-            HEADPHONES,
-            STEREO_SYSTEM,
-            UPS,
-            GAMING_CHAIR,
-            CELL,
-        ]
+class Bip(StoreWithUrlExtensions):
+    url_extensions = [
+        ['166', NOTEBOOK],  # Notebooks
+        ['218', ALL_IN_ONE],  # All in One
+        ['792', VIDEO_CARD],  # Tarjetas de video
+        ['784', PROCESSOR],  # Procesadores
+        ['761', MONITOR],  # Monitores
+        ['785', MOTHERBOARD],  # Placas madre
+        ['132', RAM],  # RAM PC
+        ['178', RAM],  # RAM Notebook
+        ['125', STORAGE_DRIVE],  # Disco Duro 2,5'
+        ['124', STORAGE_DRIVE],  # Disco Duro 3,5'
+        ['413', SOLID_STATE_DRIVE],  # Disco Duro SSD
+        ['88', POWER_SUPPLY],  # Fuentes de poder
+        ['8', COMPUTER_CASE],  # Gabinetes
+        ['707', COMPUTER_CASE],  # Gabinetes gamer
+        ['5', CPU_COOLER],  # Coolers CPU
+        ['790', CPU_COOLER],  # Coolers CPU
+        ['286', TABLET],  # Tablets
+        ['128', EXTERNAL_STORAGE_DRIVE],  # Discos externos 2.5
+        ['528', USB_FLASH_DRIVE],  # USB Flash
+        ['82', MEMORY_CARD],  # Memory card
+        ['20', MOUSE],  # Mouse
+        ['703', MOUSE],  # Mouse Gamer
+        ['769', PRINTER],  # Impresoras
+        ['770', PRINTER],  # Plotter
+        ['12', KEYBOARD],  # Teclados
+        ['70', HEADPHONES],  # Audífono/Micrófono
+        ['13', STEREO_SYSTEM],  # Parlantes
+        ['31', UPS],  # UPS
+        ['591', GAMING_CHAIR],  # Sillas
+        ['864', CELL],
+        ['762', TELEVISION],
+    ]
 
     @classmethod
-    def discover_urls_for_category(cls, category, extra_args=None):
-        url_extensions = [
-            # Notebooks
-            ['166', NOTEBOOK],
-            # All in One
-            ['218', ALL_IN_ONE],
-            # Tarjetas de video
-            ['792', VIDEO_CARD],
-            # Proces
-            ['784', PROCESSOR],
-            # Monitores
-            ['761', MONITOR],
-            # Placas madre
-            ['785', MOTHERBOARD],
-            # RAM PC
-            ['132', RAM],
-            # RAM Notebook
-            ['178', RAM],
-            # Disco Duro 2,5'
-            ['125', STORAGE_DRIVE],
-            # Disco Duro 3,5'
-            ['124', STORAGE_DRIVE],
-            # Disco Duro SSD
-            ['413', SOLID_STATE_DRIVE],
-            # Fuentes de poder
-            ['88', POWER_SUPPLY],
-            # Gabinetes
-            ['8', COMPUTER_CASE],
-            # Gabinetes gamer
-            ['707', COMPUTER_CASE],
-            # Coolers CPU
-            ['5', CPU_COOLER],
-            ['790', CPU_COOLER],
-            # Tablets
-            ['286', TABLET],
-            # Discos externos 2.5
-            ['128', EXTERNAL_STORAGE_DRIVE],
-            # USB Flash
-            ['528', USB_FLASH_DRIVE],
-            # Memory card
-            ['82', MEMORY_CARD],
-            # Mouse
-            ['20', MOUSE],
-            # Mouse Gamer
-            ['703', MOUSE],
-            # Impresoras
-            ['769', PRINTER],
-            # Plotter
-            ['770', PRINTER],
-            # Teclados
-            ['12', KEYBOARD],
-            # Audífono/Micrófono
-            ['70', HEADPHONES],
-            # Parlantes
-            ['13', STEREO_SYSTEM],
-            # UPS
-            ['31', UPS],
-            # Sillas
-            ['591', GAMING_CHAIR],
-            ['864', CELL],
-            ['762', TELEVISION],
-        ]
-
+    def discover_urls_for_url_extension(cls, url_extension, extra_args=None):
         session = session_with_proxy(extra_args)
-
         product_urls = []
+        offset = 0
 
-        for url_extension, local_category in url_extensions:
-            if local_category != category:
-                continue
+        while True:
+            if offset >= 300:
+                raise Exception('Page overflow: ' + url_extension)
 
-            offset = 0
+            url_webpage = 'https://bip.cl/categoria/{}/{}'.format(
+                url_extension, offset
+            )
 
-            while True:
-                if offset >= 300:
-                    raise Exception('Page overflow: ' + url_extension)
+            data = session.get(url_webpage, verify=False).text
 
-                url_webpage = 'https://bip.cl/categoria/{}/{}'.format(
-                    url_extension, offset
-                )
+            soup = BeautifulSoup(data, 'html5lib')
+            product_containers = soup.findAll('div', 'producto')
 
-                data = session.get(url_webpage, verify=False).text
+            if not product_containers:
+                if offset == 0:
+                    logging.warning('Empty category: ' + url_webpage)
+                break
 
-                soup = BeautifulSoup(data, 'html5lib')
-                product_containers = soup.findAll('div', 'producto')
+            for container in product_containers:
+                product_url = container.find('a')['href']
+                product_urls.append(product_url)
 
-                if not product_containers:
-                    if offset == 0:
-                        logging.warning('Empty category: ' + url_webpage)
-                    break
-
-                for container in product_containers:
-                    product_url = container.find('a')['href']
-                    product_urls.append(product_url)
-
-                offset += 20
-
+            offset += 20
         return product_urls
 
     @classmethod
