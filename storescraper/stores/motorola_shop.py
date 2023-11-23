c2b1c2def7297024b72036dd164b972b432afc65
Vijay Khemlani <vj@solotodo.com>
2023-11-23 14:23:40 -0300

    * Updated MotorolaShop

--- storescraper/stores/motorola_shop.py
+++ storescraper/stores/motorola_shop.py
@@ -1,46 +1,61 @@
+import base64
 import json
-import re
 from decimal import Decimal
+
+from bs4 import BeautifulSoup
 
 from storescraper.categories import CELL
 from storescraper.product import Product
-from storescraper.store import Store
-from storescraper.utils import session_with_proxy
+from storescraper.store_with_url_extensions import StoreWithUrlExtensions
+from storescraper.utils import session_with_proxy, vtex_preflight
 
 
-class MotorolaShop(Store):
-    @classmethod
-    def categories(cls):
-        return [
-            CELL
-        ]
+class MotorolaShop(StoreWithUrlExtensions):
+    url_extensions = [
+        ('smartphones', CELL)
+    ]
 
     @classmethod
-    def discover_urls_for_category(cls, category, extra_args=None):
-        url_extensions = [
-            ['smartphones/d', CELL]
-        ]
-
-        session = session_with_proxy(extra_args)
+    def discover_urls_for_url_extension(cls, url_extension, extra_args=None):
         product_urls = []
-        for url_extension, local_category in url_extensions:
-            if local_category != category:
-                continue
+        session = session_with_proxy(extra_args)
 
-            url_webpage = 'https://www.motorola.cl/{}'.format(
-                url_extension)
-            print(url_webpage)
-            response = session.get(url_webpage)
-            products_state_text = '{' + re.search(
-                r'__STATE__ = \{(.+)}', response.text).groups()[0] + '}'
-            products_json = json.loads(products_state_text)
-            for key, product in products_json.items():
-                if 'productId' not in product:
-                    continue
+        offset = 0
+        while True:
+            if offset >= 600:
+                raise Exception('Page overflow')
 
+            variables = {
+                "from": offset,
+                "to": offset + 24,
+                "selectedFacets": [{"key": "c", "value": url_extension}]
+            }
+
+            payload = {
+                "persistedQuery": {
+                    "version": 1,
+                    "sha256Hash": extra_args['sha256Hash']
+                },
+                "variables": base64.b64encode(json.dumps(
+                    variables).encode('utf-8')).decode('utf-8')
+            }
+
+            endpoint = 'https://www.motorola.cl/_v/segment/graphql/v1' \
+                       '?extensions={}'.format(json.dumps(payload))
+            response = session.get(endpoint).json()
+
+            product_entries = response['data']['productSearch']['products']
+
+            if not product_entries:
+                break
+
+            for product_entry in product_entries:
                 product_url = 'https://www.motorola.cl/{}/p'.format(
-                    product['linkText'])
+                    product_entry['linkText'])
                 product_urls.append(product_url)
+
+            offset += 24
+
         return product_urls
 
     @classmethod
@@ -48,10 +63,11 @@ class MotorolaShop(Store):
         print(url)
         session = session_with_proxy(extra_args)
         response = session.get(url)
-        product_container = re.search(r'__STATE__ = {(.+)}',
-                                      response.text).groups()[0]
+        soup = BeautifulSoup(response.text, 'html5lib')
 
-        json_product = json.loads('{' + product_container + '}')
+        product_data_tag = soup.find('template', {'data-varname': '__STATE__'})
+        json_product = json.loads(str(
+            product_data_tag.find('script').contents[0]))
         item_key = list(json_product.keys())[0]
         products = []
         index = 0
@@ -96,3 +112,8 @@ class MotorolaShop(Store):
             products.append(p)
             index += 1
         return products
+
+    @classmethod
+    def preflight(cls, extra_args=None):
+        return vtex_preflight(
+            extra_args, 'https://www.motorola.cl/smartphones/d')
