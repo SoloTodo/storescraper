e67584895c31f8509b3c2378215c700ec2ba1a1e
Vijay Khemlani <vj@solotodo.com>
2023-12-22 10:21:03 -0300

    * Updated Dreamtec

--- storescraper/stores/dreamtec.py
+++ storescraper/stores/dreamtec.py
@@ -1,82 +1,83 @@
+import re
 from decimal import Decimal
-import logging
-from bs4 import BeautifulSoup
 from storescraper.categories import ALL_IN_ONE, GAMING_CHAIR, HEADPHONES, \
-    KEYBOARD, MONITOR, NOTEBOOK, PRINTER, STEREO_SYSTEM, TABLET, TELEVISION, \
-    VIDEO_GAME_CONSOLE, WEARABLE, RAM, MOUSE
+    MONITOR, NOTEBOOK, PRINTER, STEREO_SYSTEM, TABLET, TELEVISION, \
+    VIDEO_GAME_CONSOLE, MOUSE
 from storescraper.product import Product
 from storescraper.store_with_url_extensions import StoreWithUrlExtensions
-from storescraper.utils import remove_words, session_with_proxy
+from storescraper.utils import session_with_proxy, \
+    html_to_markdown, check_ean13
 
 
 class Dreamtec(StoreWithUrlExtensions):
     url_extensions = [
-        ['gamer-zone/accesorios-gamer-zone', MOUSE],
-        ['gamer-zone/audifonos-gamer-zone', HEADPHONES],
-        ['gamer-zone/audio', STEREO_SYSTEM],
-        ['gamer-zone/consolas', VIDEO_GAME_CONSOLE],
-        ['gamer-zone/monitores-gamer-zone', MONITOR],
-        ['gamer-zone/notebooks-gamer-zone', NOTEBOOK],
-        ['gamer-zone/sillas-gamer', GAMING_CHAIR],
-        ['home-office/accesorios-home-office', KEYBOARD],
-        ['home-office/all-in-one', ALL_IN_ONE],
-        ['home-office/impresoras', PRINTER],
-        ['home-office/monitores', MONITOR],
-        ['home-office/notebooks', NOTEBOOK],
-        ['smart-home/parlantes', STEREO_SYSTEM],
-        ['smart-home/smart-tv', TELEVISION],
-        ['smart-home/smart-watch', WEARABLE],
-        ['smart-home/tablets', TABLET],
+        ['accesorios', MOUSE],
+        ['audifonos', HEADPHONES],
+        ['audio', STEREO_SYSTEM],
+        ['consolas', VIDEO_GAME_CONSOLE],
+        ['monitores', MONITOR],
+        ['notebooks', NOTEBOOK],
+        ['sillas-gamer', GAMING_CHAIR],
+        ['all-in-one', ALL_IN_ONE],
+        ['impresoras', PRINTER],
+        ['macbook', NOTEBOOK],
+        ['parlantes', STEREO_SYSTEM],
+        ['smart-tv', TELEVISION],
+        ['soundbar', STEREO_SYSTEM],
+        ['tablets', TABLET],
     ]
 
     @classmethod
     def discover_urls_for_url_extension(cls, url_extension, extra_args):
         session = session_with_proxy(extra_args)
         product_urls = []
-        url_webpage = 'http://www.dreamtec.cl/{}/' \
-            ''.format(url_extension)
-        data = session.get(url_webpage).text
-        soup = BeautifulSoup(data, 'html.parser')
-        product_containers = soup.findAll('div', 'foto-prod')
-        if not product_containers:
-            logging.warning('Empty category: ' + url_extension)
+        page = 1
+        
+        while True:
+            if page > 20:
+                raise Exception('Page overflow')
 
-        for container in product_containers:
-            product_url = container.find('a')['href']
-            product_urls.append(product_url)
+            url_webpage = 'https://dreamtec.cl:3000/categoria/{}?page={}'.format(url_extension, page)
+            print(url_webpage)
+            response = session.get(url_webpage, verify=False)
+            products_json = response.json()['results']
+
+            if not products_json:
+                break
+    
+            for container in products_json:
+                product_url = 'https://www.dreamtec.cl/' + container['slug']
+                product_urls.append(product_url)
+            page += 1
         return product_urls
 
     @classmethod
     def products_for_url(cls, url, category=None, extra_args=None):
         print(url)
         session = session_with_proxy(extra_args)
-        response = session.get(url)
-        soup = BeautifulSoup(response.text, 'html.parser')
+        slug = re.search(r'https://www.dreamtec.cl/(.+)$', url).groups()[0]
+        endpoint = 'https://dreamtec.cl:3000/slug/' + slug
+        response = session.get(endpoint, verify=False)
+        product_data = response.json()['results'][0]
+        name = product_data['nombre']
+        description = html_to_markdown(product_data['descripcion'])
+        sku = product_data['sku']
+        normal_price = Decimal(product_data['precio'])
+        offer_price = Decimal(product_data['precio_descuento'])
+        part_number = product_data['pn']
+        stock = product_data['stock']
 
-        if 'No hemos podido encontrar el contenido' in soup.text:
-            return []
-
-        name = soup.find('h1').text.strip()
-        description = soup.find('div', {'id': 'nav-home'}).text.strip()
-        sku_detalle_tags = soup.findAll('span', 'sku-detalle')
-        sku = str(sku_detalle_tags[0].find('strong').text)
-
-        if len(sku_detalle_tags) == 3:
-            part_number = str(sku_detalle_tags[1].find('strong').text)
+        ean_candidate = product_data['upc'] or ''
+        if len(ean_candidate) == 12:
+            ean_candidate = '0' + ean_candidate
+        if check_ean13(ean_candidate):
+            ean = ean_candidate
         else:
-            part_number = None
+            ean = None
 
-        price_tag = soup.find('strong', 'fs-3')
-        if not price_tag:
-            return []
-        normal_price = Decimal(remove_words(price_tag.text))
-        offer_price = Decimal(remove_words(soup.find('h3', 'fs-1').text))
-
-        stock_select = soup.find('select', {'id': 'select-cantidad'})
-        options = stock_select.findAll('option')
-        stock = int(options[-1]['value'])
-
-        picture_urls = []
+        picture_urls = ['https://dreamtec.cl/' + product_data[tag].replace(' ', '%20')
+                        for tag in ['foto_1', 'foto_2', 'foto_3']
+                        if tag in product_data]
 
         p = Product(
             name,
@@ -92,6 +93,7 @@ class Dreamtec(StoreWithUrlExtensions):
             sku=sku,
             part_number=part_number,
             picture_urls=picture_urls,
-            description=description
+            description=description,
+            ean=ean
         )
         return [p]
