961531f65d15dd364dc9b06e18ddbfc33d3cbfd8
Vijay Khemlani <vj@solotodo.com>
2024-02-01 12:46:29 -0300

    * Updated GamingHouse

--- storescraper/stores/gaming_house.py
+++ storescraper/stores/gaming_house.py
@@ -3,81 +3,76 @@ from decimal import Decimal
 
 from bs4 import BeautifulSoup
 
-from storescraper.categories import MONITOR, MOUSE, KEYBOARD, GAMING_CHAIR, \
-    POWER_SUPPLY, COMPUTER_CASE, PROCESSOR, VIDEO_CARD, MOTHERBOARD, RAM, \
-    HEADPHONES, NOTEBOOK, SOLID_STATE_DRIVE, MICROPHONE, CPU_COOLER
+from storescraper.categories import (
+    MONITOR,
+    MOUSE,
+    KEYBOARD,
+    GAMING_CHAIR,
+    POWER_SUPPLY,
+    COMPUTER_CASE,
+    PROCESSOR,
+    VIDEO_CARD,
+    MOTHERBOARD,
+    RAM,
+    HEADPHONES,
+    NOTEBOOK,
+    SOLID_STATE_DRIVE,
+    MICROPHONE,
+    CPU_COOLER,
+)
 from storescraper.product import Product
 from storescraper.store import Store
+from storescraper.store_with_url_extensions import StoreWithUrlExtensions
 from storescraper.utils import session_with_proxy, remove_words
 
 
-class GamingHouse(Store):
+class GamingHouse(StoreWithUrlExtensions):
     preferred_discover_urls_concurrency = 1
     preferred_products_for_url_concurrency = 1
 
-    @classmethod
-    def categories(cls):
-        return [
-            MONITOR,
-            MOUSE,
-            KEYBOARD,
-            GAMING_CHAIR,
-            POWER_SUPPLY,
-            COMPUTER_CASE,
-            MOTHERBOARD,
-            PROCESSOR,
-            VIDEO_CARD,
-            RAM,
-            HEADPHONES,
-            NOTEBOOK,
-            SOLID_STATE_DRIVE,
-            MICROPHONE,
-            CPU_COOLER,
-        ]
+    url_extensions = [
+        ["hardware/almacenamiento", SOLID_STATE_DRIVE],
+        ["hardware/fuentes-de-poder", POWER_SUPPLY],
+        ["hardware/gabinetes", COMPUTER_CASE],
+        ["hardware/memorias-ram", RAM],
+        ["hardware/placas-madre", MOTHERBOARD],
+        ["hardware/procesadores", PROCESSOR],
+        ["hardware/refrigeracion-cpu", CPU_COOLER],
+        ["hardware/tarjetas-graficas", VIDEO_CARD],
+        ["notebooks", NOTEBOOK],
+        ["perifericos/audifonos", HEADPHONES],
+        ["perifericos/microfonos", MICROPHONE],
+        ["perifericos/monitores", MONITOR],
+        ["perifericos/mouse", MOUSE],
+        ["perifericos/teclados", KEYBOARD],
+        ["sillas", GAMING_CHAIR],
+    ]
 
     @classmethod
-    def discover_urls_for_category(cls, category, extra_args=None):
-        url_extensions = [
-            ['hardware/almacenamiento', SOLID_STATE_DRIVE],
-            ['hardware/fuentes-de-poder', POWER_SUPPLY],
-            ['hardware/gabinetes', COMPUTER_CASE],
-            ['hardware/memorias-ram', RAM],
-            ['hardware/placas-madre', MOTHERBOARD],
-            ['hardware/procesadores', PROCESSOR],
-            ['hardware/refrigeracion-cpu', CPU_COOLER],
-            ['hardware/tarjetas-graficas', VIDEO_CARD],
-            ['notebooks', NOTEBOOK],
-            ['perifericos/audifonos', HEADPHONES],
-            ['perifericos/microfonos', MICROPHONE],
-            ['perifericos/monitores', MONITOR],
-            ['perifericos/mouse', MOUSE],
-            ['perifericos/teclados', KEYBOARD],
-            ['sillas', GAMING_CHAIR],
-        ]
+    def discover_urls_for_url_extension(cls, url_extension, extra_args=None):
         session = session_with_proxy(extra_args)
         product_urls = []
-        for url_extension, local_category in url_extensions:
-            if local_category != category:
-                continue
-            page = 1
-            while True:
-                if page > 10:
-                    raise Exception('page overflow: ' + url_extension)
-                url_webpage = 'https://gaming-house.cl/categoria-producto/' \
-                              '{}/page/{}/'.format(url_extension, page)
-                print(url_webpage)
-                response = session.get(url_webpage, timeout=30)
-                soup = BeautifulSoup(response.text, 'html.parser')
-                product_containers = soup.findAll('div', 'product-grid-item')
+        page = 1
+        while True:
+            if page > 10:
+                raise Exception("page overflow: " + url_extension)
+            url_webpage = (
+                "https://gaming-house.cl/categoria-producto/"
+                "{}/page/{}/".format(url_extension, page)
+            )
+            print(url_webpage)
+            response = session.get(url_webpage, timeout=30)
+            soup = BeautifulSoup(response.text, "html.parser")
+            product_containers = soup.findAll("div", "product-grid-item")
 
-                if not product_containers:
-                    if page == 1:
-                        logging.warning('Empty category: ' + url_extension)
-                    break
-                for container in product_containers:
-                    product_url = container.find('a')['href']
-                    product_urls.append(product_url)
-                page += 1
+            if not product_containers:
+                if page == 1:
+                    logging.warning("Empty category: " + url_extension)
+                break
+            for container in product_containers:
+                product_url = container.find("a")["href"]
+                product_urls.append(product_url)
+            page += 1
         return product_urls
 
     @classmethod
@@ -85,24 +80,26 @@ class GamingHouse(Store):
         print(url)
         session = session_with_proxy(extra_args)
         response = session.get(url, timeout=30)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        name = soup.find('h1', 'product_title').text
-        sku = soup.find('link', {'rel': 'shortlink'})['href'].split('p=')[-1]
-        if soup.find('p', 'stock in-stock'):
-            stock = int(soup.find('p', 'stock in-stock').text.split()[0])
-        elif soup.find('button', {'name': 'add-to-cart'}):
+        soup = BeautifulSoup(response.text, "html.parser")
+        name = soup.find("h1", "product_title").text.strip()
+        sku = soup.find("link", {"rel": "shortlink"})["href"].split("p=")[-1]
+        if soup.find("p", "stock in-stock"):
+            stock = int(soup.find("p", "stock in-stock").text.split()[0])
+        elif soup.find("button", {"name": "add-to-cart"}):
             stock = -1
         else:
             stock = 0
-        if soup.find('p', 'price').find('ins'):
-            price = Decimal(
-                remove_words(soup.find('p', 'price').find('ins').text))
+        if soup.find("p", "price").find("ins"):
+            offer_price = Decimal(
+                remove_words(soup.find("p", "price").find("ins").text)
+            )
         else:
-            price = Decimal(remove_words(soup.find('p', 'price').text))
-        picture_urls = [tag['src'] for tag in soup.find('div',
-                                                        'woocommerce-product'
-                                                        '-gallery').findAll(
-            'img')]
+            offer_price = Decimal(remove_words(soup.find("p", "price").text))
+        normal_price = (offer_price * Decimal("1.039")).quantize(0)
+        picture_urls = [
+            tag["src"]
+            for tag in soup.find("div", "woocommerce-product" "-gallery").findAll("img")
+        ]
         p = Product(
             name,
             cls.__name__,
@@ -111,10 +108,10 @@ class GamingHouse(Store):
             url,
             sku,
             stock,
-            price,
-            price,
-            'CLP',
+            normal_price,
+            offer_price,
+            "CLP",
             sku=sku,
-            picture_urls=picture_urls
+            picture_urls=picture_urls,
         )
         return [p]
