977ede1bfc992f08e8fab634894461c37721732b
Vijay Khemlani <vj@solotodo.com>
2024-02-07 12:54:04 -0300

    * Updated ASUS Store, Reuse

--- storescraper/stores/asus_store.py
+++ storescraper/stores/asus_store.py
@@ -4,52 +4,82 @@ from decimal import Decimal
 
 from bs4 import BeautifulSoup
 
-from storescraper.categories import NOTEBOOK, ALL_IN_ONE, VIDEO_CARD, MOUSE, \
-    VIDEO_GAME_CONSOLE
+from storescraper.categories import (
+    NOTEBOOK,
+    ALL_IN_ONE,
+    VIDEO_CARD,
+    MOUSE,
+    VIDEO_GAME_CONSOLE,
+)
 from storescraper.product import Product
 from storescraper.store_with_url_extensions import StoreWithUrlExtensions
-from storescraper.utils import session_with_proxy, html_to_markdown
+from storescraper.utils import (
+    session_with_proxy,
+    html_to_markdown,
+)
 
 
 class AsusStore(StoreWithUrlExtensions):
     url_extensions = [
-        ('laptops', NOTEBOOK),
-        ('displays-desktops', ALL_IN_ONE),
-        ('motherboards-components', VIDEO_CARD),
-        ('accessories', MOUSE),
-        ('mobile-handhelds', VIDEO_GAME_CONSOLE),
+        ("laptops", NOTEBOOK),
+        ("displays-desktops", ALL_IN_ONE),
+        ("motherboards-components", VIDEO_CARD),
+        ("accessories", MOUSE),
+        ("mobile-handhelds", VIDEO_GAME_CONSOLE),
     ]
 
     @classmethod
     def discover_urls_for_url_extension(cls, url_extension, extra_args=None):
         session = session_with_proxy(extra_args)
-        session.headers['user-agent'] = \
-            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 ' \
-            '(KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
+        session.headers["user-agent"] = (
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 "
+            "(KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"
+        )
 
         product_urls = []
         page = 1
 
         while True:
             if page >= 10:
-                raise Exception('Page overflow')
+                raise Exception("Page overflow")
 
-            url_webpage = 'https://odinapi.asus.com/recent-data/apiv2/' \
-                          'ShopAPI/ShopFilterResult?SystemCode=asus&' \
-                          'WebsiteCode=cl&ProductLevel1Code={}&' \
-                          'PageSize=25&PageIndex={}&Sort=' \
-                          'Newsest&siteID=www'.format(url_extension, page)
+            url_webpage = (
+                "https://odinapi.asus.com/recent-data/apiv2/"
+                "ShopAPI/ShopFilterResult?SystemCode=asus&"
+                "WebsiteCode=cl&ProductLevel1Code={}&"
+                "PageSize=25&PageIndex={}&Sort="
+                "Newsest&siteID=www".format(url_extension, page)
+            )
             print(url_webpage)
             page += 1
             response = session.get(url_webpage).json()
-            product_entries = response['Result']['ProductList']
+            product_entries = response["Result"]["ProductList"]
             if not product_entries:
                 if page == 1:
-                    logging.warning('Empty category: ' + url_extension)
+                    logging.warning("Empty category: " + url_extension)
                 break
 
+            product_ids = []
+            rog_ids = []
+
             for product_entry in product_entries:
-                product_url = product_entry['ProductCardURL']
+                source_type = product_entry["SourceType"]
+                if source_type == "prod":
+                    product_ids.append(product_entry["ProductID"])
+                elif source_type == "rog":
+                    rog_ids.append(product_entry["PartNo"])
+                else:
+                    raise Exception("Invalid source type " + source_type)
+
+            endpoint = (
+                "https://odinapi.asus.com/recent-data/apiv2/GetPrice?SystemCode=asus&WebsiteCode=cl"
+                "&ProductId={}&ROGProduct={}"
+            ).format(",".join(product_ids), ",".join(rog_ids))
+            print(endpoint)
+            json_response = session.get(endpoint).json()
+
+            for entry in json_response["Result"]["ProductList"]:
+                product_url = entry["BuyLink"]
                 product_urls.append(product_url)
         return product_urls
 
@@ -65,10 +95,10 @@ class AsusStore(StoreWithUrlExtensions):
         if response.status_code == 401:
             return []
 
-        soup = BeautifulSoup(response.text, 'html.parser')
+        soup = BeautifulSoup(response.text, "html.parser")
 
-        for script_tag in soup.findAll('script', {'type': 'text/x-magento-init'}):
-            if 'jsonConfig' in script_tag.text:
+        for script_tag in soup.findAll("script", {"type": "text/x-magento-init"}):
+            if "jsonConfig" in script_tag.text:
                 variations_data = json.loads(script_tag.text)
                 break
         else:
@@ -76,13 +106,19 @@ class AsusStore(StoreWithUrlExtensions):
 
         products = []
 
-        if variations_data:
-            variations_data = variations_data['[data-role=swatch-options]']['Magento_Swatches/js/swatch-renderer']['jsonConfig']
-            for key, sku in variations_data['sku'].items():
-                name = variations_data['sales_model_name'][key]
-                price = Decimal(variations_data['optionPrices'][key]['finalPrice']['amount'])
-                picture_urls = [x['full'] for x in variations_data['images'][key]]
-                description = html_to_markdown(variations_data['dynamic']['short_description'][key]['value'])
+        if variations_data and "[data-role=swatch-options]" in variations_data:
+            variations_data = variations_data["[data-role=swatch-options]"][
+                "Magento_Swatches/js/swatch-renderer"
+            ]["jsonConfig"]
+            for key, sku in variations_data["sku"].items():
+                name = variations_data["sales_model_name"][key]
+                price = Decimal(
+                    variations_data["optionPrices"][key]["finalPrice"]["amount"]
+                )
+                picture_urls = [x["full"] for x in variations_data["images"][key]]
+                description = html_to_markdown(
+                    variations_data["dynamic"]["short_description"][key]["value"]
+                )
 
                 p = Product(
                     name,
@@ -94,30 +130,36 @@ class AsusStore(StoreWithUrlExtensions):
                     -1,
                     price,
                     price,
-                    'CLP',
+                    "CLP",
                     sku=sku,
                     part_number=sku,
                     picture_urls=picture_urls,
-                    description=description
+                    description=description,
                 )
                 products.append(p)
 
         else:
-            base_name = soup.find('span', {'data-dynamic': 'product_name'})\
-                .text.replace('\u200b', '').strip()
-            model_name = soup.find('div', 'simple-sku').text.split(' - ')[-1]\
+            base_name = (
+                soup.find("span", {"data-dynamic": "product_name"})
+                .text.replace("\u200b", "")
                 .strip()
-            name = '{} ({})'.format(base_name, model_name)
-            key = soup.find('div', 'price-box')['data-product-id']
-            sku = soup.find('div', 'simple-part').text.split(' - ')[-1]
-            if soup.find('div', 'box-tocart').find('div', 'out-stock'):
+            )
+            model_name = soup.find("div", "simple-sku").text.split(" - ")[-1].strip()
+            name = "{} ({})".format(base_name, model_name)
+            key = soup.find("div", "price-box")["data-product-id"]
+            sku = soup.find("div", "simple-part").text.split(" - ")[-1]
+            if soup.find("div", "box-tocart").find("div", "out-stock"):
                 stock = 0
             else:
                 stock = -1
-            price = Decimal(soup.find('span', {'data-price-type': 'finalPrice'})[
-                                'data-price-amount'])
-            picture_urls = [tag['src'] for tag in
-                            soup.find('div', 'product media').findAll('img')]
+            price = Decimal(
+                soup.find("span", {"data-price-type": "finalPrice"})[
+                    "data-price-amount"
+                ]
+            )
+            picture_urls = [
+                tag["src"] for tag in soup.find("div", "product media").findAll("img")
+            ]
             p = Product(
                 name,
                 cls.__name__,
@@ -128,10 +170,10 @@ class AsusStore(StoreWithUrlExtensions):
                 stock,
                 price,
                 price,
-                'CLP',
+                "CLP",
                 sku=sku,
                 part_number=sku,
-                picture_urls=picture_urls
+                picture_urls=picture_urls,
             )
             products.append(p)
         return products
--- storescraper/stores/reuse.py
+++ storescraper/stores/reuse.py
@@ -4,8 +4,14 @@ from decimal import Decimal
 
 from bs4 import BeautifulSoup
 
-from storescraper.categories import CELL, NOTEBOOK, ALL_IN_ONE, TABLET, \
-    HEADPHONES, WEARABLE
+from storescraper.categories import (
+    CELL,
+    NOTEBOOK,
+    ALL_IN_ONE,
+    TABLET,
+    HEADPHONES,
+    WEARABLE,
+)
 from storescraper.product import Product
 from storescraper.store_with_url_extensions import StoreWithUrlExtensions
 from storescraper.utils import session_with_proxy, html_to_markdown
@@ -13,13 +19,13 @@ from storescraper.utils import session_with_proxy, html_to_markdown
 
 class Reuse(StoreWithUrlExtensions):
     url_extensions = [
-        ['celulares', CELL],
-        ['computadores/tipo-de-producto_notebook', NOTEBOOK],
-        ['computadores/tipo-de-producto_chromebook', NOTEBOOK],
-        ['computadores/tipo-de-producto_imac', ALL_IN_ONE],
-        ['tablets-ipad', TABLET],
-        ['accesorios-1/audifonos', HEADPHONES],
-        ['accesorios-1/watch', WEARABLE]
+        ["celulares", CELL],
+        ["computadores/tipo-de-producto_notebook", NOTEBOOK],
+        ["computadores/tipo-de-producto_chromebook", NOTEBOOK],
+        ["computadores/tipo-de-producto_imac", ALL_IN_ONE],
+        ["tablets-ipad", TABLET],
+        ["accesorios-1/audifonos", HEADPHONES],
+        ["accesorios-1/watch", WEARABLE],
     ]
 
     @classmethod
@@ -29,20 +35,21 @@ class Reuse(StoreWithUrlExtensions):
         product_urls = []
         while True:
             if page > 10:
-                raise Exception('page overflow: ' + url_extension)
-            url_webpage = 'https://www.reuse.cl/collections/' \
-                          '{}?page={}'.format(url_extension, page)
+                raise Exception("page overflow: " + url_extension)
+            url_webpage = "https://www.reuse.cl/collections/" "{}?page={}".format(
+                url_extension, page
+            )
             print(url_webpage)
             response = session.get(url_webpage)
-            soup = BeautifulSoup(response.text, 'html.parser')
-            product_containers = soup.findAll('li', 'productgrid--item')
+            soup = BeautifulSoup(response.text, "html.parser")
+            product_containers = soup.findAll("li", "productgrid--item")
             if not product_containers:
                 if page == 1:
-                    logging.warning('Empty category: ' + url_extension)
+                    logging.warning("Empty category: " + url_extension)
                 break
             for container in product_containers:
-                product_url = container.find('a')['href']
-                product_urls.append('https://www.reuse.cl' + product_url)
+                product_url = container.find("a")["href"]
+                product_urls.append("https://www.reuse.cl" + product_url)
             page += 1
         return product_urls
 
@@ -51,26 +58,29 @@ class Reuse(StoreWithUrlExtensions):
         print(url)
         session = session_with_proxy(extra_args)
         response = session.get(url)
-        soup = BeautifulSoup(response.text, 'html.parser')
+        soup = BeautifulSoup(response.text, "html.parser")
         product_json = json.loads(
-            soup.find('script', {'data-section-id': 'template--16894576066777__main'}).text)[
-            'product']
-        description = html_to_markdown(product_json['description'])
-        vendor = product_json['vendor']
-        seller = vendor if vendor != 'Reuse Chile' else None
+            soup.find(
+                "script", {"data-section-id": "template--17204236714201__main"}
+            ).text
+        )["product"]
+        description = html_to_markdown(product_json["description"])
+        vendor = product_json["vendor"]
+        seller = vendor if vendor != "Reuse Chile" else None
 
         products = []
-        for variant in product_json['variants']:
-            if not variant['inventory_management']:
+        for variant in product_json["variants"]:
+            if not variant["inventory_management"]:
                 continue
 
-            name = variant['name']
-            sku = variant['sku']
-            key = str(variant['id'])
-            stock = -1 if variant['available'] else 0
-            price = Decimal(variant['price'] / 100)
-            picture_urls = ['https:' + tag.split('?v')[0] for tag in
-                            product_json['images']]
+            name = variant["name"]
+            sku = variant["sku"]
+            key = str(variant["id"])
+            stock = -1 if variant["available"] else 0
+            price = Decimal(variant["price"] / 100)
+            picture_urls = [
+                "https:" + tag.split("?v")[0] for tag in product_json["images"]
+            ]
             p = Product(
                 name,
                 cls.__name__,
@@ -81,12 +91,12 @@ class Reuse(StoreWithUrlExtensions):
                 stock,
                 price,
                 price,
-                'CLP',
+                "CLP",
                 sku=sku,
                 picture_urls=picture_urls,
-                condition='https://schema.org/RefurbishedCondition',
+                condition="https://schema.org/RefurbishedCondition",
                 description=description,
-                seller=seller
+                seller=seller,
             )
             products.append(p)
 
