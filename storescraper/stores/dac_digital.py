2eb117a22a3214a84b73865b52ffcb613ea09d09
Vijay Khemlani <vj@solotodo.com>
2024-02-15 11:36:42 -0300

    * Updated DacDigital

--- storescraper/stores/dac_digital.py
+++ storescraper/stores/dac_digital.py
@@ -1,8 +1,15 @@
 import logging
 
 from bs4 import BeautifulSoup
-from storescraper.categories import VIDEO_GAME_CONSOLE, MOUSE, \
-    VIDEO_CARD, TABLET, PRINTER, HEADPHONES, NOTEBOOK
+from storescraper.categories import (
+    VIDEO_GAME_CONSOLE,
+    MOUSE,
+    VIDEO_CARD,
+    TABLET,
+    PRINTER,
+    HEADPHONES,
+    NOTEBOOK,
+)
 from storescraper.stores.mercado_libre_chile import MercadoLibreChile
 from storescraper.utils import session_with_proxy
 
@@ -23,12 +30,13 @@ class DacDigital(MercadoLibreChile):
     @classmethod
     def discover_urls_for_category(cls, category, extra_args=None):
         url_extensions = [
-            ['computacion/perifericos-pc', MOUSE],
-            ['computacion/componentes-pc', VIDEO_CARD],
-            ['computacion/tablets-accesorios', TABLET],
-            ['computacion/impresion', PRINTER],
-            ['electronica-audio-video', HEADPHONES],
-            ['computacion/notebooks-accesorios', NOTEBOOK],
+            ["computacion/perifericos-pc", MOUSE],
+            ["computacion/componentes-pc", VIDEO_CARD],
+            ["computacion/tablets-accesorios", TABLET],
+            ["computacion/impresion", PRINTER],
+            ["electronica-audio-video", HEADPHONES],
+            ["computacion/notebooks-accesorios", NOTEBOOK],
+            ["consolas-videojuegos", VIDEO_GAME_CONSOLE],
         ]
 
         session = session_with_proxy(extra_args)
@@ -39,22 +47,25 @@ class DacDigital(MercadoLibreChile):
             page = 1
             while True:
                 if page > 10:
-                    raise Exception('Page overflow: ' + url_extension)
-                index = str(50*(page - 1) + 1)
-                url_webpage = 'https://www.dacdigital.cl/listado/' \
-                    '{}/_Desde_{}_NoIndex_True'.format(url_extension, index)
+                    raise Exception("Page overflow: " + url_extension)
+                index = str(50 * (page - 1) + 1)
+                url_webpage = (
+                    "https://www.dacdigital.cl/listado/"
+                    "{}/_Desde_{}_NoIndex_True".format(url_extension, index)
+                )
                 data = session.get(url_webpage).text
-                soup = BeautifulSoup(data, 'html.parser')
-                product_containers = soup.findAll(
-                    'li', 'ui-search-layout__item')
+                soup = BeautifulSoup(data, "html.parser")
+                product_containers = soup.findAll("li", "ui-search-layout__item")
                 if not product_containers:
                     if page == 1:
-                        logging.warning('Empty category: ' + url_extension)
+                        logging.warning("Empty category: " + url_extension)
                     break
                 for container in product_containers:
-                    product_url = container.find(
-                        'a',
-                        'ui-search-link')['href'].split('#')[0].split('?')[0]
+                    product_url = (
+                        container.find("a", "ui-search-link")["href"]
+                        .split("#")[0]
+                        .split("?")[0]
+                    )
                     product_urls.append(product_url)
                 page += 1
         return product_urls
@@ -62,4 +73,5 @@ class DacDigital(MercadoLibreChile):
     @classmethod
     def products_for_url(cls, url, category=None, extra_args=None):
         return cls._products_for_url_with_custom_price(
-            url, category=category, extra_args=extra_args)
+            url, category=category, extra_args=extra_args
+        )
