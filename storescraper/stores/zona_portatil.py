b9b66ffd1272a2e93c49f1c0bbfab29ce26934b6
Vijay Khemlani <vj@solotodo.com>
2024-02-21 17:26:30 -0300

    * Updated ZonaPortatil

--- storescraper/stores/zona_portatil.py
+++ storescraper/stores/zona_portatil.py
@@ -1,13 +1,33 @@
 import logging
+import re
 from decimal import Decimal
 
 from bs4 import BeautifulSoup
 
-from storescraper.categories import ALL_IN_ONE, NOTEBOOK, TABLET, HEADPHONES, \
-    VIDEO_GAME_CONSOLE, GAMING_CHAIR, KEYBOARD, COMPUTER_CASE, RAM, \
-    MOTHERBOARD, PROCESSOR, VIDEO_CARD, STORAGE_DRIVE, SOLID_STATE_DRIVE, \
-    EXTERNAL_STORAGE_DRIVE, MEMORY_CARD, MONITOR, PRINTER, STEREO_SYSTEM, \
-    MICROPHONE, CPU_COOLER, POWER_SUPPLY
+from storescraper.categories import (
+    ALL_IN_ONE,
+    NOTEBOOK,
+    HEADPHONES,
+    VIDEO_GAME_CONSOLE,
+    GAMING_CHAIR,
+    COMPUTER_CASE,
+    RAM,
+    MOTHERBOARD,
+    PROCESSOR,
+    VIDEO_CARD,
+    STORAGE_DRIVE,
+    SOLID_STATE_DRIVE,
+    EXTERNAL_STORAGE_DRIVE,
+    MEMORY_CARD,
+    MONITOR,
+    PRINTER,
+    CPU_COOLER,
+    POWER_SUPPLY,
+    KEYBOARD_MOUSE_COMBO,
+    CASE_FAN,
+    UPS,
+    MOUSE,
+)
 from storescraper.product import Product
 from storescraper.store_with_url_extensions import StoreWithUrlExtensions
 from storescraper.utils import session_with_proxy, remove_words
@@ -15,41 +35,49 @@ from storescraper.utils import session_with_proxy, remove_words
 
 class ZonaPortatil(StoreWithUrlExtensions):
     url_extensions = [
-        ['all-in-one-todo-en-uno', ALL_IN_ONE],
-        ['notebook', NOTEBOOK],
-        ['tablets', TABLET],
-        ['audifonos-accesorios', HEADPHONES],
-        ['parlantes', STEREO_SYSTEM],
-        ['consolas', VIDEO_GAME_CONSOLE],
-        ['sillas-gamer', GAMING_CHAIR],
-        ['teclados-mouse', KEYBOARD],
-        ['gabinetes', COMPUTER_CASE],
-        ['memorias-notebook', RAM],
-        ['memorias-pc', RAM],
-        ['placas-madres', MOTHERBOARD],
-        ['procesadores', PROCESSOR],
-        ['tarjetas-graficas', VIDEO_CARD],
-        ['discos-duros-pc', STORAGE_DRIVE],
-        ['discos-duros-notebook', STORAGE_DRIVE],
-        ['discos-ssd', SOLID_STATE_DRIVE],
-        ['discos-duros-externos', EXTERNAL_STORAGE_DRIVE],
-        ['ssd-externos', EXTERNAL_STORAGE_DRIVE],
-        ['discos-duros-video-vigilancia', STORAGE_DRIVE],
-        ['memorias-flash', MEMORY_CARD],
-        ['monitores', MONITOR],
-        ['impresoras', PRINTER],
-        ['fuente-de-poder', POWER_SUPPLY],
-        ['monitores-zona-gamer', MONITOR],
-        ['sillas-gamer', GAMING_CHAIR],
-        ['gabinetes-zona-gamer', COMPUTER_CASE],
-        ['audifonos', HEADPHONES],
-        ['teclados-mouse-zona-gamer', KEYBOARD],
-        ['microfonos', MICROPHONE],
-        ['refrigeracion', CPU_COOLER],
-        ['placas-madres-zona-gamer', MOTHERBOARD],
-        ['procesadores-zona-gamer', PROCESSOR],
-        ['tarjetas-graficas-zona-gamer', VIDEO_CARD],
-        ['cyber-zona-portatil', MONITOR]
+        ["teclado-mouse", KEYBOARD_MOUSE_COMBO],
+        ["fuente-de-poder", POWER_SUPPLY],
+        ["memoria-notebook", RAM],
+        ["memoria-pc", RAM],
+        ["placa-madre", MOTHERBOARD],
+        ["procesador", PROCESSOR],
+        ["tarjeta-de-video", VIDEO_CARD],
+        ["ventilador", CASE_FAN],
+        ["disco-duro-externo", EXTERNAL_STORAGE_DRIVE],
+        ["disco-duro-servidor", STORAGE_DRIVE],
+        ["disco-duro-ssd", SOLID_STATE_DRIVE],
+        ["memorias-flash", MEMORY_CARD],
+        ["aio-todo-en-uno", ALL_IN_ONE],
+        ["notebook", NOTEBOOK],
+        ["ups", UPS],
+        ["impresoras", PRINTER],
+        ["monitores-monitores", MONITOR],
+        ["impresoras-open-box", PRINTER],
+        ["notebook-partes-y-piezas-open-box", NOTEBOOK],
+        ["memoria-notebook-partes-y-piezas-open-box", RAM],
+        ["memoria-pc-partes-y-piezas-open-box", RAM],
+        ["notebook-computadores-open-box", RAM],
+        ["disco-duro-externo-almacenamiento-open-box", EXTERNAL_STORAGE_DRIVE],
+        ["disco-duro-ssd-almacenamiento-open-box", SOLID_STATE_DRIVE],
+        ["ups-y-energia-ups-y-energia-ups-y-energia", UPS],
+        ["mouse", MOUSE],
+        ["teclado-mouse-accesorios-zona-gamer", KEYBOARD_MOUSE_COMBO],
+        ["silla-gamer", GAMING_CHAIR],
+        ["ventilador-accesorios-zona-gamer", CPU_COOLER],
+        ["gabinetes-gabinetes", COMPUTER_CASE],
+        ["monitores-monitores-zona-gamer", MONITOR],
+        ["refrigeracion-refrigeracion", CPU_COOLER],
+        ["audifonos-audio-zona-gamer", HEADPHONES],
+        ["audifonos-audifonos", HEADPHONES],
+        ["memorias-notebook-memorias-notebook", RAM],
+        ["tarjetas-graficas-tarjetas-graficas", VIDEO_CARD],
+        ["tarjeta-de-video-partes-y-piezas-zona-gamer", VIDEO_CARD],
+        ["placa-madre-partes-y-piezas-zona-gamer", MOTHERBOARD],
+        ["consolas-consolas", VIDEO_GAME_CONSOLE],
+        ["memorias-pc-memorias-pc", RAM],
+        ["placas-madres-placas-madres", MOTHERBOARD],
+        ["fuente-de-poder-fuente-de-poder-zona-gamer", POWER_SUPPLY],
+        ["procesadores-procesadores", PROCESSOR],
     ]
 
     @classmethod
@@ -59,29 +87,23 @@ class ZonaPortatil(StoreWithUrlExtensions):
         page = 1
         while True:
             if page > 10:
-                raise Exception('page overflow: ' + url_extension)
+                raise Exception("page overflow: " + url_extension)
 
-            if 'cyber-zona-portatil' == url_extension:
-                url_webpage = 'https://www.zonaportatil.cl/' \
-                    'cyber-zona-portatil/'
-            else:
-                url_webpage = 'https://www.zonaportatil.cl/categoria-pro' \
-                    'ducto/{}/page/{}/'.format(url_extension, page)
+            url_webpage = "https://zonaportatil.cl/categoria/{}/page/{}/".format(
+                url_extension, page
+            )
             print(url_webpage)
             response = session.get(url_webpage)
-            soup = BeautifulSoup(response.text, 'html.parser')
-            product_containers = soup.findAll('li', 'product')
+            soup = BeautifulSoup(response.text, "html.parser")
+            product_containers = soup.findAll("article", "product")
 
             if not product_containers:
                 if page == 1:
-                    logging.warning('Empty category')
+                    logging.warning("Empty category")
                 break
             for container in product_containers:
-                product_url = container.find('a')['href']
+                product_url = container.find("a")["href"]
                 product_urls.append(product_url)
-
-            if 'cyber-zona-portatil' == url_extension:
-                break
             page += 1
         return product_urls
 
@@ -90,47 +112,58 @@ class ZonaPortatil(StoreWithUrlExtensions):
         print(url)
         session = session_with_proxy(extra_args)
         response = session.get(url)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        name = soup.find('h1', 'product_title').text
+        soup = BeautifulSoup(response.text, "html.parser")
+        name = soup.find("p", "product_title").text.strip()
+        key = soup.find("link", {"rel": "shortlink"})["href"].split("?p=")[-1]
 
-        if soup.find('p', 'price').find('ins'):
-            normal_price = Decimal(
-                remove_words(soup.find('p', 'price').find('ins').text))
+        qty_input = soup.find("input", "input-text qty text")
+        if qty_input:
+            if qty_input["max"]:
+                stock = int(qty_input["max"])
+            else:
+                stock = -1
         else:
-            price_text = soup.find('p', 'price').text.strip()
-            if not price_text:
-                return []
-            normal_price = Decimal(remove_words(price_text))
-        offer_price = (normal_price * Decimal('0.95')).quantize(0)
+            if soup.find("button", "single_add_to_cart_button"):
+                stock = 1
+            else:
+                stock = 0
 
-        if 'OPEN BOX' in name:
-            condition = 'https://schema.org/RefurbishedCondition'
+        price_containers = soup.find("p", "price")
+        normal_price = Decimal(
+            remove_words(price_containers.find("span", "woocommerce-Price-amount").text)
+        )
+        offer_price = Decimal(
+            remove_words(
+                price_containers.find("p", "price-transferencia").find("bdi").text
+            )
+        )
+        sku = re.search(r"Part Number: (.+?)<", response.text).groups()[0]
+        picture_urls = [
+            x["href"]
+            for x in soup.find("div", "woocommerce-product-gallery__wrapper").findAll(
+                "a"
+            )
+        ]
+
+        if "OPEN" in name.upper():
+            condition = "https://schema.org/OpenBoxCondition"
         else:
-            condition = 'https://schema.org/NewCondition'
-        sku = soup.find('span', 'sku').text
-        key = soup.find('link', {'rel': 'shortlink'})['href'].split('p=')[-1]
-        if soup.find('p', 'stock in-stock'):
-            stock = int(soup.find('p', 'stock in-stock').text.split()[0])
-        else:
-            stock = 0
-        picture_urls = ['https://www.zonaportatil.cl' + tag['src'] for tag in
-                        soup.find('div', 'woocommerce-product-gallery')
-                        .findAll('img')]
+            condition = "https://schema.org/NewCondition"
+
         p = Product(
             name,
             cls.__name__,
             category,
             url,
             url,
-            key,
+            sku,
             stock,
             normal_price,
             offer_price,
-            'CLP',
-            sku=sku,
+            "CLP",
+            sku=key,
             part_number=sku,
             picture_urls=picture_urls,
-            condition=condition
-
+            condition=condition,
         )
         return [p]
