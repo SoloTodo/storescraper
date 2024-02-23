3e07699d79499d6c433d6169b7ec06f2c55b576d
Vijay Khemlani <vj@solotodo.com>
2024-02-23 12:04:36 -0300

    * Refactored LaPolar * Fixed Sodimac and Tottus banners

--- storescraper/stores/tottus.py
+++ storescraper/stores/tottus.py
@@ -1,3 +1,4 @@
+from storescraper import banner_sections as bs
 from .falabella import Falabella
 
 
@@ -5,6 +6,10 @@ class Tottus(Falabella):
     store_and_subdomain = "tottus"
     seller = [{"id": "TOTTUS", "section_prefix": None, "include_in_fast_mode": True}]
     seller_blacklist = []
+    banners_base_url = "https://tottus.falabella.com/tottus-cl/{}"
+    banners_sections_data = [
+        [bs.HOME, "Home", bs.SUBSECTION_TYPE_HOME, ""],
+    ]
 
     @classmethod
     def products_for_url(cls, url, category=None, extra_args=None):
