45ac02ea083eaa80554b825d433e2f8e98e71f1e
Vijay Khemlani <vj@solotodo.com>
2024-03-01 10:42:47 -0300

    * Updated JumboColombia, MobileHut

--- storescraper/stores/jumbo_colombia.py
+++ storescraper/stores/jumbo_colombia.py
@@ -1,5 +1,6 @@
 import base64
 import json
+import re
 
 from bs4 import BeautifulSoup
 from decimal import Decimal
@@ -78,6 +79,8 @@ class JumboColombia(Store):
         session = session_with_proxy(extra_args)
         response = session.get(url)
         soup = BeautifulSoup(response.text, "html5lib")
+        runtime_match = re.search("__RUNTIME__ = (.+)", response.text)
+        runtime_json = json.loads(runtime_match.groups()[0])
 
         product_data_tag = soup.find("template", {"data-varname": "__STATE__"})
         product_data = json.loads(str(product_data_tag.find("script").contents[0]))
@@ -88,7 +91,7 @@ class JumboColombia(Store):
         base_json_key = list(product_data.keys())[0]
         product_specs = product_data[base_json_key]
 
-        key = soup.find("meta", {"property": "product:sku"})["content"]
+        key = runtime_json["route"]["params"]["id"]
         name = product_specs["productName"]
         sku = product_specs["productReference"]
         description = product_specs.get("description", None)
