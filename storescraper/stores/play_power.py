16468501b03b29ad8e3e3d5710d5b01155a459ce
Vijay Khemlani <vj@solotodo.com>
2024-03-08 17:02:43 -0300

    * Updated PlayPower

--- storescraper/stores/play_power.py
+++ storescraper/stores/play_power.py
@@ -3,12 +3,13 @@ import re
 from decimal import Decimal
 import logging
 
+import pyjson5
 from bs4 import BeautifulSoup
 
 from storescraper.categories import MONITOR
 from storescraper.product import Product
 from storescraper.store_with_url_extensions import StoreWithUrlExtensions
-from storescraper.utils import session_with_proxy, remove_words
+from storescraper.utils import session_with_proxy
 
 
 class PlayPower(StoreWithUrlExtensions):
@@ -47,10 +48,49 @@ class PlayPower(StoreWithUrlExtensions):
         session = session_with_proxy(extra_args)
         response = session.get(url)
         soup = BeautifulSoup(response.text, "html.parser")
-
-        variants_tag = soup.find("variant-radios")
         products = []
-        if variants_tag:
+        name_tag = soup.find("h3", {"data-product-type": "title"})
+
+        if name_tag:
+            name_tag = soup.find("h3", {"data-product-type": "title"})
+            page_id = name_tag["data-product-id"]
+
+            search_string = (
+                r'window.__pageflyProducts\["'
+                + re.escape(page_id)
+                + r'"] = ([\s\S]+?);'
+            )
+            match = re.search(search_string, response.text)
+            product_data = pyjson5.decode(match.groups()[0])
+
+            base_name = product_data["title"]
+            picture_urls = ["https:" + x["src"] for x in product_data["media"]]
+            for variant in product_data["variants"]:
+                name = "{} ({})".format(base_name, variant["title"])
+                key = str(variant["id"])
+                stock = -1 if variant["available"] else 0
+                price = Decimal(variant["price"] // 100)
+
+                p = Product(
+                    name,
+                    cls.__name__,
+                    category,
+                    url,
+                    url,
+                    key,
+                    stock,
+                    price,
+                    price,
+                    "CLP",
+                    picture_urls=picture_urls,
+                )
+                products.append(p)
+        else:
+            variants_tag = soup.find("variant-radios")
+
+            if not variants_tag:
+                return []
+
             base_name = soup.find("h1").text.strip()
             variants_data = json.loads(variants_tag.find("script").text)
             for variant in variants_data:
@@ -74,41 +114,4 @@ class PlayPower(StoreWithUrlExtensions):
                     picture_urls=picture_urls,
                 )
                 products.append(p)
-        else:
-            name_tag = soup.find("h3", {"data-product-type": "title"})
-
-            if not name_tag:
-                return []
-
-            name = name_tag.text.strip()
-            key, stock_text = re.search(
-                r'quantity: \["(\d+):(\d)"]', response.text
-            ).groups()
-            stock = int(stock_text)
-            price = Decimal(
-                remove_words(
-                    soup.find("div", {"data-product-type": "price"}).text.strip()
-                )
-            )
-            picture_urls = [
-                "https:" + x.find("img")["src"]
-                for x in soup.find("div", "pf-media-slider").findAll(
-                    "div", "pf-slide-main-media"
-                )
-            ]
-
-            p = Product(
-                name,
-                cls.__name__,
-                category,
-                url,
-                url,
-                key,
-                stock,
-                price,
-                price,
-                "CLP",
-                picture_urls=picture_urls,
-            )
-            products.append(p)
         return products
