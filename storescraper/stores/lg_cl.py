57760b4b58e606c470360ef9fdd8b3b6af25d007
Vijay Khemlani <vj@solotodo.com>
2024-03-20 16:36:20 -0300

    * Updated LgCl

--- storescraper/stores/lg_cl.py
+++ storescraper/stores/lg_cl.py
@@ -1,35 +1,44 @@
 from .lg_v6 import LgV6
-from storescraper.categories import (TELEVISION, STEREO_SYSTEM, REFRIGERATOR,
-                                     WASHING_MACHINE, MONITOR, PROJECTOR,
-                                     DISH_WASHER, AIR_CONDITIONER)
+from storescraper.categories import (
+    TELEVISION,
+    STEREO_SYSTEM,
+    REFRIGERATOR,
+    WASHING_MACHINE,
+    MONITOR,
+    PROJECTOR,
+    DISH_WASHER,
+    AIR_CONDITIONER,
+)
 
 
 class LgCl(LgV6):
-    region_code = 'CL'
-    currency = 'CLP'
-    price_approximation = '0'
+    region_code = "CL"
+    currency = "CLP"
+    price_approximation = "0"
 
     @classmethod
     def _category_paths(cls):
         return [
             # Todos los TVs y Soundbars
-            ('CT52000104', TELEVISION),
+            ("CT52000104", TELEVISION),
             # Object collection - Pose
-            ('CT52000790', TELEVISION),
+            ("CT52000790", TELEVISION),
             # Flex
-            ('CT52000799', TELEVISION),
+            ("CT52000799", TELEVISION),
+            # StandbyMe
+            ("CT52020281", TELEVISION),
             # To do Audio
-            ('CT52000100', STEREO_SYSTEM),
+            ("CT52000100", STEREO_SYSTEM),
             # Proyectores
-            ('CT52000784', PROJECTOR),
+            ("CT52000784", PROJECTOR),
             # Refrigeradores
-            ('CT52000103', REFRIGERATOR),
+            ("CT52000103", REFRIGERATOR),
             # Lavadoras
-            ('CT52000102', WASHING_MACHINE),
+            ("CT52000102", WASHING_MACHINE),
             # Lavavajillas
-            ('CT52000105', DISH_WASHER),
+            ("CT52000105", DISH_WASHER),
             # Monitores
-            ('CT52000106', MONITOR),
+            ("CT52000106", MONITOR),
             # Aires acondicionados
-            ('CT52002586', AIR_CONDITIONER),
+            ("CT52002586", AIR_CONDITIONER),
         ]
