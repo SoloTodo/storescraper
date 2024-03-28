c4ba60a2f2afd85e876ef2fea49dc11bb6696341
Vijay Khemlani <vj@solotodo.com>
2024-03-28 12:57:04 -0300

    * Updated CosmoClima

--- storescraper/stores/cosmo_clima.py
+++ storescraper/stores/cosmo_clima.py
@@ -13,8 +13,11 @@ class CosmoClima(StoreWithUrlExtensions):
     # ONLY CONSIDERS LG PRODUCTS
 
     url_extensions = [
-        ['aire-acondicionado/aire-acondicionado-residencial/'
-         'split-muro?filter[cfv][42869][]=117715', AIR_CONDITIONER],
+        [
+            "aire-acondicionado/aire-acondicionado-residencial/split-muro-multisplit-anwo-clark-hisense-samsung-"
+            "kendal-lg-airolite-splendid-fujitsu-9000-12000-18000-24000-btu?filter[cfv][42869][]=117715",
+            AIR_CONDITIONER,
+        ],
     ]
 
     @classmethod
@@ -25,20 +28,20 @@ class CosmoClima(StoreWithUrlExtensions):
 
         while True:
             if page > 10:
-                raise Exception('Page overflow')
+                raise Exception("Page overflow")
 
-            url = 'https://www.cosmoclima.cl/{}&page={}'.format(url_extension, page)
+            url = "https://www.cosmoclima.cl/{}&page={}".format(url_extension, page)
             print(url)
             response = session.get(url)
 
-            soup = BeautifulSoup(response.text, 'html.parser')
-            products = soup.findAll('div', 'product-block')
+            soup = BeautifulSoup(response.text, "html.parser")
+            products = soup.findAll("div", "product-block")
 
             if not products:
                 break
 
             for product in products:
-                product_url = 'https://www.cosmoclima.cl' + product.find('a')['href']
+                product_url = "https://www.cosmoclima.cl" + product.find("a")["href"]
                 product_urls.append(product_url)
 
             page += 1
@@ -49,31 +52,30 @@ class CosmoClima(StoreWithUrlExtensions):
     def products_for_url(cls, url, category=None, extra_args=None):
         print(url)
         session = session_with_proxy(extra_args)
-        soup = BeautifulSoup(session.get(url).text, 'html.parser')
+        soup = BeautifulSoup(session.get(url).text, "html.parser")
 
-        buy_form_tag = soup.find('form', 'product-form')
+        buy_form_tag = soup.find("form", "product-form")
 
         if not buy_form_tag:
             return []
 
-        key = buy_form_tag['data-id']
-        sku_tag = soup.find('span', 'product-heading__sku')
-        sku = sku_tag.text.replace('SKU:', '').strip()
-        product_data = \
-            json.loads(soup.find(
-                'script', {'type': 'application/ld+json'}).text)
+        key = buy_form_tag["data-id"]
+        sku_tag = soup.find("span", "product-heading__sku")
+        sku = sku_tag.text.replace("SKU:", "").strip()
+        product_data = json.loads(
+            soup.find("script", {"type": "application/ld+json"}).text
+        )
 
-        name = product_data['name']
-        if product_data['offers']['availability'] == \
-                'http://schema.org/InStock':
+        name = product_data["name"]
+        if product_data["offers"]["availability"] == "http://schema.org/InStock":
             stock = -1
         else:
             stock = 0
 
-        price = Decimal(product_data['offers']['price'].replace('.', ''))
-        picture_urls = [product_data['image']]
+        price = Decimal(product_data["offers"]["price"].replace(".", ""))
+        picture_urls = [product_data["image"]]
 
-        description = html_to_markdown(str(soup.find('ul', 'product-accordion__list')))
+        description = html_to_markdown(str(soup.find("ul", "product-accordion__list")))
 
         p = Product(
             name,
@@ -85,11 +87,11 @@ class CosmoClima(StoreWithUrlExtensions):
             stock,
             price,
             price,
-            'CLP',
+            "CLP",
             sku=sku,
             part_number=sku,
             description=description,
-            picture_urls=picture_urls
+            picture_urls=picture_urls,
         )
 
         return [p]
