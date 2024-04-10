e049f0582fef32e69495223f1cb1bbd41870c5a0
Vijay Khemlani <vj@solotodo.com>
2024-04-10 09:30:02 -0400

    * Updated Oportutek

--- storescraper/stores/oportutek.py
+++ storescraper/stores/oportutek.py
@@ -2,10 +2,26 @@ from decimal import Decimal
 import json
 import logging
 from bs4 import BeautifulSoup
-from storescraper.categories import ALL_IN_ONE, COMPUTER_CASE, \
-    EXTERNAL_STORAGE_DRIVE, GAMING_CHAIR, HEADPHONES, KEYBOARD, \
-    KEYBOARD_MOUSE_COMBO, MONITOR, MOTHERBOARD, MOUSE, NOTEBOOK, \
-    POWER_SUPPLY, PRINTER, PROCESSOR, RAM, SOLID_STATE_DRIVE, UPS, VIDEO_CARD
+from storescraper.categories import (
+    ALL_IN_ONE,
+    COMPUTER_CASE,
+    EXTERNAL_STORAGE_DRIVE,
+    GAMING_CHAIR,
+    HEADPHONES,
+    KEYBOARD,
+    KEYBOARD_MOUSE_COMBO,
+    MONITOR,
+    MOTHERBOARD,
+    MOUSE,
+    NOTEBOOK,
+    POWER_SUPPLY,
+    PRINTER,
+    PROCESSOR,
+    RAM,
+    SOLID_STATE_DRIVE,
+    UPS,
+    VIDEO_CARD,
+)
 from storescraper.product import Product
 from storescraper.store import Store
 from storescraper.utils import session_with_proxy
@@ -32,37 +48,38 @@ class Oportutek(Store):
             ALL_IN_ONE,
             MONITOR,
             UPS,
-            PRINTER
+            PRINTER,
         ]
 
     @classmethod
     def discover_urls_for_category(cls, category, extra_args=None):
         url_extensions = [
-            ['notebooks', NOTEBOOK],
-            ['gamer/Gabinetes', COMPUTER_CASE],
-            ['gamer/Sillas-Gamer', GAMING_CHAIR],
-            ['gamer/Motherboard', MOTHERBOARD],
-            ['gamer/Memoria-RAM', RAM],
-            ['gamer/Placa-de-Video', VIDEO_CARD],
-            ['gamer/Fuentes-de-Poder', POWER_SUPPLY],
-            ['computacion/Interno', SOLID_STATE_DRIVE],
-            ['computacion/Externo', EXTERNAL_STORAGE_DRIVE],
-            ['computacion/Procesador-AMD', PROCESSOR],
-            ['computacion/Procesador-Intel', PROCESSOR],
-            ['perifericos/Mouse', MOUSE],
-            ['computacion/Teclado', KEYBOARD],
-            ['perifericos/Mouse-y-Teclado', KEYBOARD_MOUSE_COMBO],
-            ['computacion/Auriculares', HEADPHONES],
-            ['computacion/All-in-One', ALL_IN_ONE],
-            ['monitores', MONITOR],
-            ['ups-estructura-energia', UPS],
-            ['impresoras-y-accesorios', PRINTER],
+            ["notebooks", NOTEBOOK],
+            ["gamer/Gabinetes", COMPUTER_CASE],
+            ["gamer/Sillas-Gamer", GAMING_CHAIR],
+            ["gamer/Motherboard", MOTHERBOARD],
+            ["gamer/Memoria-RAM", RAM],
+            ["gamer/Placa-de-Video", VIDEO_CARD],
+            ["gamer/Fuentes-de-Poder", POWER_SUPPLY],
+            ["computacion/Interno", SOLID_STATE_DRIVE],
+            ["computacion/Externo", EXTERNAL_STORAGE_DRIVE],
+            ["computacion/Procesador-AMD", PROCESSOR],
+            ["computacion/Procesador-Intel", PROCESSOR],
+            ["perifericos/Mouse", MOUSE],
+            ["computacion/Teclado", KEYBOARD],
+            ["perifericos/Mouse-y-Teclado", KEYBOARD_MOUSE_COMBO],
+            ["computacion/Auriculares", HEADPHONES],
+            ["computacion/All-in-One", ALL_IN_ONE],
+            ["monitores", MONITOR],
+            ["ups-estructura-energia", UPS],
+            ["impresoras-y-accesorios", PRINTER],
         ]
 
         session = session_with_proxy(extra_args)
-        session.headers['user-agent'] = \
-            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 ' \
-            '(KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
+        session.headers["user-agent"] = (
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 "
+            "(KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"
+        )
         product_urls = []
         for url_extension, local_category in url_extensions:
             if local_category != category:
@@ -70,21 +87,22 @@ class Oportutek(Store):
             page = 1
             while True:
                 if page > 10:
-                    raise Exception('Page overflow: ' + url_extension)
-                url_webpage = 'https://www.oportutek.cl/collections/{}/' \
-                              '?page={}'.format(url_extension, page)
+                    raise Exception("Page overflow: " + url_extension)
+                url_webpage = (
+                    "https://www.oportutek.cl/collections/{}/"
+                    "?page={}".format(url_extension, page)
+                )
                 print(url_webpage)
                 data = session.get(url_webpage).text
-                soup = BeautifulSoup(data, 'html.parser')
-                product_containers = soup.findAll('div', 'devita-product-2')
+                soup = BeautifulSoup(data, "html.parser")
+                product_containers = soup.findAll("div", "devita-product-2")
                 if not product_containers:
                     if page == 1:
-                        logging.warning('Empty category: ' + url_extension)
+                        logging.warning("Empty category: " + url_extension)
                     break
                 for container in product_containers:
-                    product_url = container.find('a')['href']
-                    product_urls.append(
-                        'https://www.oportutek.cl' + product_url)
+                    product_url = container.find("a")["href"]
+                    product_urls.append("https://www.oportutek.cl" + product_url)
                 page += 1
         return product_urls
 
@@ -92,34 +110,39 @@ class Oportutek(Store):
     def products_for_url(cls, url, category=None, extra_args=None):
         print(url)
         session = session_with_proxy(extra_args)
-        session.headers['user-agent'] = \
-            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 ' \
-            '(KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
+        session.headers["user-agent"] = (
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 "
+            "(KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"
+        )
         response = session.get(url)
-        soup = BeautifulSoup(response.text, 'html.parser')
+        soup = BeautifulSoup(response.text, "html.parser")
 
-        json_data = json.loads(soup.findAll(
-            'script', {'type': 'application/ld+json'})[1].text)
+        json_data = json.loads(
+            soup.findAll("script", {"type": "application/ld+json"})[1].text
+        )
 
-        key = soup.find('input', {'name': 'product-id'})['value']
-        name = json_data['name']
-        sku = json_data['sku']
-        description = json_data['description']
-        price = Decimal(json_data['offers'][0]['price'])
+        key = soup.find("input", {"name": "product-id"})["value"]
+        name = json_data["name"]
+        sku = json_data["sku"]
+        description = json_data["description"]
+        price = Decimal(json_data["offers"][0]["price"])
 
-        stock_span = soup.find('span', {'id': 'variant-inventory'})
-        if 'SIN STOCK' in stock_span.text.upper():
+        stock_span = soup.find("span", {"id": "variant-inventory"})
+        if "SIN STOCK" in stock_span.text.upper():
             stock = 0
         else:
-            stock = int(stock_span.text.split(' ')[0])
+            stock = int(stock_span.text.split(" ")[0])
+
+        if not stock and not price:
+            return []
 
         picture_urls = []
-        picture_container = soup.find('div', {'id': 'ProductThumbs'})
+        picture_container = soup.find("div", {"id": "ProductThumbs"})
         if picture_container:
-            for a in picture_container.findAll('a'):
-                picture_urls.append('https:' + a['data-image'])
+            for a in picture_container.findAll("a"):
+                picture_urls.append("https:" + a["data-image"])
         else:
-            picture_urls = [json_data['image']]
+            picture_urls = [json_data["image"]]
 
         p = Product(
             name,
@@ -131,10 +154,10 @@ class Oportutek(Store):
             stock,
             price,
             price,
-            'CLP',
+            "CLP",
             sku=sku,
             part_number=sku,
             picture_urls=picture_urls,
-            description=description
+            description=description,
         )
         return [p]
