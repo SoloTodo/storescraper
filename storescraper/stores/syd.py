4bc2a5b445e2f51763513015a385f7b51c17a10e
Vijay Khemlani <vj@solotodo.com>
2024-04-13 19:30:05 -0400

    * Updated Syd

--- storescraper/stores/syd.py
+++ storescraper/stores/syd.py
@@ -4,8 +4,14 @@ import pyjson5
 from bs4 import BeautifulSoup
 from decimal import Decimal
 
-from storescraper.categories import ALL_IN_ONE, NOTEBOOK, MONITOR, \
-    EXTERNAL_STORAGE_DRIVE, MOUSE, HEADPHONES, RAM
+from storescraper.categories import (
+    NOTEBOOK,
+    MONITOR,
+    EXTERNAL_STORAGE_DRIVE,
+    MOUSE,
+    HEADPHONES,
+    RAM,
+)
 from storescraper.product import Product
 from storescraper.store_with_url_extensions import StoreWithUrlExtensions
 from storescraper.utils import session_with_proxy
@@ -13,45 +19,47 @@ from storescraper.utils import session_with_proxy
 
 class Syd(StoreWithUrlExtensions):
     url_extensions = [
-        ['imac-m1', ALL_IN_ONE],
-        ['macbook-pro-16-m1-de-apple', NOTEBOOK],
-        ['macbook-air-m1', NOTEBOOK],
-        ['studio-display', MONITOR],
-        ['macbook-pro-13-m2-nuevo', NOTEBOOK],
-        ['usb-c', EXTERNAL_STORAGE_DRIVE],
-        ['thunderbolt-3', EXTERNAL_STORAGE_DRIVE],
-        ['ssd', EXTERNAL_STORAGE_DRIVE],
-        ['raid', EXTERNAL_STORAGE_DRIVE],
-        ['mouse-y-teclados', MOUSE],
-        ['audio', HEADPHONES],
-        ['memorias', RAM],
+        ["macbook-pro-16-m1-de-apple", NOTEBOOK],
+        ["macbook-pro-16-m2-de-apple", NOTEBOOK],
+        ["macbook-air-m1", NOTEBOOK],
+        ["macbook-pro-14-m2-pro", NOTEBOOK],
+        ["studio-display", MONITOR],
+        ["macbook-pro-13-m2-nuevo", NOTEBOOK],
+        ["usb-c", EXTERNAL_STORAGE_DRIVE],
+        ["thunderbolt-3", EXTERNAL_STORAGE_DRIVE],
+        ["ssd", EXTERNAL_STORAGE_DRIVE],
+        ["raid", EXTERNAL_STORAGE_DRIVE],
+        ["mouse-y-teclados", MOUSE],
+        ["audio", HEADPHONES],
+        ["memorias", RAM],
     ]
 
     @classmethod
     def discover_urls_for_url_extension(cls, url_extension, extra_args):
-        url_base = 'https://syd.cl'
+        url_base = "https://syd.cl"
         product_urls = []
         session = session_with_proxy(extra_args)
-        session.headers['user-agent'] = \
-            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 ' \
-            '(KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'
+        session.headers["user-agent"] = (
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 "
+            "(KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"
+        )
 
-        category_url = url_base + '/collection/' + url_extension
+        category_url = url_base + "/collection/" + url_extension
 
         response = session.get(category_url)
 
         if response.url != category_url:
-            raise Exception('Invalid category: ' + category_url)
+            raise Exception("Invalid category: " + category_url)
 
-        soup = BeautifulSoup(response.text, 'html.parser')
-        titles = soup.findAll('div', 'bs-product')
+        soup = BeautifulSoup(response.text, "html.parser")
+        titles = soup.findAll("div", "bs-product")
 
         if not titles:
-            logging.warning('Empty category: ' + category_url)
+            logging.warning("Empty category: " + category_url)
 
         for title in titles:
-            product_link = title.find('a')
-            product_url = url_base + product_link['href']
+            product_link = title.find("a")
+            product_url = url_base + product_link["href"]
             product_urls.append(product_url)
 
         return product_urls
@@ -60,22 +68,23 @@ class Syd(StoreWithUrlExtensions):
     def products_for_url(cls, url, category=None, extra_args=None):
         print(url)
         session = session_with_proxy(extra_args)
-        session.headers['user-agent'] = \
-            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 ' \
-            '(KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'
+        session.headers["user-agent"] = (
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 "
+            "(KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"
+        )
 
-        soup = BeautifulSoup(session.get(url).text, 'html.parser')
-        json_data = pyjson5.decode(soup.findAll(
-            'script', {'type': 'application/ld+json'})[2].text)
-        name = json_data['name']
-        sku = json_data['sku']
-        picture_urls = json_data['image']
-        normal_price = Decimal(json_data['offers']['price'])
-        offer_price = (normal_price * Decimal('0.97')).quantize(0)
-        description = json_data['description']
+        soup = BeautifulSoup(session.get(url).text, "html.parser")
+        json_data = pyjson5.decode(
+            soup.findAll("script", {"type": "application/ld+json"})[2].text
+        )
+        name = json_data["name"]
+        sku = json_data["sku"]
+        picture_urls = json_data["image"]
+        normal_price = Decimal(json_data["offers"]["price"])
+        offer_price = (normal_price * Decimal("0.97")).quantize(0)
+        description = json_data["description"]
 
-        if json_data['offers']['availability'] == \
-                'https://schema.org/InStock':
+        if json_data["offers"]["availability"] == "https://schema.org/InStock":
             stock = -1
         else:
             stock = 0
@@ -90,11 +99,11 @@ class Syd(StoreWithUrlExtensions):
             stock,
             normal_price,
             offer_price,
-            'CLP',
+            "CLP",
             sku=sku,
             part_number=sku,
             description=description,
-            picture_urls=picture_urls
+            picture_urls=picture_urls,
         )
 
         return [p]
