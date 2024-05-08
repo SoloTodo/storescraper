780fd518438a099b0ce138a5620fe60887d27a0f
Vijay Khemlani <vj@solotodo.com>
2024-05-08 10:37:27 -0400

    * Updated Natcom

--- storescraper/stores/natcom.py
+++ storescraper/stores/natcom.py
@@ -3,10 +3,27 @@ from decimal import Decimal
 
 from bs4 import BeautifulSoup
 
-from storescraper.categories import EXTERNAL_STORAGE_DRIVE, STORAGE_DRIVE, \
-    SOLID_STATE_DRIVE, POWER_SUPPLY, COMPUTER_CASE, RAM, MONITOR, NOTEBOOK, \
-    KEYBOARD, MOUSE, HEADPHONES, MOTHERBOARD, PROCESSOR, CPU_COOLER, \
-    VIDEO_CARD, CASE_FAN, GAMING_CHAIR, USB_FLASH_DRIVE
+from storescraper.categories import (
+    EXTERNAL_STORAGE_DRIVE,
+    STORAGE_DRIVE,
+    SOLID_STATE_DRIVE,
+    POWER_SUPPLY,
+    COMPUTER_CASE,
+    RAM,
+    MONITOR,
+    NOTEBOOK,
+    KEYBOARD,
+    MOUSE,
+    HEADPHONES,
+    MOTHERBOARD,
+    PROCESSOR,
+    CPU_COOLER,
+    VIDEO_CARD,
+    CASE_FAN,
+    GAMING_CHAIR,
+    USB_FLASH_DRIVE,
+    ALL_IN_ONE,
+)
 from storescraper.product import Product
 from storescraper.store_with_url_extensions import StoreWithUrlExtensions
 from storescraper.utils import session_with_proxy, remove_words
@@ -14,29 +31,30 @@ from storescraper.utils import session_with_proxy, remove_words
 
 class Natcom(StoreWithUrlExtensions):
     url_extensions = [
-        ['disco-duro-externo', EXTERNAL_STORAGE_DRIVE],
-        ['discos-duros', STORAGE_DRIVE],
-        ['discos-ssd', SOLID_STATE_DRIVE],
-        ['certificada', POWER_SUPPLY],
-        ['no-certificada', POWER_SUPPLY],
-        ['gabinetes', COMPUTER_CASE],
-        ['ram-pc-escritorio', RAM],
-        ['ram-portatiles', RAM],
-        ['monitores', MONITOR],
-        ['notebooks', NOTEBOOK],
-        ['teclados', KEYBOARD],
-        ['mouse', MOUSE],
-        ['audifonos', HEADPHONES],
-        ['placa-madre-amd', MOTHERBOARD],
-        ['placa-madre-intel', MOTHERBOARD],
-        ['procesadores-amd', PROCESSOR],
-        ['procesadores-intel', PROCESSOR],
-        ['refrigeracion-liquida', CPU_COOLER],
-        ['ventiladores', CASE_FAN],
-        ['tarjetas-de-video-amd', VIDEO_CARD],
-        ['tarjetas-de-video-nvidia', VIDEO_CARD],
-        ['categoria-producto/sillas-gamer', GAMING_CHAIR],
-        ['pendrive', USB_FLASH_DRIVE]
+        ["disco-duro-externo", EXTERNAL_STORAGE_DRIVE],
+        ["discos-duros", STORAGE_DRIVE],
+        ["discos-ssd", SOLID_STATE_DRIVE],
+        ["certificada", POWER_SUPPLY],
+        ["no-certificada", POWER_SUPPLY],
+        ["gabinetes", COMPUTER_CASE],
+        ["ram-pc-escritorio", RAM],
+        ["ram-portatiles", RAM],
+        ["monitores", MONITOR],
+        ["notebooks", NOTEBOOK],
+        ["teclados", KEYBOARD],
+        ["mouse", MOUSE],
+        ["audifonos", HEADPHONES],
+        ["placa-madre-amd", MOTHERBOARD],
+        ["placa-madre-intel", MOTHERBOARD],
+        ["procesadores-amd", PROCESSOR],
+        ["procesadores-intel", PROCESSOR],
+        ["refrigeracion-liquida", CPU_COOLER],
+        ["ventiladores", CASE_FAN],
+        ["tarjetas-de-video-amd", VIDEO_CARD],
+        ["tarjetas-de-video-nvidia", VIDEO_CARD],
+        ["categoria-producto/sillas-gamer", GAMING_CHAIR],
+        ["pendrive", USB_FLASH_DRIVE],
+        ["all-in-one", ALL_IN_ONE],
     ]
 
     @classmethod
@@ -47,22 +65,24 @@ class Natcom(StoreWithUrlExtensions):
         done = False
         while not done:
             if page > 10:
-                raise Exception('page overflow: ' + url_extension)
-            url_webpage = 'https://natcomchile.cl/{}/page/{}/'.format(
-                url_extension, page)
+                raise Exception("page overflow: " + url_extension)
+            url_webpage = "https://natcomchile.cl/{}/page/{}/".format(
+                url_extension, page
+            )
             print(url_webpage)
             response = session.get(url_webpage, timeout=60)
-            soup = BeautifulSoup(response.text, 'html.parser')
-            product_containers = soup.findAll(
-                'article', 'w-grid-item') + soup.findAll('li', 'product')
+            soup = BeautifulSoup(response.text, "html.parser")
+            product_containers = soup.findAll("article", "w-grid-item") + soup.findAll(
+                "li", "product"
+            )
 
             if not product_containers:
                 if page == 1:
-                    logging.warning('Empty category: ' + url_extension)
+                    logging.warning("Empty category: " + url_extension)
                 break
 
             for container in product_containers:
-                product_url = container.find('a')['href']
+                product_url = container.find("a")["href"]
                 if product_url in product_urls:
                     return product_urls
                 product_urls.append(product_url)
@@ -78,31 +98,34 @@ class Natcom(StoreWithUrlExtensions):
         if response.status_code == 404:
             return []
 
-        soup = BeautifulSoup(response.text, 'html.parser')
-        name = soup.find('h1', 'product_title').text
-        sku = soup.find('link', {'rel': 'shortlink'})['href'].split('p=')[1]
-        stock_tag = soup.find('p', 'stock in-stock')
+        soup = BeautifulSoup(response.text, "html.parser")
+        name = soup.find("h1", "product_title").text
+        sku = soup.find("link", {"rel": "shortlink"})["href"].split("p=")[1]
+        stock_tag = soup.find("p", "stock in-stock")
         if stock_tag:
-            stock = int(soup.find('p', 'stock in-stock').text.split()[0])
+            stock = int(soup.find("p", "stock in-stock").text.split()[0])
         else:
             stock = -1
-        price_container = soup.find('p', 'price')
+        price_container = soup.find("p", "price")
 
         part_number_tag = soup.find(
-            'div', 'woocommerce-product-details__short-description')
+            "div", "woocommerce-product-details__short-description"
+        )
 
-        if part_number_tag and part_number_tag.find('p'):
-            part_number = part_number_tag.find('p').text.strip()[:40]
+        if part_number_tag and part_number_tag.find("p"):
+            part_number = part_number_tag.find("p").text.strip()[:40]
         else:
             part_number = None
 
-        if price_container.find('ins'):
-            price = Decimal(remove_words(price_container.find('ins').text))
+        if price_container.find("ins"):
+            price = Decimal(remove_words(price_container.find("ins").text))
         else:
             price = Decimal(remove_words(price_container.text))
 
-        picture_urls = [tag['src'] for tag in soup.find(
-            'div', 'woocommerce-product-gallery').findAll('img')]
+        picture_urls = [
+            tag["src"]
+            for tag in soup.find("div", "woocommerce-product-gallery").findAll("img")
+        ]
 
         p = Product(
             name,
@@ -114,9 +137,9 @@ class Natcom(StoreWithUrlExtensions):
             stock,
             price,
             price,
-            'CLP',
+            "CLP",
             sku=sku,
             picture_urls=picture_urls,
-            part_number=part_number
+            part_number=part_number,
         )
         return [p]
