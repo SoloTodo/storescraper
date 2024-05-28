6856d3f046c0169bfe72e63a9992c406b33f83c8
Vijay Khemlani <vj@solotodo.com>
2024-05-28 14:35:29 -0400

    * Updated Lider

--- storescraper/stores/lider.py
+++ storescraper/stores/lider.py
@@ -43,6 +43,11 @@ from storescraper import banner_sections as bs
 
 
 class Lider(Store):
+    DEFAULT_USER_AGENT = (
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/124.0.0.0 Safari/537.3"
+    )
     category_paths = [
         ("Celulares > Audífonos y Accesorios > Audífonos", HEADPHONES),
         ("Celulares > Mundo Apple > AirPods", HEADPHONES),
