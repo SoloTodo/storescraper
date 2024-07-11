18a1cf00ebb50a75e378391f5bf3608f533a3ee8
Vijay Khemlani <vj@solotodo.com>
2024-07-11 11:22:34 -0400

    * Updated AlmacenesJapon

--- storescraper/stores/almacenes_japon.py
+++ storescraper/stores/almacenes_japon.py
@@ -1,11 +1,17 @@
+import base64
+import json
+import re
 from decimal import Decimal
-
-from bs4 import BeautifulSoup
 
 from storescraper.categories import TELEVISION
 from storescraper.product import Product
 from storescraper.store import Store
-from storescraper.utils import html_to_markdown, session_with_proxy
+from storescraper.utils import (
+    html_to_markdown,
+    session_with_proxy,
+    vtex_preflight,
+    check_ean13,
+)
 
 
 class AlmacenesJapon(Store):
@@ -17,31 +23,50 @@ class AlmacenesJapon(Store):
 
     @classmethod
     def discover_urls_for_category(cls, category, extra_args=None):
-        session = session_with_proxy(extra_args)
+        if category != TELEVISION:
+            return []
         product_urls = []
-        page = 1
+        session = session_with_proxy(extra_args)
+
+        offset = 0
         while True:
-            url_webpage = 'https://almacenesjapon.com/brand/53-lg?' \
-                          'page={}'.format(page)
-            print(url_webpage)
+            if offset >= 100:
+                raise Exception("Page overflow")
 
-            if page > 20:
-                raise Exception('page overflow: ' + url_webpage)
+            variables = {
+                "from": offset,
+                "to": offset + 10,
+                "selectedFacets": [{"key": "b", "value": "lg"}],
+            }
 
-            data = session.get(url_webpage).text
-            soup = BeautifulSoup(data, 'html.parser')
-            product_containers = soup.findAll('div',
-                                              'ajax_block_product')
+            payload = {
+                "persistedQuery": {
+                    "version": 1,
+                    "sha256Hash": extra_args["sha256Hash"],
+                },
+                "variables": base64.b64encode(
+                    json.dumps(variables).encode("utf-8")
+                ).decode("utf-8"),
+            }
 
-            if not product_containers:
-                if page == 1:
-                    raise Exception('No products found: ' + url_webpage)
+            endpoint = (
+                "https://www.almacenesjapon.com/_v/segment/graphql/v1"
+                "?extensions={}".format(json.dumps(payload))
+            )
+            response = session.get(endpoint).json()
+
+            product_entries = response["data"]["productSearch"]["products"]
+
+            if not product_entries:
                 break
 
-            for product in product_containers:
-                product_url = product.find('a')['href']
+            for product_entry in product_entries:
+                product_url = "https://www.almacenesjapon.com/{}/p".format(
+                    product_entry["linkText"]
+                )
                 product_urls.append(product_url)
-            page += 1
+
+            offset += 10
 
         return product_urls
 
@@ -50,25 +75,45 @@ class AlmacenesJapon(Store):
         print(url)
         session = session_with_proxy(extra_args)
         response = session.get(url)
-        if response.status_code == 403:
+
+        if response.status_code == 404:
             return []
-        soup = BeautifulSoup(response.text, 'html.parser')
-        name = soup.find('h1', 'product-detail-name').text.strip()
-        sku = soup.find('meta', {'property': 'product:retailer_item_id'})[
-            'content']
 
-        if soup.find('link', {'itemprop': 'availability'})['href'] \
-                == 'https://schema.org/InStock':
-            stock = -1
-        else:
-            stock = 0
+        product_data = json.loads(
+            "{" + re.search(r"__STATE__ = {(.+)}", response.text).groups()[0] + "}"
+        )
 
-        price = Decimal(soup.find('h4', {'itemprop': 'price'})['content'])
+        base_json_key = list(product_data.keys())[0]
+        product_specs = product_data[base_json_key]
 
-        picture_urls = [soup.find('meta', {'property': 'og:image'})['content']]
+        item_key = "{}.items.0".format(base_json_key)
+        key = product_data[item_key]["itemId"]
+        ean = product_data[item_key]["ean"]
+        if not check_ean13(ean):
+            ean = None
 
-        description = html_to_markdown(
-            str(soup.find('div', {'id': 'full_spec'})))
+        name = product_specs["productName"]
+        sku = product_specs["productReference"]
+        description = html_to_markdown(product_specs.get("description", ""))
+
+        pricing_key = "${}.items.0.sellers.0.commertialOffer".format(base_json_key)
+        pricing_data = product_data[pricing_key]
+
+        price = Decimal(str(pricing_data["Price"])) + Decimal(str(pricing_data["Tax"]))
+
+        if not price:
+            return []
+
+        stock = pricing_data["AvailableQuantity"]
+
+        picture_list_key = "{}.items.0".format(base_json_key)
+        picture_list_node = product_data[picture_list_key]
+        picture_ids = [x["id"] for x in picture_list_node["images"]]
+
+        picture_urls = []
+        for picture_id in picture_ids:
+            picture_node = product_data[picture_id]
+            picture_urls.append(picture_node["imageUrl"].split("?")[0])
 
         p = Product(
             name,
@@ -76,14 +121,22 @@ class AlmacenesJapon(Store):
             category,
             url,
             url,
-            sku,
+            key,
             stock,
             price,
             price,
-            'USD',
+            "USD",
             sku=sku,
-            picture_urls=picture_urls,
+            ean=ean,
             description=description,
+            picture_urls=picture_urls,
         )
 
         return [p]
+
+    @classmethod
+    def preflight(cls, extra_args=None):
+        return vtex_preflight(
+            extra_args,
+            "https://www.almacenesjapon.com/tecnologia/TV-y-Video/LEDS-y-Smart-TV",
+        )
--- storescraper/stores/creditos_economicos.py
+++ storescraper/stores/creditos_economicos.py
@@ -15,9 +15,7 @@ from storescraper.categories import TELEVISION
 class CreditosEconomicos(Store):
     @classmethod
     def categories(cls):
-        return [
-            TELEVISION
-        ]
+        return [TELEVISION]
 
     @classmethod
     def discover_urls_for_category(cls, category, extra_args=None):
@@ -31,34 +29,29 @@ class CreditosEconomicos(Store):
 
         while True:
             if page > 10:
-                raise Exception('Page overflow')
+                raise Exception("Page overflow")
 
-            url = 'https://www.crecos.com/lg?_q=lg&map=ft&page=' \
-                '{}&sc=2'.format(page)
+            url = "https://www.crecos.com/lg?_q=lg&map=ft&page=" "{}&sc=2".format(page)
             print(url)
 
-            soup = BeautifulSoup(session.get(url).text, 'html5lib')
-            # import ipdb
-            # ipdb.set_trace()
+            soup = BeautifulSoup(session.get(url).text, "html5lib")
             product_containers = json.loads(
-                '{' +
-                re.search(r'__STATE__ = {(.+)}', soup.text).groups()[0] +
-                '}')
+                "{" + re.search(r"__STATE__ = {(.+)}", soup.text).groups()[0] + "}"
+            )
 
-            r = re.compile(r'Product:sp-(\d+$)')
+            r = re.compile(r"Product:sp-(\d+$)")
 
             product_container_keys = product_containers.keys()
             products_to_find = list(filter(r.match, product_container_keys))
             if not products_to_find:
                 if page == 1:
-                    logging.warning('Empty category: ' + category)
+                    logging.warning("Empty category: " + category)
                 else:
                     break
 
             for product_key in products_to_find:
-                product_url = product_containers[product_key]['link']
-                product_urls.append(
-                    'https://www.crecos.com' + product_url)
+                product_url = product_containers[product_key]["link"]
+                product_urls.append("https://www.crecos.com" + product_url)
 
             page += 1
 
@@ -68,22 +61,25 @@ class CreditosEconomicos(Store):
     def products_for_url(cls, url, category=None, extra_args=None):
         print(url)
         session = session_with_proxy(extra_args)
-        session.cookies['vtex_segment'] = 'eyJjYW1wYWlnbnMiOm51bGwsImNoYW5uZ' \
-            'WwiOiIyIiwicHJpY2VUYWJsZXMiOm51bGwsInJlZ2lvbklkIjpudWxsLCJ1dG1f' \
-            'Y2FtcGFpZ24iOm51bGwsInV0bV9zb3VyY2UiOm51bGwsInV0bWlfY2FtcGFpZ24' \
-            'iOm51bGwsImN1cnJlbmN5Q29kZSI6IlVTRCIsImN1cnJlbmN5U3ltYm9sIjoiJC' \
-            'IsImNvdW50cnlDb2RlIjoiRUNVIiwiY3VsdHVyZUluZm8iOiJlcy1FQyIsImFkb' \
-            'WluX2N1bHR1cmVJbmZvIjoiZXMtRUMiLCJjaGFubmVsUHJpdmFjeSI6InB1Ymxp' \
-            'YyJ9'
-        new_url = '{}?sc=2'.format(url)
+        session.cookies["vtex_segment"] = (
+            "eyJjYW1wYWlnbnMiOm51bGwsImNoYW5uZ"
+            "WwiOiIyIiwicHJpY2VUYWJsZXMiOm51bGwsInJlZ2lvbklkIjpudWxsLCJ1dG1f"
+            "Y2FtcGFpZ24iOm51bGwsInV0bV9zb3VyY2UiOm51bGwsInV0bWlfY2FtcGFpZ24"
+            "iOm51bGwsImN1cnJlbmN5Q29kZSI6IlVTRCIsImN1cnJlbmN5U3ltYm9sIjoiJC"
+            "IsImNvdW50cnlDb2RlIjoiRUNVIiwiY3VsdHVyZUluZm8iOiJlcy1FQyIsImFkb"
+            "WluX2N1bHR1cmVJbmZvIjoiZXMtRUMiLCJjaGFubmVsUHJpdmFjeSI6InB1Ymxp"
+            "YyJ9"
+        )
+        new_url = "{}?sc=2".format(url)
         response = session.get(new_url)
 
         if response.status_code != 200:
             return []
 
-        soup = BeautifulSoup(response.text, 'html5lib')
-        product_data = json.loads('{' + re.search(r'__STATE__ = {(.+)}',
-                                                  soup.text).groups()[0] + '}')
+        soup = BeautifulSoup(response.text, "html5lib")
+        product_data = json.loads(
+            "{" + re.search(r"__STATE__ = {(.+)}", soup.text).groups()[0] + "}"
+        )
 
         base_json_keys = list(product_data.keys())
 
@@ -92,31 +88,29 @@ class CreditosEconomicos(Store):
 
         base_json_key = base_json_keys[0]
         product_specs = product_data[base_json_key]
-        slug = product_specs['linkText']
+        slug = product_specs["linkText"]
 
         # key = product_specs['productId']
-        key_key = '{}.items.0'.format(
-            base_json_key)
-        key = product_data[key_key]['itemId']
+        key_key = "{}.items.0".format(base_json_key)
+        key = product_data[key_key]["itemId"]
 
-        name = product_specs['productName']
-        sku = product_specs['productReference']
-        description = html_to_markdown(product_specs.get('description', None))
+        name = product_specs["productName"]
+        sku = product_specs["productReference"]
+        description = html_to_markdown(product_specs.get("description", None))
 
-        pricing_key = '${}.items.0.sellers.0.commertialOffer'.format(
-            base_json_key)
+        pricing_key = "${}.items.0.sellers.0.commertialOffer".format(base_json_key)
         pricing_data = product_data[pricing_key]
-        price = Decimal(str(pricing_data['Price']))
-        stock = pricing_data['AvailableQuantity']
+        price = Decimal(str(pricing_data["Price"]))
+        stock = pricing_data["AvailableQuantity"]
 
-        picture_list_key = '{}.items.0'.format(base_json_key)
+        picture_list_key = "{}.items.0".format(base_json_key)
         picture_list_node = product_data[picture_list_key]
-        picture_ids = [x['id'] for x in picture_list_node['images']]
+        picture_ids = [x["id"] for x in picture_list_node["images"]]
 
         picture_urls = []
         for picture_id in picture_ids:
             picture_node = product_data[picture_id]
-            picture_urls.append(picture_node['imageUrl'].split('?')[0])
+            picture_urls.append(picture_node["imageUrl"].split("?")[0])
 
         p = Product(
             name,
@@ -128,7 +122,7 @@ class CreditosEconomicos(Store):
             stock,
             price,
             price,
-            'USD',
+            "USD",
             sku=sku,
             picture_urls=picture_urls,
             description=description,
