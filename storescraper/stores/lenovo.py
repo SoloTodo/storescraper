43c3fc11868e3ca7a24f2abcee93b8b601ab35ac
Vijay Khemlani <vj@solotodo.com>
2024-07-15 15:40:56 -0400

    * Updated Lenovo

--- storescraper/stores/lenovo.py
+++ storescraper/stores/lenovo.py
@@ -9,30 +9,23 @@ from urllib.parse import quote
 
 from storescraper.categories import NOTEBOOK, TABLET, ALL_IN_ONE
 from storescraper.product import Product
-from storescraper.store import Store
+from storescraper.store_with_url_extensions import StoreWithUrlExtensions
 from storescraper.utils import session_with_proxy
 
 
-class Lenovo(Store):
+class Lenovo(StoreWithUrlExtensions):
     base_domain = "https://www.lenovo.com"
     currency = "USD"
     region_extension = ""
 
-    @classmethod
-    def categories(cls):
-        return [
-            NOTEBOOK,
-            TABLET,
-            ALL_IN_ONE,
-        ]
+    url_extensions = [
+        ("7ca29953-e3fd-48a9-8c04-b790cfef3842", NOTEBOOK),
+        ("5efac680-d533-4fba-ad6d-28311eca5544", TABLET),
+        ("738528ce-a63a-4853-9d21-ddda6bb57b14", ALL_IN_ONE),
+    ]
 
     @classmethod
-    def discover_urls_for_category(cls, category, extra_args=None):
-        url_extensions = [
-            ("7ca29953-e3fd-48a9-8c04-b790cfef3842", NOTEBOOK),
-            # ("5efac680-d533-4fba-ad6d-28311eca5544", TABLET),
-            ("738528ce-a63a-4853-9d21-ddda6bb57b14", ALL_IN_ONE),
-        ]
+    def discover_urls_for_url_extension(cls, url_extension, extra_args=None):
         product_urls = []
         session = session_with_proxy(extra_args)
         session.headers["Referer"] = "https://www.lenovo.com/"
@@ -40,44 +33,40 @@ class Lenovo(Store):
             "User-Agent"
         ] = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
 
-        for category_id, local_category in url_extensions:
-            if local_category != category:
-                continue
+        page = 1
+        while True:
+            payload = {
+                "pageFilterId": url_extension,
+                "pageSize": 100,
+                "version": "v2",
+                "page": page,
+            }
 
-            page = 1
-            while True:
-                payload = {
-                    "pageFilterId": category_id,
-                    "pageSize": 100,
-                    "version": "v2",
-                    "page": page,
-                }
+            payload_str = json.dumps(payload)
+            encoded_payload = quote(payload_str)
+            endpoint = "https://openapi.lenovo.com/cl/es/ofp/search/dlp/product/query/get/_tsc?subSeriesCode=&loyalty=false&params={}".format(
+                encoded_payload
+            )
+            res = session.get(endpoint)
+            products_data = res.json()
+            product_entries = products_data["data"]["data"][0]["products"]
 
-                payload_str = json.dumps(payload)
-                encoded_payload = quote(payload_str)
-                endpoint = "https://openapi.lenovo.com/cl/es/ofp/search/dlp/product/query/get/_tsc?subSeriesCode=&loyalty=false&params={}".format(
-                    encoded_payload
+            if not product_entries:
+                if page == 1:
+                    raise Exception("Empty category: " + url_extension)
+                break
+
+            for entry in product_entries:
+                subseries_code = entry.get("subseriesCode", None)
+                if not subseries_code:
+                    # print(entry["summary"])
+                    continue
+                product_url = "https://www.lenovo.com{}/p/{}".format(
+                    cls.region_extension, subseries_code
                 )
-                res = session.get(endpoint)
-                products_data = res.json()
-                product_entries = products_data["data"]["data"][0]["products"]
+                product_urls.append(product_url)
 
-                if not product_entries:
-                    if page == 1:
-                        raise Exception("Empty category: " + category_id)
-                    break
-
-                for entry in product_entries:
-                    subseries_code = entry.get("subseriesCode", None)
-                    if not subseries_code:
-                        # print(entry["summary"])
-                        continue
-                    product_url = "https://www.lenovo.com{}/p/{}".format(
-                        cls.region_extension, subseries_code
-                    )
-                    product_urls.append(product_url)
-
-                page += 1
+            page += 1
 
         return product_urls
 
