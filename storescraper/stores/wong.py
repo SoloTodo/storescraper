4895461398419ce62c640009bdba56a40900cbb6
Tomás Díaz Toro <tomasdiaztoro@gmail.com>
2024-08-20 10:54:36 -0400

    Update Wong

--- storescraper/stores/wong.py
+++ storescraper/stores/wong.py
@@ -74,6 +74,10 @@ class Wong(Store):
         print(url)
         session = session_with_proxy(extra_args)
         response = session.get(url)
+
+        if response.status_code == 404:
+            return []
+
         soup = BeautifulSoup(response.text, "lxml")
 
         product_data = json.loads(
