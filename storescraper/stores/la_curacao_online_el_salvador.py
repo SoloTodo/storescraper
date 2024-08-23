295c953a3723f49d19a045e2463ce335f8fe126a
Tomás Díaz Toro <tomasdiaztoro@gmail.com>
2024-08-23 10:03:42 -0400

    Update LaCuracaoOnline

--- storescraper/stores/la_curacao_online_el_salvador.py
+++ storescraper/stores/la_curacao_online_el_salvador.py
@@ -2,6 +2,10 @@ from storescraper.stores.la_curacao_online import LaCuracaoOnline
 
 
 class LaCuracaoOnlineElSalvador(LaCuracaoOnline):
-    country = 'elsalvador'
-    currency = '$'
-    currency_iso = 'USD'
+    country = "elsalvador"
+    currency = "$"
+    currency_iso = "USD"
+
+    @classmethod
+    def format_url(cls, page):
+        return f"https://www.lacuracaonline.com/{cls.country}/search/lg?p={page}"
