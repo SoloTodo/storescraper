663e862bad72138b54d4882b5399b2e282f5b0d2
Tomás Díaz Toro <tomasdiaztoro@gmail.com>
2024-08-26 10:22:39 -0400

    Update LaCuracaoOnlineGuatemala

--- storescraper/stores/la_curacao_online_guatemala.py
+++ storescraper/stores/la_curacao_online_guatemala.py
@@ -2,6 +2,10 @@ from storescraper.stores.la_curacao_online import LaCuracaoOnline
 
 
 class LaCuracaoOnlineGuatemala(LaCuracaoOnline):
-    country = 'guatemala'
-    currency = 'Q'
-    currency_iso = 'GTQ'
+    country = "guatemala"
+    currency = "Q"
+    currency_iso = "GTQ"
+
+    @classmethod
+    def format_url(cls, page):
+        return f"https://www.lacuracaonline.com/{cls.country}/search/lg?p={page}"
