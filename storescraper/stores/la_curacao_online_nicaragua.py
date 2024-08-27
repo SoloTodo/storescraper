789b7b659d6fd8d41aa6f47e4104e210efb63aca
Tomás Díaz Toro <tomasdiaztoro@gmail.com>
2024-08-27 10:15:40 -0400

    Update LaCuracaoOnlineNicaragua

--- storescraper/stores/la_curacao_online_nicaragua.py
+++ storescraper/stores/la_curacao_online_nicaragua.py
@@ -2,6 +2,10 @@ from storescraper.stores.la_curacao_online import LaCuracaoOnline
 
 
 class LaCuracaoOnlineNicaragua(LaCuracaoOnline):
-    country = 'nicaragua'
-    currency = 'C$'
-    currency_iso = 'NIO'
+    country = "nicaragua"
+    currency = "C$"
+    currency_iso = "NIO"
+
+    @classmethod
+    def format_url(cls, page):
+        return f"https://www.lacuracaonline.com/{cls.country}/search/lg?p={page}"
