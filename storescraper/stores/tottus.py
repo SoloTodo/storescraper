e2d3a7f1eb0bf6127a822401165084695142a56d
Tomás Díaz Toro <tomasdiaztoro@gmail.com>
2024-09-05 11:55:20 -0400

    Update Tottus

--- storescraper/stores/tottus.py
+++ storescraper/stores/tottus.py
@@ -1,4 +1,7 @@
+from bs4 import BeautifulSoup
+
 from storescraper import banner_sections as bs
+from storescraper.utils import session_with_proxy
 from .falabella import Falabella
 
 
@@ -6,10 +9,7 @@ class Tottus(Falabella):
     store_and_subdomain = "tottus"
     seller = [{"id": "TOTTUS", "section_prefix": None, "include_in_fast_mode": True}]
     seller_blacklist = []
-    banners_base_url = "https://tottus.falabella.com/tottus-cl/{}"
-    banners_sections_data = [
-        [bs.HOME, "Home", bs.SUBSECTION_TYPE_HOME, ""],
-    ]
+    banners_base_url = "https://tottus.cl/"
 
     @classmethod
     def products_for_url(cls, url, category=None, extra_args=None):
@@ -23,3 +23,30 @@ class Tottus(Falabella):
             product.seller = None
 
         return products
+
+    @classmethod
+    def banners(cls, extra_args=None):
+        session = session_with_proxy(extra_args)
+        banners = []
+
+        soup = BeautifulSoup(session.get(cls.banners_base_url).text, "lxml")
+        slider_container = soup.find("div", "slideshow-container-desk")
+        slider_tags = slider_container.findAll("a")
+
+        for index, slider_tag in enumerate(slider_tags):
+            destination_urls = [slider_tag["href"]]
+            picture_url = slider_tag.find("img")["src"]
+
+            banners.append(
+                {
+                    "url": cls.banners_base_url,
+                    "picture_url": picture_url,
+                    "destination_urls": destination_urls,
+                    "key": picture_url,
+                    "position": index + 1,
+                    "section": bs.HOME,
+                    "subsection": bs.HOME,
+                    "type": bs.SUBSECTION_TYPE_HOME,
+                }
+            )
+        return banners
