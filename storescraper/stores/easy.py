6eb529e74d0a268630e7877738f0f7769c74c000
Tomás Díaz Toro <tomasdiaztoro@gmail.com>
2024-09-11 15:26:03 -0300

    Update easy

--- storescraper/stores/easy.py
+++ storescraper/stores/easy.py
@@ -221,47 +221,25 @@ class Easy(Store):
             if category not in local_categories:
                 continue
 
-            page = 0
+            page = 1
+
             while True:
                 if page > 20:
                     raise Exception("page overflow: " + category_path)
 
-                facets = [{"key": "c", "value": x} for x in category_path.split("/")]
+                url = f"https://cl-ccom-easy-bff-web.ecomm.cencosud.com/v2/search/categories?count=40&page={page}&categories={category_path}"
 
-                variables = {
-                    "from": page * 40,
-                    "to": (page + 1) * 40 - 1,
-                    "selectedFacets": facets,
-                }
+                response = session.get(
+                    url, headers={"X-Api-Key": "jFt3XhoLqFAGr6qN9SCpr9K6y83HpakP"}
+                ).json()
 
-                payload = {
-                    "persistedQuery": {
-                        "version": 1,
-                        "sha256Hash": extra_args["sha256Hash"],
-                        "sender": "vtex.store-resources@0.x",
-                        "provider": "vtex.search-graphql@0.x",
-                    },
-                    "variables": base64.b64encode(
-                        json.dumps(variables).encode("utf-8")
-                    ).decode("utf-8"),
-                }
-
-                endpoint = (
-                    "https://www.easy.cl/_v/segment/graphql/v1?"
-                    "extensions={}".format(json.dumps(payload))
-                )
-                response = session.get(endpoint)
-
-                products_data = response.json()["data"]["productSearch"]["products"]
-
-                if not products_data:
+                if response["productList"] == []:
                     break
 
-                for idx, product_data in enumerate(products_data):
-                    product_url = "https://www.easy.cl/{}/p".format(
-                        product_data["linkText"]
-                    )
-                    product_entries[product_url].append(
+                for idx, product in enumerate(response["productList"]):
+                    product_entries[
+                        f"https://www.easy.cl/{product['linkText']}"
+                    ].append(
                         {
                             "category_weight": category_weight,
                             "section_name": section_name,
@@ -367,53 +345,32 @@ class Easy(Store):
     def products_for_url(cls, url, category=None, extra_args=None):
         print(url)
         session = session_with_proxy(extra_args)
-        res = session.get(url)
-        soup = BeautifulSoup(res.text, "html.parser")
+        sku = url.split("-")[-1]
+        response = session.get(
+            f"https://cl-ccom-easy-bff-web.ecomm.cencosud.com/v2/products/by-sku/{sku}",
+            headers={"X-Api-Key": "jFt3XhoLqFAGr6qN9SCpr9K6y83HpakP"},
+        ).json()
+        assert len(response["items"]) == 1
 
-        product_data = json.loads(
-            str(
-                soup.find("template", {"data-varname": "__STATE__"})
-                .find("script")
-                .contents[0]
-            )
-        )
+        item = response["items"][0]
+        name = response["productName"]
+        key = response["productId"]
+        picture_urls = [img["imageUrl"] for img in item["images"]]
 
-        base_json_keys = list(product_data.keys())
+        for seller in item["sellers"]:
+            if seller["sellerName"] == "Easy.cl":
+                offer = seller["commertialOffer"]
+                normal_price = Decimal(offer["prices"]["normalPrice"])
 
-        if not base_json_keys:
-            return []
+                if "offerPrice" in offer["prices"] and offer["prices"]["offerPrice"]:
+                    offer_price = Decimal(offer["prices"]["offerPrice"])
+                else:
+                    offer_price = normal_price
 
-        base_json_key = base_json_keys[0]
-        product_specs = product_data[base_json_key]
+                stock = offer["availableQuantity"]
+                break
 
-        key = product_specs["productId"]
-        name = product_specs["productName"]
-        sku = product_specs["productReference"]
-        description = html_to_markdown(product_specs.get("description", None))
-
-        pricing_key = "${}.items.0.sellers.0.commertialOffer".format(base_json_key)
-        pricing_data = product_data[pricing_key]
-
-        normal_price = Decimal(pricing_data["Price"])
-
-        offer_price_key = "{}.teasers.0.effects.parameters.0".format(pricing_key)
-        offer_price_json_value = product_data.get(offer_price_key, None)
-        if offer_price_json_value:
-            offer_price = Decimal(offer_price_json_value["value"]) / Decimal(100)
-            if offer_price > normal_price:
-                offer_price = normal_price
-        else:
-            offer_price = normal_price
-
-        stock = pricing_data["AvailableQuantity"]
-        picture_list_key = "{}.items.0".format(base_json_key)
-        picture_list_node = product_data[picture_list_key]
-        picture_ids = [x["id"] for x in picture_list_node["images"]]
-
-        picture_urls = []
-        for picture_id in picture_ids:
-            picture_node = product_data[picture_id]
-            picture_urls.append(picture_node["imageUrl"].split("?")[0])
+        description = html_to_markdown(response["description"])
 
         p = Product(
             name,
@@ -432,10 +389,3 @@ class Easy(Store):
         )
 
         return [p]
-
-    @classmethod
-    def preflight(cls, extra_args=None):
-        return vtex_preflight(
-            extra_args,
-            "https://www.easy.cl/electrohogar-y-climatizacion/" "refrigeracion",
-        )
