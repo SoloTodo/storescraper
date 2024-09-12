2b38c99a5e77889a8262a9eebf5eb292ce342b1d
Tomás Díaz Toro <tomasdiaztoro@gmail.com>
2024-09-12 10:11:45 -0300

    Update Max

--- storescraper/stores/max.py
+++ storescraper/stores/max.py
@@ -55,6 +55,10 @@ class Max(StoreWithUrlExtensions):
         print(url)
         session = session_with_proxy(extra_args)
         response = session.get(url)
+
+        if response.status_code == 404:
+            return []
+
         soup = BeautifulSoup(response.text, "lxml")
         product = json.loads(soup.find("script", {"id": "__NEXT_DATA__"}).text)[
             "props"
