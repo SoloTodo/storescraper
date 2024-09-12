b1ca95142875e884428d704010654aa8bdd49106
Tomás Díaz Toro <tomasdiaztoro@gmail.com>
2024-09-12 11:23:50 -0300

    Update Electrojaponesa

--- storescraper/stores/electrojaponesa.py
+++ storescraper/stores/electrojaponesa.py
@@ -22,57 +22,67 @@ class Electrojaponesa(Store):
         session = session_with_proxy(extra_args)
         product_urls = []
         page = 1
+
         while True:
             if page > 10:
                 raise Exception("Page overflow")
-            url = (
-                "https://www.electrojaponesa.com/buscapagina?fq=B%3a2000013&PS=12&"
-                "sl=dd7e433e-5e08-4153-9a84-f0efc19710d9&cc=3&sm=0&PageNumber={}"
-            ).format(page)
+
+            url = f"https://www.electrojaponesa.com/lg?page={page}"
             print(url)
             res = session.get(url)
             soup = BeautifulSoup(res.text, "lxml")
+            product_containers = soup.findAll(
+                "section", "vtex-product-summary-2-x-container"
+            )
 
-            product_containers = soup.findAll("figure", "vitrina")
             if not product_containers:
                 break
 
             for product in product_containers:
                 product_url = product.find("a")["href"]
-                product_urls.append(product_url)
+                product_urls.append(f"https://www.electrojaponesa.com{product_url}")
 
             page += 1
+
         return product_urls
 
     @classmethod
     def products_for_url(cls, url, category=None, extra_args=None):
         print(url)
+
         session = session_with_proxy(extra_args)
         response = session.get(url)
-        product_match = re.search(r"var skuJson_0 = (.+)}", response.text).groups()[0]
-        product_data = json.loads(product_match + "}")
+        soup = BeautifulSoup(response.text, "lxml")
+        product_data = json.loads(
+            soup.find("script", {"type": "application/ld+json"}).text
+        )
+        offers = product_data["offers"]["offers"]
 
-        products = []
-        for variant in product_data["skus"]:
-            name = variant["skuname"]
-            key = str(variant["sku"])
-            stock = -1 if variant["availablequantity"] else 0
-            price = Decimal(variant["bestPrice"] // 100)
-            picture_urls = [variant["image"]]
-            p = Product(
-                name,
-                cls.__name__,
-                category,
-                url,
-                url,
-                key,
-                stock,
-                price,
-                price,
-                "COP",
-                sku=key,
-                picture_urls=picture_urls,
-            )
-            products.append(p)
+        assert len(offers) == 1
 
-        return products
+        offer = offers[0]
+        name = product_data["name"]
+        key = str(offer["sku"])
+        stock = -1 if offer["availability"] == "http://schema.org/InStock" else 0
+        price = Decimal(offer["price"])
+        picture_urls = [
+            img["src"]
+            for img in soup.findAll("img", "vtex-store-components-3-x-productImageTag")
+        ]
+
+        p = Product(
+            name,
+            cls.__name__,
+            category,
+            url,
+            url,
+            key,
+            stock,
+            price,
+            price,
+            "COP",
+            sku=key,
+            picture_urls=picture_urls,
+        )
+
+        return [p]
