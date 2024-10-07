2b230e3632d93e983f49f99df2b060ae1783a6d3
Tomás Díaz Toro <tomasdiaztoro@gmail.com>
2024-10-07 12:11:38 -0300

    Update AcerStore

--- storescraper/stores/acer_store.py
+++ storescraper/stores/acer_store.py
@@ -4,7 +4,7 @@ import re
 
 from bs4 import BeautifulSoup
 from decimal import Decimal
-from storescraper.categories import MONITOR, NOTEBOOK, ALL_IN_ONE, MOUSE
+from storescraper.categories import MONITOR, NOTEBOOK, MOUSE, HEADPHONES, WEARABLE
 
 from storescraper.product import Product
 from storescraper.store_with_url_extensions import StoreWithUrlExtensions
@@ -12,65 +12,48 @@ from storescraper.utils import (
     check_ean13,
     session_with_proxy,
     remove_words,
-    vtex_preflight,
+    html_to_markdown,
 )
 
 
 class AcerStore(StoreWithUrlExtensions):
     url_extensions = [
+        ["monitores", MONITOR],
+        ["outlet-monitores-pc", MONITOR],
+        ["mouse", MOUSE],
+        ["headset", HEADPHONES],
         ["notebook-gamer", NOTEBOOK],
         ["notebook", NOTEBOOK],
-        ["corporativo", NOTEBOOK],
-        ["outlet", NOTEBOOK],
-        ["monitores", MONITOR],
-        ["aio", ALL_IN_ONE],
-        ["accesorios", MOUSE],
+        ["outlet-notebook-tradicional", NOTEBOOK],
+        ["outlet-notebook-gamer", NOTEBOOK],
+        ["outlet-ultralivianos", NOTEBOOK],
+        ["smartband", WEARABLE],
     ]
 
     @classmethod
     def discover_urls_for_url_extension(cls, url_extension, extra_args=None):
         product_urls = []
         session = session_with_proxy(extra_args)
+        page = 1
 
-        offset = 0
         while True:
-            if offset >= 120:
-                raise Exception("Page overflow")
+            url = f"https://acerstore.cl/collections/{url_extension}?page={page}"
+            print(url)
 
-            variables = {
-                "from": offset,
-                "to": offset + 12,
-                "selectedFacets": [{"key": "c", "value": url_extension}],
-            }
+            response = session.get(url)
+            soup = BeautifulSoup(response.text, "lxml")
+            products = soup.findAll("div", "product-card-wrapper")
 
-            payload = {
-                "persistedQuery": {
-                    "version": 1,
-                    "sha256Hash": extra_args["sha256Hash"],
-                },
-                "variables": base64.b64encode(
-                    json.dumps(variables).encode("utf-8")
-                ).decode("utf-8"),
-            }
-
-            endpoint = (
-                "https://www.acerstore.cl/_v/segment/graphql/v1"
-                "?extensions={}".format(json.dumps(payload))
-            )
-            response = session.get(endpoint).json()
-
-            product_entries = response["data"]["productSearch"]["products"]
-
-            if not product_entries:
+            if not products:
+                if page == 1:
+                    raise Exception(f"Empty category: {url_extension}")
                 break
 
-            for product_entry in product_entries:
-                product_url = "https://www.acerstore.cl/{}/p".format(
-                    product_entry["linkText"]
-                )
-                product_urls.append(product_url)
+            for product in products:
+                product_url = product.find("a")["href"]
+                product_urls.append(f"https://www.acerstore.cl{product_url}")
 
-            offset += 12
+            page += 1
 
         return product_urls
 
@@ -79,67 +62,40 @@ class AcerStore(StoreWithUrlExtensions):
         print(url)
         session = session_with_proxy(extra_args)
         response = session.get(url)
-        soup = BeautifulSoup(response.text, "html5lib")
+        soup = BeautifulSoup(response.text, "lxml")
+        product_data = json.loads(
+            soup.findAll("script", {"type": "application/ld+json"})[1].text
+        )
 
-        runtime_match = re.search("__RUNTIME__ = (.+)", response.text)
-        runtime_json = json.loads(runtime_match.groups()[0])
+        name = product_data["name"]
+        sku = product_data["sku"]
+        part_number = product_data["category"]
+        description = html_to_markdown(soup.find("p", "product__text").text)
+        description_lower = description.lower()
+        name_lower = name.lower()
 
-        product_data_tag = soup.find("template", {"data-varname": "__STATE__"})
-        product_data = json.loads(str(product_data_tag.find("script").contents[0]))
-
-        if not product_data:
-            return []
-
-        base_json_key = list(product_data.keys())[0]
-        product_specs = product_data[base_json_key]
-
-        key = runtime_json["route"]["params"]["id"]
-        name = product_specs["productName"]
-        sku = product_specs["productReference"]
-        description = product_specs.get("description", None)
-
-        for section in product_specs["categories"]["json"]:
-            if "OUTLET" in section.upper():
+        if (
+            "outlet" in description_lower
+            or "outlet" in name_lower
+            or "seminuevo" in description_lower
+            or "seminuevo" in name.lower()
+        ):
+            if "openbox" in description_lower or "open box" in description_lower:
+                condition = "https://schema.org/OpenBoxCondition"
+            else:
                 condition = "https://schema.org/RefurbishedCondition"
-                break
         else:
             condition = "https://schema.org/NewCondition"
 
-        pricing_key = "${}.items.0.sellers.0.commertialOffer".format(base_json_key)
-        pricing_data = product_data[pricing_key]
-
-        price = Decimal(str(pricing_data["Price"]))
-
-        if not price:
-            pricing_key = "{}.specificationGroups.2.specifications.0".format(
-                base_json_key
-            )
-            price_text = product_data[pricing_key]["name"].strip()
-            if price_text == "Precio sin stock":
-                return []
-            price = Decimal(remove_words(price_text))
-
-        stock = pricing_data["AvailableQuantity"]
-        picture_list_key = "{}.items.0".format(base_json_key)
-        picture_list_node = product_data[picture_list_key]
-        picture_ids = [x["id"] for x in picture_list_node["images"]]
-
-        picture_urls = []
-        for picture_id in picture_ids:
-            picture_node = product_data[picture_id]
-            picture_urls.append(picture_node["imageUrl"].split("?")[0])
-
-        item_key = "{}.items.0".format(base_json_key)
-        part_number = product_data[item_key]["name"]
-
-        ean = None
-        for property in product_specs["properties"]:
-            if product_data[property["id"]]["name"] == "EAN":
-                ean = product_data[property["id"]]["values"]["json"][0]
-                if check_ean13(ean):
-                    break
-                else:
-                    ean = None
+        offer = product_data["offers"]
+        price = Decimal(offer["price"])
+        key = offer["url"].split("?variant=")[1]
+        stock = -1 if offer["availability"] == "http://schema.org/InStock" else 0
+        pictures_container = soup.find("div", "grid__item product__media-wrapper")
+        picture_urls = set(
+            f"https:{img['src'].split('?')[0]}"
+            for img in pictures_container.findAll("img")
+        )
 
         p = Product(
             name,
@@ -154,16 +110,9 @@ class AcerStore(StoreWithUrlExtensions):
             "CLP",
             sku=sku,
             part_number=part_number,
-            ean=ean,
             description=description,
             picture_urls=picture_urls,
             condition=condition,
         )
 
         return [p]
-
-    @classmethod
-    def preflight(cls, extra_args=None):
-        return vtex_preflight(
-            extra_args, "https://www.acerstore.cl/notebook/notebook-clasico"
-        )
