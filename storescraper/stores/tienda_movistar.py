36a3c6140e1013648702571988b000a4534e9180
Tomás Díaz Toro <tomasdiaztoro@gmail.com>
2024-10-22 14:33:43 -0300

    Update TiendaMovistar

--- storescraper/stores/tienda_movistar.py
+++ storescraper/stores/tienda_movistar.py
@@ -1,3 +1,5 @@
+from bs4 import BeautifulSoup
+
 from storescraper.categories import (
     HEADPHONES,
     TABLET,
@@ -39,23 +41,30 @@ class TiendaMovistar(Movistar):
         products = super(TiendaMovistar, cls).products_for_url(url)
 
         session = session_with_proxy(extra_args)
-        session.headers[
-            "Content-Type"
-        ] = "application/x-www-form-urlencoded; charset=UTF-8"
+        session.headers["Content-Type"] = (
+            "application/x-www-form-urlencoded; charset=UTF-8"
+        )
         session.headers["x-requested-with"] = "XMLHttpRequest"
         session.headers["referer"] = url
+        soup = BeautifulSoup(session.get(url).text, "html5lib")
+        form_key = soup.find("input", {"id": "du-form-key"})["value"]
+        form_emh = soup.find("input", {"id": "du-form-emh"})["value"]
 
         for product in products:
             product.key = product.sku
             product.cell_plan_name = None
             product.cell_monthly_payment = None
-            payload = "sku={}&tipo_producto=fullprice".format(product.sku)
+            payload = f"key={form_key}&emh={form_emh}&sku={product.sku}&tipo_producto=fullprice"
             stock_res = session.post(
                 "https://catalogo.movistar.cl/tienda/detalleequipo/ajax/consultastockunificado",
                 payload,
             )
             stock_json = stock_res.json()
-            if stock_json["respuesta"]["detalle"] == "con-stock":
+
+            if (
+                "respuesta" in stock_json
+                and stock_json["respuesta"]["detalle"] == "con-stock"
+            ):
                 product.stock = -1
             else:
                 product.stock = 0
